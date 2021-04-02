b031934129a6d90ebd26c3f14fe6357da1420597
quincylin1 <quincylin.333@gmail.com>
2021-04-03 00:41:23 +0800

    documentation and dbnet related code

---
+++ mmocr/models/textdet/detectors/dbnet.py
@@ -0,0 +1,23 @@
+from mmdet.models.builder import DETECTORS
+from . import SingleStageTextDetector, TextDetectorMixin
+
+
+@DETECTORS.register_module()
+class DBNet(TextDetectorMixin, SingleStageTextDetector):
+    """The class for implementing DBNet text detector: Real-time Scene Text
+    Detection with Differentiable Binarization.
+
+    [https://arxiv.org/abs/1911.08947].
+    """
+
+    def __init__(self,
+                 backbone,
+                 neck,
+                 bbox_head,
+                 train_cfg=None,
+                 test_cfg=None,
+                 pretrained=None,
+                 show_score=False):
+        SingleStageTextDetector.__init__(self, backbone, neck, bbox_head,
+                                         train_cfg, test_cfg, pretrained)
+        TextDetectorMixin.__init__(self, show_score)
