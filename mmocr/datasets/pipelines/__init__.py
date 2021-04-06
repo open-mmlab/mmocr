8d527bdeca467fa101e0be9da2ee484c3ad5d418
Hongbin Sun <hongbin306@gmail.com>
2021-04-06 15:28:07 +0800

    fix #45: remove useless (#46)

--- mmocr/datasets/pipelines/__init__.py
+++ mmocr/datasets/pipelines/__init__.py
@@ -8,8 +8,7 @@ from .ocr_transforms import (FancyPCA, NormalizeOCR, OnlineCropOCR,
                              OpencvToPil, PilToOpencv, RandomPaddingOCR,
                              RandomRotateImageBox, ResizeOCR, ToTensorOCR)
 from .test_time_aug import MultiRotateAugOCR
-from .textdet_targets import (DBNetTargets, DRRGTargets, PANetTargets,
-                              TextSnakeTargets)
+from .textdet_targets import DBNetTargets, PANetTargets, TextSnakeTargets
 from .transforms import (ColorJitter, RandomCropInstances,
                          RandomCropPolyInstances, RandomRotatePolyInstances,
                          RandomRotateTextDet, ScaleAspectJitter,
@@ -20,8 +19,8 @@ __all__ = [
     'ToTensorOCR', 'CustomFormatBundle', 'DBNetTargets', 'PANetTargets',
     'ColorJitter', 'RandomCropInstances', 'RandomRotateTextDet',
     'ScaleAspectJitter', 'MultiRotateAugOCR', 'OCRSegTargets', 'FancyPCA',
-    'DRRGTargets', 'RandomCropPolyInstances', 'RandomRotatePolyInstances',
-    'RandomPaddingOCR', 'ImgAug', 'EastRandomCrop', 'RandomRotateImageBox',
-    'OpencvToPil', 'PilToOpencv', 'KIEFormatBundle', 'SquareResizePad',
-    'TextSnakeTargets', 'sort_vertex'
+    'RandomCropPolyInstances', 'RandomRotatePolyInstances', 'RandomPaddingOCR',
+    'ImgAug', 'EastRandomCrop', 'RandomRotateImageBox', 'OpencvToPil',
+    'PilToOpencv', 'KIEFormatBundle', 'SquareResizePad', 'TextSnakeTargets',
+    'sort_vertex'
 ]
--- mmocr/datasets/pipelines/textdet_targets/__init__.py
+++ mmocr/datasets/pipelines/textdet_targets/__init__.py
@@ -1,11 +1,10 @@
 from .base_textdet_targets import BaseTextDetTargets
 from .dbnet_targets import DBNetTargets
-from .drrg_targets import DRRGTargets
 from .panet_targets import PANetTargets
 from .psenet_targets import PSENetTargets
 from .textsnake_targets import TextSnakeTargets
 
 __all__ = [
     'BaseTextDetTargets', 'PANetTargets', 'PSENetTargets', 'DBNetTargets',
-    'TextSnakeTargets', 'DRRGTargets'
+    'TextSnakeTargets'
 ]
