90e14b97cc20075cb8b1ab2c0886c88f3a0ad863
LvTianlei <2793145003@qq.com>
2021-04-22 22:40:36 +0800

    Add licence, change variable name and format

--- mmocr/models/textrecog/__init__.py
+++ mmocr/models/textrecog/__init__.py
@@ -5,5 +5,5 @@ from .encoders import *  # noqa: F401,F403
 from .heads import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
+from .preprocessor import *  # noqa: F401,F403
 from .recognizer import *  # noqa: F401,F403
-from .preprocessor import *
