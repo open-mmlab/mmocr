5c6f9ddbedb259738b201c7ba05ef197d8fd38be
Hongbin Sun <hongbin306@gmail.com>
2021-05-11 16:13:56 +0800

    fix bug of loss nan (#159)
    * fix bug of loss nan
    
    * update
    
    * update ctc loss

--- mmocr/models/textrecog/losses/ctc_loss.py
+++ mmocr/models/textrecog/losses/ctc_loss.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 
@@ -36,12 +38,15 @@ class CTCLoss(nn.Module):
         self.ctc_loss = nn.CTCLoss(
             blank=blank, reduction=reduction, zero_infinity=zero_infinity)
 
-    def forward(self, outputs, targets_dict):
+    def forward(self, outputs, targets_dict, img_metas=None):
+        valid_ratios = None
+        if img_metas is not None:
+            valid_ratios = [
+                img_meta.get('valid_ratio', 1.0) for img_meta in img_metas
+            ]
 
         outputs = torch.log_softmax(outputs, dim=2)
         bsz, seq_len = outputs.size(0), outputs.size(1)
-        input_lengths = torch.full(
-            size=(bsz, ), fill_value=seq_len, dtype=torch.long)
         outputs_for_loss = outputs.permute(1, 0, 2).contiguous()  # T * N * C
 
         if self.flatten:
@@ -54,6 +59,16 @@ class CTCLoss(nn.Module):
                 targets[idx, :valid_len] = tensor[:valid_len]
 
         target_lengths = targets_dict['target_lengths']
+        target_lengths = torch.clamp(target_lengths, min=1, max=seq_len).long()
+
+        input_lengths = torch.full(
+            size=(bsz, ), fill_value=seq_len, dtype=torch.long)
+        if not self.flatten and valid_ratios is not None:
+            input_lengths = [
+                math.ceil(valid_ratio * seq_len)
+                for valid_ratio in valid_ratios
+            ]
+            input_lengths = torch.Tensor(input_lengths).long()
 
         loss_ctc = self.ctc_loss(outputs_for_loss, targets, input_lengths,
                                  target_lengths)
