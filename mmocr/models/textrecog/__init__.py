3acd8f7e87e3cbff8ddead8acf4cd59d93f4de9b
lizz <lizz@sensetime.com>
2021-05-11 20:45:38 +0800

    Add things from "import *" to __all__
    Signed-off-by: lizz <lizz@sensetime.com>

--- mmocr/models/textrecog/__init__.py
+++ mmocr/models/textrecog/__init__.py
@@ -1,9 +1,17 @@
-from .backbones import *  # noqa: F401,F403
-from .convertors import *  # noqa: F401,F403
-from .decoders import *  # noqa: F401,F403
-from .encoders import *  # noqa: F401,F403
-from .heads import *  # noqa: F401,F403
-from .losses import *  # noqa: F401,F403
-from .necks import *  # noqa: F401,F403
-from .preprocessor import *  # noqa: F401,F403
-from .recognizer import *  # noqa: F401,F403
+from . import (backbones, convertors, decoders, encoders, heads, losses, necks,
+               preprocessor, recognizer)
+
+from .backbones import *  # NOQA
+from .convertors import *  # NOQA
+from .decoders import *  # NOQA
+from .encoders import *  # NOQA
+from .heads import *  # NOQA
+from .losses import *  # NOQA
+from .necks import *  # NOQA
+from .preprocessor import *  # NOQA
+from .recognizer import *  # NOQA
+
+__all__ = (
+    backbones.__all__ + convertors.__all__ + decoders.__all__ +
+    encoders.__all__ + heads.__all__ + losses.__all__ + necks.__all__ +
+    preprocessor.__all__ + recognizer.__all__)
