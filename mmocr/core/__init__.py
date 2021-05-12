1b32689569e73d952322a6694bfed154246b7dbf
lizz <lizz@sensetime.com>
2021-05-12 15:17:10 +0800

    Remove tile_image
    Signed-off-by: lizz <lizz@sensetime.com>

--- mmocr/core/__init__.py
+++ mmocr/core/__init__.py
@@ -3,14 +3,14 @@ from .mask import extract_boundary, points2boundary, seg2boundary
 from .visualize import (det_recog_show_result, imshow_edge_node,
                         imshow_pred_boundary, imshow_text_char_boundary,
                         imshow_text_label, overlay_mask_img, show_feature,
-                        show_img_boundary, show_pred_gt, tile_image)
+                        show_img_boundary, show_pred_gt)
 
 from .evaluation import *  # NOQA
 
 __all__ = [
     'points2boundary', 'seg2boundary', 'extract_boundary', 'overlay_mask_img',
     'show_feature', 'show_img_boundary', 'show_pred_gt',
-    'imshow_pred_boundary', 'imshow_text_char_boundary', 'tile_image',
-    'imshow_text_label', 'imshow_edge_node', 'det_recog_show_result'
+    'imshow_pred_boundary', 'imshow_text_char_boundary', 'imshow_text_label',
+    'imshow_edge_node', 'det_recog_show_result'
 ]
 __all__ += evaluation.__all__
