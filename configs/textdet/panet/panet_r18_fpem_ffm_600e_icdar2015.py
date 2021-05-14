bd83d8aac5ea5bdc283de85eef58afd8de0c2443
jeffreykuang <kuangzhh@gmail.com>
2021-05-15 07:37:48 +0800

    Update configs/textdet/panet/panet_r18_fpem_ffm_600e_icdar2015.py

--- configs/textdet/panet/panet_r18_fpem_ffm_600e_icdar2015.py
+++ configs/textdet/panet/panet_r18_fpem_ffm_600e_icdar2015.py
@@ -79,7 +79,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=8,
-    workers_per_gpu=0,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + '/instances_training.json',
