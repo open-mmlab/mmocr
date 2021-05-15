0e3dd8e9ee5bc172e7b8957666f8b22192f68663
HolyCrap <theochan666@gmail.com>
2021-05-15 15:21:48 +0800

    add unittest

--- tests/test_models/test_loss.py
+++ tests/test_models/test_loss.py
@@ -70,3 +70,32 @@ def test_fcenetloss():
 
     loss = fcenetloss(preds, 0, p3_maps, p4_maps, p5_maps)
     assert isinstance(loss, dict)
+
+
+def test_drrgloss():
+    drrgloss = losses.DRRGLoss()
+
+    # test balanced_bce_loss
+    pred = torch.tensor([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=torch.float)
+    target = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=torch.long)
+    mask = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=torch.long)
+    bce_loss = drrgloss.balance_bce_loss(pred, target, mask).item()
+    assert np.allclose(bce_loss, 0)
+
+    # test gcn_loss
+    preds = torch.tensor([[0., 1.], [1., 0.]])
+    labels = torch.tensor([1, 0], dtype=torch.long)
+    gcn_loss = drrgloss.gcn_loss((preds, labels))
+    assert gcn_loss.item()
+
+    # test bitmasks2tensor
+    mask = [[1, 0, 1], [1, 1, 1], [0, 0, 1]]
+    target = [[1, 0, 1, 0, 0], [1, 1, 1, 0, 0], [0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    masks = [np.array(mask)]
+    bitmasks = BitmapMasks(masks, 3, 3)
+    target_sz = (6, 5)
+    results = drrgloss.bitmasks2tensor([bitmasks], target_sz)
+    assert len(results) == 1
+    assert torch.sum(torch.abs(results[0].float() -
+                               torch.Tensor(target))).item() == 0
