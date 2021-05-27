b6a496ae82b5bce3806518c4eb8796eacd2506f9
RangiLyu <lyuchqi@gmail.com>
2021-05-27 21:28:48 +0800

    fix lint

--- mmocr/models/common/backbones/unet.py
+++ mmocr/models/common/backbones/unet.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as cp
+from mmcv.cnn import UPSAMPLE_LAYERS as MMCV_UPSAMPLE_LAYERS
 from mmcv.cnn import (ConvModule, build_activation_layer, build_norm_layer,
                       build_upsample_layer, constant_init, kaiming_init)
-from mmcv.cnn import UPSAMPLE_LAYERS as MMCV_UPSAMPLE_LAYERS
 from mmcv.runner import load_checkpoint
 from mmcv.utils import Registry
 from mmcv.utils.parrots_wrapper import _BatchNorm
