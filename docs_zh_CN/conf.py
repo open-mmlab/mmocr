f2160e21dc7acb155173d45eb32ae68afa294fa2
gaotongxiao <gaotongxiao@gmail.com>
2021-08-02 15:57:18 +0800

    update docs version

--- docs_zh_CN/conf.py
+++ docs_zh_CN/conf.py
@@ -23,7 +23,7 @@ copyright = '2020-2030, OpenMMLab'
 author = 'OpenMMLab'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 
