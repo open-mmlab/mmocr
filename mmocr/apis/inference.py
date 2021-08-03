e078370d40f4c25eefae3e6aff1a026926f66218
Tong Gao <gaotongxiao@gmail.com>
2021-08-03 13:01:53 +0800

    Update mmocr/apis/inference.py
    Co-authored-by: Hongbin Sun <hongbin306@gmail.com>

--- mmocr/apis/inference.py
+++ mmocr/apis/inference.py
@@ -52,7 +52,7 @@ def model_inference(model,
         imgs (str/ndarray or list[str/ndarray] or tuple[str/ndarray]):
             Either image files or loaded images.
         batch_mode (bool): If True, use batch mode for inference.
-        ann (dict): annotation info for kie
+        ann (dict): Annotation info for key information extraction.
         return_data: return postprocessed data
     Returns:
         result (dict): Predicted results.
