304b48e187cee37618a91eeed075b9f470635ac1
Tong Gao <gaotongxiao@gmail.com>
2021-08-09 19:16:43 +0800

    [Docs] Full screen support in readthedocs (#413)
    * add full screen css
    
    * New dir structure

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,8 @@ master_doc = 'index'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+html_css_files = ['css/readthedocs.css']
 
 
 def builder_inited_handler(app):
