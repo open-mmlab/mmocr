e267d0628141cc74a10b6daf2946a88958e5c24b
Tong Gao <gaotongxiao@gmail.com>
2021-11-18 22:26:21 +0800

    [Fix] Always broadcast a random seed to all the processes (#600)

--- mmocr/apis/__init__.py
+++ mmocr/apis/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import init_detector, model_inference
-from .train import train_detector
+from .train import init_random_seed, train_detector
 
-__all__ = ['model_inference', 'train_detector', 'init_detector']
+__all__ = [
+    'model_inference', 'train_detector', 'init_detector', 'init_random_seed'
+]
