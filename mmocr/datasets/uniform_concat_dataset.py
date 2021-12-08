c93c4e0d50bb36f8ebee4f76e5d8be3a8842b79a
Hongbin Sun <hongbin306@gmail.com>
2021-12-08 14:40:39 +0800

    support different pipelines

--- mmocr/datasets/uniform_concat_dataset.py
+++ mmocr/datasets/uniform_concat_dataset.py
@@ -12,7 +12,7 @@ class UniformConcatDataset(ConcatDataset):
     replacement.
 
     Args:
-        datasets (list[:obj:`Dataset`]): A list of datasets.
+        datasets (list[dict] | list[list[dict]]): A list of datasets cfgs.
         separate_eval (bool): Whether to evaluate the results
             separately if it is used as validation dataset.
             Defaults to True.
@@ -23,22 +23,45 @@ class UniformConcatDataset(ConcatDataset):
             If ``list[list[dict]]``, pipeline of dataset which is None
             in datasets will be replaced by the corresponding pipeline
             in the list.
+        force_apply (bool): If True, apply pipeline above to each dataset
+            even if it have its own pipeline. Default: False.
     """
 
-    def __init__(self, datasets, separate_eval=True, pipeline=None, **kwargs):
-        from_cfg = all(isinstance(x, dict) for x in datasets)
+    def __init__(self,
+                 datasets,
+                 separate_eval=True,
+                 pipeline=None,
+                 force_apply=False,
+                 **kwargs):
+        new_datasets = []
         if pipeline is not None:
-            assert from_cfg, 'datasets should be config dicts'
+            assert len(
+                pipeline
+            ) > 0, 'pipeline must be list[dict] or list[list[dict]],'
             if is_2dlist(pipeline):
+                assert is_2dlist(datasets)
                 assert len(datasets) == len(pipeline)
-                for dataset, tmp_pipeline in zip(datasets, pipeline):
-                    assert all(isinstance(x, dict) for x in tmp_pipeline)
-                    if dataset['pipeline'] is None:
-                        dataset['pipeline'] = copy.deepcopy(tmp_pipeline)
+                for sub_datasets, tmp_pipeline in zip(datasets, pipeline):
+                    self._apply_pipeline(sub_datasets, tmp_pipeline,
+                                         force_apply)
+                    new_datasets.extend(sub_datasets)
             elif isinstance(pipeline, list):
-                assert all(isinstance(x, dict) for x in pipeline)
-                for dataset in datasets:
-                    if dataset['pipeline'] is None:
-                        dataset['pipeline'] = copy.deepcopy(pipeline)
-        datasets = [build_dataset(c, kwargs) for c in datasets]
+                self._apply_pipeline(datasets, pipeline, force_apply)
+                new_datasets = datasets
+        else:
+            if is_2dlist(datasets):
+                for sub_datasets in datasets:
+                    new_datasets.extend(sub_datasets)
+            else:
+                new_datasets = datasets
+        datasets = [build_dataset(c, kwargs) for c in new_datasets]
         super().__init__(datasets, separate_eval)
+
+    @staticmethod
+    def _apply_pipeline(datasets, pipeline, force_apply=False):
+        from_cfg = all(isinstance(x, dict) for x in datasets)
+        assert from_cfg, 'datasets should be config dicts'
+        assert all(isinstance(x, dict) for x in pipeline)
+        for dataset in datasets:
+            if dataset['pipeline'] is None or force_apply:
+                dataset['pipeline'] = copy.deepcopy(pipeline)
--- tests/test_dataset/test_uniform_concat_dataset.py
+++ tests/test_dataset/test_uniform_concat_dataset.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import copy
+
 from mmocr.datasets import UniformConcatDataset
 from mmocr.utils import list_from_file
 
@@ -27,19 +29,32 @@ def test_dataset_warpper():
     train2 = {key: value for key, value in train1.items()}
     train2['pipeline'] = pipeline2
 
-    # test pipeline is 1d list
-    uniform_concat_dataset = UniformConcatDataset(
-        datasets=[train1, train2], pipeline=pipeline1)
+    # pipeline is 1d list
+    copy_train1 = copy.deepcopy(train1)
+    copy_train2 = copy.deepcopy(train2)
+    tmp_dataset = UniformConcatDataset(
+        datasets=[copy_train1, copy_train2],
+        pipeline=pipeline1,
+        force_apply=True)
 
-    assert len(uniform_concat_dataset) == 2 * len(list_from_file(ann_file))
-    assert len(uniform_concat_dataset.datasets[0].pipeline.transforms) != len(
-        uniform_concat_dataset.datasets[1].pipeline.transforms)
+    assert len(tmp_dataset) == 2 * len(list_from_file(ann_file))
+    assert len(tmp_dataset.datasets[0].pipeline.transforms) == len(
+        tmp_dataset.datasets[1].pipeline.transforms)
 
-    # test pipeline is None
-    tmp_dataset = UniformConcatDataset(datasets=[train2], pipeline=None)
+    # pipeline is None
+    copy_train2 = copy.deepcopy(train2)
+    tmp_dataset = UniformConcatDataset(datasets=[copy_train2], pipeline=None)
     assert len(tmp_dataset.datasets[0].pipeline.transforms) == len(pipeline2)
 
-    # test pipeline is 2d list
+    copy_train2 = copy.deepcopy(train2)
     tmp_dataset = UniformConcatDataset(
-        datasets=[train1, train2], pipeline=[pipeline1, pipeline2])
+        datasets=[[copy_train2], [copy_train2]], pipeline=None)
+    assert len(tmp_dataset.datasets[0].pipeline.transforms) == len(pipeline2)
+
+    # pipeline is 2d list
+    copy_train1 = copy.deepcopy(train1)
+    copy_train2 = copy.deepcopy(train2)
+    tmp_dataset = UniformConcatDataset(
+        datasets=[[copy_train1], [copy_train2]],
+        pipeline=[pipeline1, pipeline2])
     assert len(tmp_dataset.datasets[0].pipeline.transforms) == len(pipeline1)
