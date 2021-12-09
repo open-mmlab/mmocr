fec986aba0e95e85c7806e8b0c34bc63b21b629b
Hongbin Sun <hongbin306@gmail.com>
2021-12-09 09:34:09 +0800

    [Enhancement] Use shape from loaded image (#652)
    * use shape from loaded image
    
    * complete unittest
    
    * add params as gate
    
    * add comment
    
    * add assert

--- tests/test_dataset/test_loading.py
+++ tests/test_dataset/test_loading.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import copy
+
 import numpy as np
 
 from mmocr.datasets.pipelines import LoadImageFromNdarray, LoadTextAnnotations
@@ -31,12 +33,37 @@ def test_loadtextannotation():
     with_seg = False
     poly2mask = False
 
-    loader = LoadTextAnnotations(with_bbox, with_label, with_mask, with_seg,
-                                 poly2mask)
-    output = loader._load_masks(results)
+    # If no 'ori_shape' in result but use_img_shape=True,
+    # result['img_info']['height'] and result['img_info']['width']
+    # will be used to generate mask.
+    loader = LoadTextAnnotations(
+        with_bbox,
+        with_label,
+        with_mask,
+        with_seg,
+        poly2mask,
+        use_img_shape=True)
+    tmp_results = copy.deepcopy(results)
+    output = loader._load_masks(tmp_results)
     assert len(output['gt_masks_ignore']) == 4
     assert np.allclose(output['gt_masks_ignore'].masks[0],
                        [[499, 94, 531, 94, 531, 124, 499, 124]])
+    assert output['gt_masks_ignore'].height == results['img_info']['height']
+
+    # If 'ori_shape' in result and use_img_shape=True,
+    # result['ori_shape'] will be used to generate mask.
+    loader = LoadTextAnnotations(
+        with_bbox,
+        with_label,
+        with_mask,
+        with_seg,
+        poly2mask=True,
+        use_img_shape=True)
+    tmp_results = copy.deepcopy(results)
+    tmp_results['ori_shape'] = (640, 640, 3)
+    output = loader._load_masks(tmp_results)
+    assert output['img_info']['height'] == 640
+    assert output['gt_masks_ignore'].height == 640
 
 
 def test_load_img_from_numpy():
@@ -47,3 +74,13 @@ def test_load_img_from_numpy():
 
     assert output['img'].shape[2] == 3
     assert len(output['img'].shape) == 3
+
+    result = {'img': np.ones((32, 100, 1), dtype=np.uint8)}
+    load = LoadImageFromNdarray(color_type='color')
+    output = load(result)
+    assert output['img'].shape[2] == 3
+
+    result = {'img': np.ones((32, 100, 3), dtype=np.uint8)}
+    load = LoadImageFromNdarray(color_type='grayscale', to_float32=True)
+    output = load(result)
+    assert output['img'].shape[2] == 1
