06a47852b44f08a85f4280009b5aecd5da5d9e39
Hongbin Sun <hongbin306@gmail.com>
2022-02-24 20:18:44 +0800

    add http backend

--- mmocr/datasets/utils/__init__.py
+++ mmocr/datasets/utils/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .loader import HardDiskLoader, LmdbLoader, PetrelLoader
+from .loader import HardDiskLoader, HttpLoader, LmdbLoader, PetrelLoader
 from .parser import LineJsonParser, LineStrParser
 
 __all__ = [
-    'HardDiskLoader', 'LmdbLoader', 'PetrelLoader', 'LineStrParser',
-    'LineJsonParser'
+    'HardDiskLoader', 'LmdbLoader', 'PetrelLoader', 'HttpLoader',
+    'LineStrParser', 'LineJsonParser'
 ]
