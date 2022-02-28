4fd729f6d53216559ef5e0e38f391da53d1b3457
gaotongxiao <gaotongxiao@gmail.com>
2022-02-28 10:38:02 +0800

    update mixer

--- mmocr/core/bbox/mixer/mixer.py
+++ mmocr/core/bbox/mixer/mixer.py
@@ -7,10 +7,19 @@ class Mixer:
     results (text regions) from the text detector with their assigned ground
     truth properties including texts. Intended for text spotter models.
 
+    It works in two mode:
+
+    Training:
+    Testing:...
+
     Args:
         pred_keys (list[str]): The keys accessing the predicted regions in
             ``pred_results``.
-        add_gt (bool): Whether to add all the ground truth data to the result.
+        add_gt (bool) Whether to add all the ground truth data to the result.
+
+    Warning:
+        It hasn't handled specific img_metas fields such as ori_shape. May
+        need extra efforts to implement extension functions.
     """
 
     def __init__(
@@ -21,15 +30,16 @@ class Mixer:
         self.pred_keys = pred_keys
         self.add_gt = add_gt
 
-    def __call__(self, img_metas, pred_results, gt_inds=None):
+    def mix_gt_pred(self, img_metas, pred_results, gt_inds=None):
         """
         Args:
             img_metas (dict): The original "img_metas".
             pred_results (dict): The prediction results for one image generated
                 from the text detector.
-            gt_inds (torch.LongTensor): The ground truth assignment results
-                from the assigner. Its length should be equal to the number of
-                predicted boxes in ``pred_results``.
+            gt_inds (torch.LongTensor or None): The ground truth assignment
+                results from the assigner. Its length should be equal to the
+                number of predicted boxes in ``pred_results``. If None, Mixer
+                works in the test mode.
 
         Returns:
             dict: New ``img_metas`` for text recognizers.
@@ -37,31 +47,59 @@ class Mixer:
 
         self.new_img_metas = {}
         self.img_metas = img_metas
-        if gt_inds is not None:
-            self._extract_pred(pred_results, gt_inds)
-        if self.add_gt:
-            self._merge_img_metas(self.img_metas)
+        self._extract_pred(pred_results, gt_inds)
+
+        # Only add gt instances for training
+        if self.add_gt and gt_inds is not None:
+            self.merge_img_metas(self.img_metas)
         return self.new_img_metas
+
+    def cat_img_metas(self, n_img_metas, get_idx_mapping=True):
+        """
+        Args:
+            get_idx_mapping (bool): Whether to get the index mapping that maps
+                new img_metas to the old one.
+
+        Returns:
+            dict or (dict, list[int]): The length of the list is the number of
+            elements in `n_img_metas` which represents the mapping from new one
+            to the old one.
+        """
+        self.new_img_metas = {}
+        if get_idx_mapping:
+            idx_mapping = []
+        for i, img_metas in enumerate(n_img_metas):
+            self.merge_img_metas(img_metas)
+            if get_idx_mapping:
+                idx_mapping += [i for _ in range(len(img_metas))]
+        return self.new_img_metas, idx_mapping if get_idx_mapping \
+            else self.new_img_metas
 
     def _extract_pred(self, pred_results, gt_inds):
         # Since we will merge everything into datasample (img_metas)
         # Let's just treat it as a dictionary to be merged
-        pred_pos_inds = torch.nonzero(gt_inds > 0, as_tuple=True)[0]
-        gt_pos_inds = gt_inds[pred_pos_inds]
-        gt_pos_inds -= 1  # minus one to sync with the indexes of img_metas
+        if gt_inds is None:
+            pred_pos_inds = None
+            pred_items = pred_results[self.pred_keys[0]]
+            gt_pos_inds = pred_items.new_zeros((len(pred_items)),
+                                               dtype=torch.long)
+        else:
+            pred_pos_inds = torch.nonzero(gt_inds > 0, as_tuple=True)[0]
+            gt_pos_inds = gt_inds[pred_pos_inds]
+            gt_pos_inds -= 1  # minus one to sync with the indexes of img_metas
 
         # Get predictions
-        self._merge_img_metas(pred_results, self.pred_keys, pred_pos_inds)
+        self.merge_img_metas(pred_results, self.pred_keys, pred_pos_inds)
 
         # Get ground truth
-        self._merge_img_metas(
+        self.merge_img_metas(
             self.img_metas, self.pred_keys, gt_pos_inds, exclude_keys=True)
 
-    def _merge_img_metas(self,
-                         src_img_metas,
-                         src_keys=None,
-                         src_inds=None,
-                         exclude_keys=False):
+    def merge_img_metas(self,
+                        src_img_metas,
+                        src_keys=None,
+                        src_inds=None,
+                        exclude_keys=False):
         """Merge certain part of ``src_img_metas`` with ``self.new_img_metas``.
 
         Args:
@@ -107,5 +145,4 @@ class Mixer:
                     stack_list.append(src_img_metas[key])
                 self.new_img_metas[key] = torch.cat(stack_list, dim=0)
             else:
-                raise NotImplementedError(f'Merging {type(src_img_metas[key])}'
-                                          'is not supported yet')
+                self.new_img_metas[key] = src_img_metas[key]  # May override
--- tests/test_core/test_mixer.py
+++ tests/test_core/test_mixer.py
@@ -9,7 +9,7 @@ def test_mixer():
         bboxes=torch.FloatTensor([[0, 0, 0, 1, 1, 0, 1, 1],
                                   [1, 0, 1, 1, 2, 0, 2, 1]]),
         texts=['hello', 'world'],
-    )
+        filename='test.jpg')
     pred_results = dict(
         bboxes=torch.FloatTensor(
             [[0.5, 0.5, 0, 1, 1, 0, 1, 1], [-1, -1, -1, -1, -1, -1, -1, 1],
@@ -18,7 +18,7 @@ def test_mixer():
     gt_inds = torch.LongTensor([1, 0, -1, 1, 2])
 
     mixer = Mixer(pred_keys=['bboxes'], add_gt=True)
-    new_img_metas = mixer(img_metas, pred_results, gt_inds)
+    new_img_metas = mixer.mix_gt_pred(img_metas, pred_results, gt_inds)
     target_bboxes = torch.FloatTensor([[0.5, 0.5, 0, 1, 1, 0, 1, 1],
                                        [-0.5, -0.5, 0, 1, 1, 0, 1, 1],
                                        [1, 0, 1, 1, 2, 0, 2, 1],
@@ -27,10 +27,16 @@ def test_mixer():
     target_texts = ['hello', 'hello', 'world', 'hello', 'world']
     assert torch.allclose(new_img_metas['bboxes'], target_bboxes)
     assert target_texts == new_img_metas['texts']
+    assert all([new_img_metas['filename'] == 'test.jpg'])
+
+    new_img_metas = mixer.mix_gt_pred(img_metas, pred_results)
+    target_bboxes = pred_results['bboxes']
+    assert torch.allclose(new_img_metas['bboxes'], target_bboxes)
+    assert all([new_img_metas['filename'] == 'test.jpg'])
 
     mixer = Mixer(pred_keys=['bboxes'], add_gt=False)
     gt_inds = torch.LongTensor([-1, -1, 2, 2, 1])
-    new_img_metas = mixer(img_metas, pred_results, gt_inds)
+    new_img_metas = mixer.mix_gt_pred(img_metas, pred_results, gt_inds)
     target_bboxes = torch.FloatTensor([[-1, -1, -1, -1, -1, -1, -1, 1],
                                        [-0.5, -0.5, 0, 1, 1, 0, 1, 1],
                                        [1, 0, 1, 1, 2, 0, 2, 1]])
