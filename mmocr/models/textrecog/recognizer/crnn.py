428688102da9bfbbb53d950a8809f2cec835c968
Hongbin Sun <hongbin306@gmail.com>
2022-03-04 16:20:50 +0800

    [Fix] Use RECOGNIZER instead of DETECTORS (#685)
    * use recognizer
    
    * rm useless

--- mmocr/models/textrecog/recognizer/crnn.py
+++ mmocr/models/textrecog/recognizer/crnn.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmocr.models.builder import DETECTORS
+from mmocr.models.builder import RECOGNIZERS
 from .encode_decode_recognizer import EncodeDecodeRecognizer
 
 
-@DETECTORS.register_module()
+@RECOGNIZERS.register_module()
 class CRNNNet(EncodeDecodeRecognizer):
     """CTC-loss based recognizer."""
