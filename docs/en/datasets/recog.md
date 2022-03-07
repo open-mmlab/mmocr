3f056f6b1d7f244f8004e84d3c4e019697c387d4
xinke-wang <xinyu.wang02@adelaide.edu.au>
2022-03-07 16:25:23 +1030

    add NAF converter

--- docs/en/datasets/recog.md
+++ docs/en/datasets/recog.md
@@ -79,6 +79,13 @@
 │   │   ├── annotations
 │   │   ├── train_label.txt
 │   │   ├── test_label.txt
+│   ├── naf
+│   │   ├── imgs
+│   │   ├── dst_imgs
+│   │   ├── annotations
+│   │   ├── train_label.txt
+│   │   ├── val_label.txt
+│   │   ├── test_label.txt
 ```
 
 |        Dataset        |                                                images                                                 |                                                                                                                                                                                                    annotation file                                                                                                                                                                                                    |                                                      annotation file                                                      |
@@ -99,6 +106,7 @@
 |       Totaltext       |                       [homepage](https://github.com/cs-chan/Total-Text-Dataset)                       |                                                                                                                                                                                                           -                                                                                                                                                                                                           |                                                             -                                                             |  |
 |       OpenVINO        |                  [Open Images](https://github.com/cvdfoundation/open-images-dataset)                  |                                                                                                                                               [annotations](https://storage.openvinotoolkit.org/repositories/openvino_training_extensions/datasets/open_images_v5_text)                                                                                                                                               | [annotations](https://storage.openvinotoolkit.org/repositories/openvino_training_extensions/datasets/open_images_v5_text) |  |
 |         FUNSD         |                          [homepage](https://guillaumejaume.github.io/FUNSD/)                          |                                                                                                                                                                                                           -                                                                                                                                                                                                           |                                                             -                                                             |  |
+|          NAF          |                           [homepage](https://github.com/herobd/NAF_dataset)                           |                                                                                                                                                                                                           -                                                                                                                                                                                                           |                                                             -                                                             |  |
 
 
 (*) Since the official homepage is unavailable now, we provide an alternative for quick reference. However, we do not guarantee the correctness of the dataset.
@@ -320,4 +328,31 @@ rm dataset.zip && rm -rf dataset
 
 ```bash
 python tools/data/textrecog/funsd_converter.py PATH/TO/funsd --nproc 4
+```
+
+### NAF
+
+- Step1: Download [labeled_images.tar.gz](https://github.com/herobd/NAF_dataset/releases/tag/v1.0) to `naf/`.
+
+```bash
+mkdir naf && cd naf
+
+# Download NAF dataset
+wget https://github.com/herobd/NAF_dataset/releases/download/v1.0/labeled_images.tar.gz
+tar -zxf labeled_images.tar.gz
+
+# For images
+mkdir annotations && mv labeled_images imgs
+
+# For annotations
+git clone https://github.com/herobd/NAF_dataset.git
+mv NAF_dataset/train_valid_test_split.json annotations/ && mv NAF_dataset/groups annotations/
+
+rm -rf NAF_dataset && rm labeled_images.tar.gz
+```
+
+- Step2: Generate `train_label.txt`, `val_label.txt`, and `test_label.txt` with following command:
+
+```bash
+python tools/data/textrecog/naf_converter.py PATH/TO/naf --nproc 4
 ```
--- tools/data/textrecog/naf_converter.py
+++ tools/data/textrecog/naf_converter.py
@@ -3,8 +3,10 @@ import argparse
 import os.path as osp
 
 import mmcv
+import numpy as np
 
-from mmocr.utils import convert_annotations
+from mmocr.datasets.pipelines.crop import crop_img
+from mmocr.utils.fileio import list_to_file
 
 
 def collect_files(img_dir, gt_dir, split_info):
@@ -92,14 +94,14 @@ def load_img_info(files):
         segm_file=osp.join(osp.basename(gt_file)))
 
     if osp.splitext(gt_file)[1] == '.json':
-        img_info = load_json_info(gt_file, img_info, img)
+        img_info = load_json_info(gt_file, img_info)
     else:
         raise NotImplementedError
 
     return img_info
 
 
-def load_json_info(gt_file, img_info, img):
+def load_json_info(gt_file, img_info):
     """Collect the annotation information.
 
     Args:
@@ -118,27 +120,26 @@ def load_json_info(gt_file, img_info, img):
     # 'textBBs' contains the printed texts of the table while 'fieldBBs'
     #  contains the text filled by human.
     for box_type in ['textBBs', 'fieldBBs']:
+        # NAF dataset only provides transcription GT for 'filedBBs', the
+        # 'textBBs' is only used for detection task.
+        if box_type == 'textBBs':
+            continue
         for anno in annotation[box_type]:
-            # Skip blanks
-            if box_type == 'fieldBBs':
-                if anno['type'] == 'blank':
-                    continue
+            # Skip images containing detection annotations only
+            if 'transcriptions' not in annotation.keys():
+                continue
+            # Skip boxes without recognition GT
+            if anno['id'] not in annotation['transcriptions'].keys():
+                continue
 
-            xs, ys = [], []
-            for p in anno['poly_points']:
-                xs.append(p[0])
-                ys.append(p[1])
-            x, y = max(0, min(xs)), max(0, min(ys))
-            w, h = max(xs) - x, max(ys) - y
-            bbox = [x, y, w, h]
-            segmentation = anno['poly_points']
+            word = annotation['transcriptions'][anno['id']]
+            # Skip blank boxes
+            if len(word) == 0:
+                continue
 
-            anno = dict(
-                iscrowd=0,
-                category_id=1,
-                bbox=bbox,
-                area=w * h,
-                segmentation=[segmentation])
+            bbox = np.array(anno['poly_points']).reshape(1, 8)[0].tolist()
+
+            anno = dict(bbox=bbox, word=word)
             anno_info.append(anno)
 
     img_info.update(anno_info=anno_info)
@@ -146,10 +147,74 @@ def load_json_info(gt_file, img_info, img):
     return img_info
 
 
+def generate_ann(root_path, split, image_infos, preserve_vertical,
+                 preserve_special_symbols):
+    """Generate cropped annotations and label txt file.
+
+    Args:
+        root_path (str): The root path of the dataset
+        split (str): The split of dataset. Namely: training or test
+        image_infos (list[dict]): A list of dicts of the img and
+            annotation information
+        preserve_vertical (bool): Whether to preserve vertical texts
+    """
+
+    dst_image_root = osp.join(root_path, 'dst_imgs', split)
+    if split == 'training':
+        dst_label_file = osp.join(root_path, 'train_label.txt')
+    elif split == 'val':
+        dst_label_file = osp.join(root_path, 'val_label.txt')
+    elif split == 'test':
+        dst_label_file = osp.join(root_path, 'test_label.txt')
+    else:
+        raise NotImplementedError
+    mmcv.mkdir_or_exist(dst_image_root)
+
+    lines = []
+    for image_info in image_infos:
+        index = 1
+        src_img_path = osp.join(root_path, 'imgs', image_info['file_name'])
+        image = mmcv.imread(src_img_path)
+        src_img_root = image_info['file_name'].split('.')[0]
+
+        for anno in image_info['anno_info']:
+            word = anno['word']
+            word = word.strip('\u202a')  # remove unicode control character
+            dst_img = crop_img(image, anno['bbox'])
+            h, w, _ = dst_img.shape
+
+            # Skip invalid annotations
+            if min(dst_img.shape) == 0:
+                continue
+            # Skip vertical texts
+            if not preserve_vertical and h / w > 2:
+                continue
+            # Skip non-ASCII characters
+            if not preserve_special_symbols and len(word) != len(
+                    word.encode()):
+                continue
+
+            dst_img_name = f'{src_img_root}_{index}.png'
+            index += 1
+            dst_img_path = osp.join(dst_image_root, dst_img_name)
+            mmcv.imwrite(dst_img, dst_img_path)
+            lines.append(f'{osp.basename(dst_image_root)}/{dst_img_name} '
+                         f'{word}')
+    list_to_file(dst_label_file, lines)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Generate training, val, and test set of NAF ')
     parser.add_argument('root_path', help='Root dir path of NAF')
+    parser.add_argument(
+        '--preserve-vertical',
+        help='Preserve samples containing vertical texts',
+        action='store_true')
+    parser.add_argument(
+        '--preserve-special-symbols',
+        help='Preserve non-ASCII characters such as tick and section sign',
+        action='store_true')
     parser.add_argument(
         '--nproc', default=1, type=int, help='Number of process')
     args = parser.parse_args()
@@ -170,9 +235,8 @@ def main():
                 osp.join(root_path, 'imgs'),
                 osp.join(root_path, 'annotations'), split_info[split])
             image_infos = collect_annotations(files, nproc=args.nproc)
-            convert_annotations(
-                image_infos, osp.join(root_path,
-                                      'instances_' + split + '.json'))
+            generate_ann(root_path, split, image_infos, args.preserve_vertical,
+                         args.preserve_special_symbols)
 
 
 if __name__ == '__main__':
