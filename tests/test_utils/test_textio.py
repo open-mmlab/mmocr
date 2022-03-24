7d3d2f438776551236511005c10c172911e28358
JiangQing <mountchicken@outlook.com>
2022-03-24 10:47:46 +0800

    Revert "[Feature] Support jsonl in recognition converter (#844)"
    This reverts commit 680dff373e9d473c42ac7c303cffb21a42dcc460.

--- tests/test_utils/test_textio.py
+++ tests/test_utils/test_textio.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import json
 import tempfile
 
 from mmocr.utils import list_from_file, list_to_file
@@ -18,46 +17,9 @@ lists = [
     ['選択', 'noël', 'Информацией', 'ÄÆä'],
 ]
 
-dicts = [
-    [{
-        'text': []
-    }],
-    [{
-        'text': [' ']
-    }],
-    [{
-        'text': ['\t']
-    }],
-    [{
-        'text': ['a']
-    }],
-    [{
-        'text': [1]
-    }],
-    [{
-        'text': [1.]
-    }],
-    [{
-        'text': ['a', 'b']
-    }],
-    [{
-        'text': ['a', 1, 1.]
-    }],
-    [{
-        'text': [1, 1., 'a']
-    }],
-    [{
-        'text': ['啊', '啊啊']
-    }],
-    [{
-        'text': ['選択', 'noël', 'Информацией', 'ÄÆä']
-    }],
-]
-
 
 def test_list_to_file():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        # test txt
         for i, lines in enumerate(lists):
             filename = f'{tmpdirname}/{i}.txt'
             list_to_file(filename, lines)
@@ -68,37 +30,19 @@ def test_list_to_file():
             lines = list(map(str, lines))
             assert len(lines) == len(lines2)
             assert all(line1 == line2 for line1, line2 in zip(lines, lines2))
-        # test jsonl
-        for i, lines in enumerate(dicts):
-            filename = f'{tmpdirname}/{i}.jsonl'
-            list_to_file(filename, [json.dumps(line) for line in lines])
-            lines2 = [
-                json.loads(line.rstrip('\r\n'))['text']
-                for line in open(filename, 'r', encoding='utf-8').readlines()
-            ][0]
-
-            lines = list(lines[0]['text'])
-            assert len(lines) == len(lines2)
-            assert all(line1 == line2 for line1, line2 in zip(lines, lines2))
 
 
 def test_list_from_file():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        # test txt file
-        for i, lines in enumerate(lists):
-            filename = f'{tmpdirname}/{i}.txt'
-            with open(filename, 'w', encoding='utf-8') as f:
-                f.writelines(f'{line}\n' for line in lines)
-            lines2 = list_from_file(filename, encoding='utf-8')
-            lines = list(map(str, lines))
-            assert len(lines) == len(lines2)
-            assert all(line1 == line2 for line1, line2 in zip(lines, lines2))
-        # test jsonl file
-        for i, lines in enumerate(dicts):
-            filename = f'{tmpdirname}/{i}.jsonl'
-            with open(filename, 'w', encoding='utf-8') as f:
-                f.writelines(f'{line}\n' for line in lines)
-            lines2 = list_from_file(filename, encoding='utf-8')
-            lines = list(map(str, lines))
-            assert len(lines) == len(lines2)
-            assert all(line1 == line2 for line1, line2 in zip(lines, lines2))
+        for encoding in ['utf-8', 'utf-8-sig']:
+            for i, lines in enumerate(lists):
+                filename = f'{tmpdirname}/{i}.txt'
+                with open(filename, 'w', encoding=encoding) as f:
+                    f.writelines(f'{line}\n' for line in lines)
+                lines2 = list_from_file(filename, encoding=encoding)
+                lines = list(map(str, lines))
+                print('lines:{}, lines2:{}, encoding:{}'.format(
+                    lines, lines2, encoding))
+                assert len(lines) == len(lines2)
+                assert all(line1 == line2
+                           for line1, line2 in zip(lines, lines2))
--- tools/data/textrecog/funsd_converter.py
+++ tools/data/textrecog/funsd_converter.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
-import json
 import math
 import os
 import os.path as osp
@@ -126,7 +125,7 @@ def load_json_info(gt_file, img_info):
     return img_info
 
 
-def generate_ann(root_path, split, image_infos, preserve_vertical, format):
+def generate_ann(root_path, split, image_infos, preserve_vertical):
     """Generate cropped annotations and label txt file.
 
     Args:
@@ -135,7 +134,6 @@ def generate_ann(root_path, split, image_infos, preserve_vertical, format):
         image_infos (list[dict]): A list of dicts of the img and
             annotation information
         preserve_vertical (bool): Whether to preserve vertical texts
-        format (str): Using jsonl(dict) or str to format annotations
     """
 
     dst_image_root = osp.join(root_path, 'dst_imgs', split)
@@ -168,20 +166,8 @@ def generate_ann(root_path, split, image_infos, preserve_vertical, format):
             index += 1
             dst_img_path = osp.join(dst_image_root, dst_img_name)
             mmcv.imwrite(dst_img, dst_img_path)
-            if format == 'txt':
-                lines.append(f'{osp.basename(dst_image_root)}/{dst_img_name} '
-                             f'{word}')
-            elif format == 'jsonl':
-                lines.append(
-                    json.dumps({
-                        'filename':
-                        f'{osp.basename(dst_image_root)}/{dst_img_name}',
-                        'text': word
-                    }),
-                    ensure_ascii=False)
-            else:
-                raise NotImplementedError
-
+            lines.append(f'{osp.basename(dst_image_root)}/{dst_img_name} '
+                         f'{word}')
     list_to_file(dst_label_file, lines)
 
 
@@ -190,16 +176,11 @@ def parse_args():
         description='Generate training and test set of FUNSD ')
     parser.add_argument('root_path', help='Root dir path of FUNSD')
     parser.add_argument(
-        '--preserve_vertical',
+        '--preserve-vertical',
         help='Preserve samples containing vertical texts',
         action='store_true')
     parser.add_argument(
         '--nproc', default=1, type=int, help='Number of processes')
-    parser.add_argument(
-        '--format',
-        default='jsonl',
-        help='Use jsonl or string to format annotations',
-        choices=['jsonl', 'txt'])
     args = parser.parse_args()
     return args
 
@@ -215,8 +196,7 @@ def main():
                 osp.join(root_path, 'imgs'),
                 osp.join(root_path, 'annotations', split))
             image_infos = collect_annotations(files, nproc=args.nproc)
-            generate_ann(root_path, split, image_infos, args.preserve_vertical,
-                         args.format)
+            generate_ann(root_path, split, image_infos, args.preserve_vertical)
 
 
 if __name__ == '__main__':
