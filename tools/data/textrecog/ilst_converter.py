387f56dd5062ae933bb70bf813668b36ce1b6bc4
Mountchicken <mountchicken@outlook.com>
2022-03-29 19:16:08 +0800

    set val-ratio to 0.

--- tools/data/textrecog/ilst_converter.py
+++ tools/data/textrecog/ilst_converter.py
@@ -242,7 +242,7 @@ def parse_args():
         help='Preserve samples containing vertical texts',
         action='store_true')
     parser.add_argument(
-        '--val-ratio', help='Split ratio for val set', default=0.2, type=float)
+        '--val-ratio', help='Split ratio for val set', default=0., type=float)
     parser.add_argument(
         '--nproc', default=1, type=int, help='Number of processes')
     parser.add_argument(
