49f815d369b8da2ae5f6ed35287f7e3436aeacb7
xinke-wang <wangxinyu2017@gmail.com>
2022-03-30 10:18:22 +0800

    fix det val default rate

--- tools/data/textdet/kaist_converter.py
+++ tools/data/textdet/kaist_converter.py
@@ -164,7 +164,7 @@ def parse_args():
         description='Generate training and val set of KAIST ')
     parser.add_argument('root_path', help='Root dir path of KAIST')
     parser.add_argument(
-        '--val-ratio', help='Split ratio for val set', default=0.2, type=float)
+        '--val-ratio', help='Split ratio for val set', default=0.0, type=float)
     parser.add_argument(
         '--nproc', default=1, type=int, help='Number of process')
     args = parser.parse_args()
