fc6e82467f2821afe869eb6e9d71007d833fce9c
Mountchicken <mountchicken@outlook.com>
2022-04-22 09:41:59 +0800

    [fix] remove remains in __init__

--- mmocr/models/textrecog/backbones/__init__.py
+++ mmocr/models/textrecog/backbones/__init__.py
@@ -3,11 +3,10 @@ from .nrtr_modality_transformer import NRTRModalityTransform
 from .resnet import ResNet
 from .resnet31_ocr import ResNet31OCR
 from .resnet_abi import ResNetABI
-from .resnet_master import ResNetMASTER
 from .shallow_cnn import ShallowCNN
 from .very_deep_vgg import VeryDeepVgg
 
 __all__ = [
     'ResNet31OCR', 'VeryDeepVgg', 'NRTRModalityTransform', 'ShallowCNN',
-    'ResNetABI', 'ResNetMASTER', 'ResNet'
+    'ResNetABI', 'ResNet'
 ]
--- mmocr/models/textrecog/convertors/__init__.py
+++ mmocr/models/textrecog/convertors/__init__.py
@@ -3,10 +3,9 @@ from .abi import ABIConvertor
 from .attn import AttnConvertor
 from .base import BaseConvertor
 from .ctc import CTCConvertor
-from .master import MasterConvertor
 from .seg import SegConvertor
 
 __all__ = [
     'BaseConvertor', 'CTCConvertor', 'AttnConvertor', 'SegConvertor',
-    'ABIConvertor', 'MasterConvertor'
+    'ABIConvertor'
 ]
