21ed7ee950a5f8d721195de54c3da6d2c53ced0e
Mountchicken <mountchicken@outlook.com>
2022-04-27 17:08:00 +0800

    reorganize and update

--- mmocr/utils/lmdb_util.py
+++ mmocr/utils/lmdb_util.py
@@ -1,21 +1,65 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import json
+import os
+import os.path as osp
 import shutil
 import sys
 import time
 from pathlib import Path
 
+import cv2
 import lmdb
+import numpy as np
 
 from mmocr.utils import list_from_file
 
 
-def lmdb_converter(img_list_file,
-                   output,
-                   batch_size=1000,
-                   coding='utf-8',
-                   lmdb_map_size=109951162776):
-    # read img_list_file
-    lines = list_from_file(img_list_file)
+def check_image_is_valid(imageBin):
+    if imageBin is None:
+        return False
+    imageBuf = np.frombuffer(imageBin, dtype=np.uint8)
+    img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
+    imgH, imgW = img.shape[0], img.shape[1]
+    if imgH * imgW == 0:
+        return False
+    return True
+
+
+def write_cache(env, cache):
+    with env.begin(write=True) as txn:
+        for k, v in cache.items():
+            txn.put(k, v)
+
+
+def parse_line(line, format):
+    if format == 'txt':
+        img_name, text = line.strip('\n').split(' ')
+    else:
+        line = json.loads(line)
+        img_name = line['filename']
+        text = line['text']
+    return [img_name, text]
+
+
+def label2lmdb(label_path,
+               label_format,
+               output,
+               batch_size=1000,
+               encoding='utf-8',
+               lmdb_map_size=109951162776):
+    """Create LMDB dataset with only labels for text recognition.
+
+    Args:
+        label_path (str): path to label file.
+        label_format (str): format of the label file, either txt or jsonl.
+        output (str): LMDB output path.
+        batch_size (int): Number of files written to the cache each time.
+        encoding (str): label encoding method.
+        lmdb_map_size (int): maximum size database may grow to.
+
+    """
+    # read labels
+    lines = list_from_file(label_path)
 
     # create lmdb database
     if Path(output).is_dir():
@@ -39,14 +83,102 @@ def lmdb_converter(img_list_file,
                          (beg_time, time.strftime('%H:%M:%S'), beg_index,
                           end_index, len(lines)))
         sys.stdout.flush()
-        batch = [(str(index).encode(coding), lines[index].encode(coding))
+        batch = [(str(index).encode(encoding),
+                  ' '.join(parse_line(lines[index],
+                                      label_format)).encode(encoding))
                  for index in range(beg_index, end_index)]
         with env.begin(write=True) as txn:
             cursor = txn.cursor()
             cursor.putmulti(batch, dupdata=False, overwrite=True)
     sys.stdout.write('\n')
     with env.begin(write=True) as txn:
-        key = 'total_number'.encode(coding)
-        value = str(len(lines)).encode(coding)
+        key = 'total_number'.encode(encoding)
+        value = str(len(lines)).encode(encoding)
         txn.put(key, value)
     print('done', flush=True)
+
+
+def img2lmdb(img_root,
+             label_path,
+             label_format,
+             output,
+             batch_size,
+             encoding,
+             lmdb_map_size,
+             verify=True):
+    """Create LMDB dataset with images and labels for text recognition. This
+    was partially adapted from https://github.com/clovaai/deep-text-
+    recognition-benchmark.
+
+    Args:
+        img_root (str): path to images.
+        label_path (str): path to label file.
+        label_format (str): format of the label file, either txt or jsonl.
+        output (str): LMDB output path.
+        batch_size (int): Number of files written to the cache each time.
+        encoding (str): label encoding method.
+        lmdb_map_size (int): maximum size database may grow to.
+        verify (bool): if true, check the validity of
+            every image.Defaults to True.
+
+    E.g.
+    This function supports MMOCR's recognition data format and the label file
+    can be txt or jsonl, as follows:
+
+        ├──img_root
+        |      |—— img1.jpg
+        |      |—— img2.jpg
+        |      |—— ...
+        |——label.txt (or label.jsonl)
+
+        label.txt: img1.jpg HELLO
+                   img2.jpg WORLD
+                   ...
+
+        label.jsonl: {'filename':'img1.jpg', 'text':'HELLO'}
+                     {'filename':'img2.jpg', 'text':'WORLD'}
+                     ...
+    """
+    # check label format
+    assert osp.basename(label_path).split('.')[-1] == label_format
+    # create lmdb env
+    os.makedirs(output, exist_ok=True)
+    env = lmdb.open(output, map_size=lmdb_map_size)
+    # load label file
+    with open(label_path, 'r', encoding=encoding) as f:
+        anno_list = f.readlines()
+
+    cache = {}
+    # index start from 1
+    cnt = 1
+    nSamples = len(anno_list)
+    for anno in anno_list:
+        img_name, text = parse_line(anno, label_format)
+        img_path = osp.join(img_root, img_name)
+        if not osp.exists(img_path):
+            print('%s does not exist' % img_path)
+            continue
+        with open(img_path, 'rb') as f:
+            imageBin = f.read()
+        if verify:
+            try:
+                if not check_image_is_valid(imageBin):
+                    print('%s is not a valid image' % img_path)
+                    continue
+            except Exception:
+                print('error occurred at ', img_name)
+
+        imageKey = 'image-%09d'.encode(encoding) % cnt
+        labelKey = 'label-%09d'.encode(encoding) % cnt
+        cache[imageKey] = imageBin
+        cache[labelKey] = text.encode(encoding)
+
+        if cnt % batch_size == 0:
+            write_cache(env, cache)
+            cache = {}
+            print('Written %d / %d' % (cnt, nSamples))
+        cnt += 1
+    nSamples = cnt - 1
+    cache['total_number'.encode(encoding)] = str(nSamples).encode(encoding)
+    write_cache(env, cache)
+    print('Created lmdb dataset with %d samples' % nSamples)
---
+++ tools/data/utils/lmdb_converter.py
@@ -0,0 +1,59 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import argparse
+
+from mmocr.utils import img2lmdb, label2lmdb
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img-root', '-i', help='input imglist path')
+    parser.add_argument(
+        '--label-path', '-l', required=True, help='Path to label file')
+    parser.add_argument(
+        '--label-format',
+        '-f',
+        default='txt',
+        choices=['txt', 'jsonl'],
+        help='The format of the label file, either txt or jsonl')
+    parser.add_argument(
+        '--output', '-o', required=True, help='output lmdb path')
+    parser.add_argument(
+        '--batch_size',
+        '-b',
+        type=int,
+        default=10000,
+        help='processing batch size, default 10000')
+    parser.add_argument(
+        '--encoding',
+        '-c',
+        default='utf8',
+        help='bytes coding scheme, default utf8')
+    parser.add_argument(
+        '--lmdb_map_size',
+        '-m',
+        type=int,
+        default=109951162776,
+        help='maximum size database may grow to , default 109951162776 bytes')
+    opt = parser.parse_args()
+
+    if opt.img_root:
+        img2lmdb(
+            opt.img_root,
+            opt.label_path,
+            opt.label_format,
+            opt.output,
+            batch_size=opt.batch_size,
+            encoding=opt.encoding,
+            lmdb_map_size=opt.lmdb_map_size)
+    else:
+        label2lmdb(
+            opt.label_path,
+            opt.label_format,
+            opt.output,
+            batch_size=opt.batch_size,
+            encoding=opt.encoding,
+            lmdb_map_size=opt.lmdb_map_size)
+
+
+if __name__ == '__main__':
+    main()
