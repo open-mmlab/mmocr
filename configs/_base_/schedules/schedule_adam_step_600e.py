798614aad0fd47c908255b26e7e068e10defca8a
Xinyu Wang <45810070+xinke-wang@users.noreply.github.com>
2022-04-27 12:53:57 +0800

    [Enhancement] Update Dataset Configs (#980)
    * update runner in configs
    
    * update AnnFileLoader

--- configs/_base_/schedules/schedule_adam_step_600e.py
+++ configs/_base_/schedules/schedule_adam_step_600e.py
@@ -3,4 +3,4 @@ optimizer = dict(type='Adam', lr=1e-4)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=[200, 400])
-total_epochs = 600
+runner = dict(type='EpochBasedRunner', max_epochs=600)
