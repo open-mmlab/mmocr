bc1b5ccad9d241892bc30282e3a8f445b019cc14
xinke-wang <wangxinyu2017@gmail.com>
2022-05-05 12:14:30 +0800

    update config

--- configs/textdet/dbnet/dbnetpp_r50dcnv2_fpnc_100k_iter_synthtext.py
+++ configs/textdet/dbnet/dbnetpp_r50dcnv2_fpnc_100k_iter_synthtext.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../_base_/runtime_10k.py',
+    '../../_base_/default_runtime.py',
     '../../_base_/schedules/schedule_sgd_100k_iters.py',
     '../../_base_/det_models/dbnetpp_r50dcnv2_fpnc.py',
     '../../_base_/det_datasets/synthtext.py',
