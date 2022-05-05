5ca4211c6426614d7c833a0c109769b154d935af
xinke-wang <wangxinyu2017@gmail.com>
2022-05-05 09:19:45 +0800

    add doc; remove configs

---
+++ configs/textdet/dbnet/dbnetpp_r50dcnv2_fpnc_100k_iter_synthtext.py
@@ -0,0 +1,33 @@
+_base_ = [
+    '../../_base_/runtime_10k.py',
+    '../../_base_/schedules/schedule_sgd_100k_iters.py',
+    '../../_base_/det_models/dbnetpp_r50dcnv2_fpnc.py',
+    '../../_base_/det_datasets/synthtext.py',
+    '../../_base_/det_pipelines/dbnet_pipeline.py'
+]
+
+train_list = {{_base_.train_list}}
+test_list = {{_base_.test_list}}
+
+train_pipeline_r50dcnv2 = {{_base_.train_pipeline_r50dcnv2}}
+test_pipeline_4068_1024 = {{_base_.test_pipeline_4068_1024}}
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=8,
+    val_dataloader=dict(samples_per_gpu=1),
+    test_dataloader=dict(samples_per_gpu=1),
+    train=dict(
+        type='UniformConcatDataset',
+        datasets=train_list,
+        pipeline=train_pipeline_r50dcnv2),
+    val=dict(
+        type='UniformConcatDataset',
+        datasets=test_list,
+        pipeline=test_pipeline_4068_1024),
+    test=dict(
+        type='UniformConcatDataset',
+        datasets=test_list,
+        pipeline=test_pipeline_4068_1024))
+
+evaluation = dict(interval=200000, metric='hmean-iou')  # do not evaluate
--- configs/textdet/dbnet/dbnetpp_r50dcnv2_fpnc_1200e_icdar2015.py
+++ configs/textdet/dbnet/dbnetpp_r50dcnv2_fpnc_1200e_icdar2015.py
@@ -1,7 +1,7 @@
 _base_ = [
-    '../../_base_/runtime_10e.py',
+    '../../_base_/runtime_100e.py',
     '../../_base_/schedules/schedule_sgd_1200e.py',
-    '../../_base_/det_models/dbnetpp_r18_fpnc.py',
+    '../../_base_/det_models/dbnetpp_r50dcnv2_fpnc.py',
     '../../_base_/det_datasets/icdar2015.py',
     '../../_base_/det_pipelines/dbnet_pipeline.py'
 ]
@@ -9,30 +9,31 @@ _base_ = [
 train_list = {{_base_.train_list}}
 test_list = {{_base_.test_list}}
 
-train_pipeline_r18 = {{_base_.train_pipeline_r18}}
-test_pipeline_1333_736 = {{_base_.test_pipeline_1333_736}}
+train_pipeline_r50dcnv2 = {{_base_.train_pipeline_r50dcnv2}}
+test_pipeline_4068_1024 = {{_base_.test_pipeline_4068_1024}}
+
+load_from = 'checkpoints/textdet/dbnetpp/res50dcnv2_synthtext.pth'
 
 data = dict(
-    samples_per_gpu=16,
+    samples_per_gpu=32,
     workers_per_gpu=8,
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1),
     train=dict(
         type='UniformConcatDataset',
         datasets=train_list,
-        pipeline=train_pipeline_r18),
+        pipeline=train_pipeline_r50dcnv2),
     val=dict(
         type='UniformConcatDataset',
         datasets=test_list,
-        pipeline=test_pipeline_1333_736),
+        pipeline=test_pipeline_4068_1024),
     test=dict(
         type='UniformConcatDataset',
         datasets=test_list,
-        pipeline=test_pipeline_1333_736))
+        pipeline=test_pipeline_4068_1024))
 
 evaluation = dict(
     interval=100,
     metric='hmean-iou',
-    min_score_thr=0.1,
-    max_score_thr=0.5,
-    step=0.1)
+    save_best='0_hmean-iou:hmean',
+    rule='greater')
