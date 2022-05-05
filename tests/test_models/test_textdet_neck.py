fbc138d2d316f4f695b5dc66bb471e4ac5ba0b45
Xinyu Wang <45810070+xinke-wang@users.noreply.github.com>
2022-05-05 14:39:20 +0800

    [Feature] Add DBNet++ (#973)
    * add dbnet++
    
    * fix docstring
    
    * fix some param names
    
    * fix
    
    * fix docstring
    
    * add init
    
    * add doc; remove configs
    
    * add dbnet++ to readme
    
    * fix readme
    
    * update config
    
    * update readme
    
    * update readme
    
    * update ocr.py
    
    * update metafile.yml
    
    * update readme
    
    * update readme
    
    * move to dbnetpp
    
    * fix paths
    
    * fix head level
    
    * fix typo
    
    * update demo.md
    
    * Update configs/textdet/dbnetpp/README.md
    
    Co-authored-by: Tong Gao <gaotongxiao@gmail.com>
    
    * fix typo
    
    * fix link

--- tests/test_models/test_textdet_neck.py
+++ tests/test_models/test_textdet_neck.py
@@ -9,14 +9,20 @@ def test_fpnc():
 
     in_channels = [64, 128, 256, 512]
     size = [112, 56, 28, 14]
+    asf_cfgs = [
+        None,
+        dict(attention_type='ScaleChannelSpatial'),
+    ]
     for flag in [False, True]:
-        fpnc = FPNC(
-            in_channels=in_channels,
-            bias_on_lateral=flag,
-            bn_re_on_lateral=flag,
-            bias_on_smooth=flag,
-            bn_re_on_smooth=flag,
-            conv_after_concat=flag)
+        for asf_cfg in asf_cfgs:
+            fpnc = FPNC(
+                in_channels=in_channels,
+                bias_on_lateral=flag,
+                bn_re_on_lateral=flag,
+                bias_on_smooth=flag,
+                bn_re_on_smooth=flag,
+                asf_cfg=asf_cfg,
+                conv_after_concat=flag)
         fpnc.init_weights()
         inputs = []
         for i in range(4):
