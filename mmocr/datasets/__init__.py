d57ac69d29d544d466519618eaa0cb7ec6b6635d
gaotongxiao <gaotongxiao@gmail.com>
2022-05-11 14:33:29 +0800

    remove some deprecated files causing import errors

--- mmocr/datasets/__init__.py
+++ mmocr/datasets/__init__.py
@@ -1,23 +1,24 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from . import utils
 from .base_dataset import BaseDataset
-from .builder import DATASETS, LOADERS, PARSERS, TRANSFORMS
+from .builder import LOADERS, PARSERS, TRANSFORMS
 from .icdar_dataset import IcdarDataset
+# TODO: check this
 from .kie_dataset import KIEDataset
 from .ner_dataset import NerDataset
 from .ocr_dataset import OCRDataset
 from .ocr_seg_dataset import OCRSegDataset
 from .openset_kie_dataset import OpensetKIEDataset
-from .pipelines import CustomFormatBundle, DBNetTargets, FCENetTargets
+from .pipelines import DBNetTargets, FCENetTargets
 from .text_det_dataset import TextDetDataset
 from .uniform_concat_dataset import UniformConcatDataset
 from .utils import *  # NOQA
 
 __all__ = [
-    'DATASETS', 'IcdarDataset', 'BaseDataset', 'OCRDataset', 'TextDetDataset',
-    'CustomFormatBundle', 'DBNetTargets', 'OCRSegDataset', 'KIEDataset',
-    'FCENetTargets', 'NerDataset', 'UniformConcatDataset', 'OpensetKIEDataset',
-    'TRANSFORMS', 'PARSERS', 'LOADERS'
+    'IcdarDataset', 'BaseDataset', 'OCRDataset', 'TextDetDataset',
+    'KIEDataset', 'DBNetTargets', 'OCRSegDataset', 'FCENetTargets',
+    'NerDataset', 'UniformConcatDataset', 'OpensetKIEDataset', 'TRANSFORMS',
+    'PARSERS', 'LOADERS'
 ]
 
 __all__ += utils.__all__
--- mmocr/datasets/pipelines/kie_transforms.py
+++ mmocr/datasets/pipelines/kie_transforms.py
@@ -1,8 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
 from mmcv import rescale_size
-from mmcv.parallel import DataContainer as DC
-from mmdet.datasets.pipelines.formatting import DefaultFormatBundle, to_tensor
 
 from mmocr.registry import TRANSFORMS
 
@@ -40,52 +38,53 @@ class ResizeNoImg:
         return results
 
 
-@TRANSFORMS.register_module()
-class KIEFormatBundle(DefaultFormatBundle):
-    """Key information extraction formatting bundle.
+# @TRANSFORMS.register_module()
+# class KIEFormatBundle(DefaultFormatBundle):
+#     """Key information extraction formatting bundle.
 
-    Based on the DefaultFormatBundle, itt simplifies the pipeline of formatting
-    common fields, including "img", "proposals", "gt_bboxes", "gt_labels",
-    "gt_masks", "gt_semantic_seg", "relations" and "texts".
-    These fields are formatted as follows.
+#     Based on the DefaultFormatBundle, itt simplifies the pipeline of
+#     formatting
+#     common fields, including "img", "proposals", "gt_bboxes", "gt_labels",
+#     "gt_masks", "gt_semantic_seg", "relations" and "texts".
+#     These fields are formatted as follows.
 
-    - img: (1) transpose, (2) to tensor, (3) to DataContainer (stack=True)
-    - proposals: (1) to tensor, (2) to DataContainer
-    - gt_bboxes: (1) to tensor, (2) to DataContainer
-    - gt_bboxes_ignore: (1) to tensor, (2) to DataContainer
-    - gt_labels: (1) to tensor, (2) to DataContainer
-    - gt_masks: (1) to tensor, (2) to DataContainer (cpu_only=True)
-    - gt_semantic_seg: (1) unsqueeze dim-0 (2) to tensor,
-                       (3) to DataContainer (stack=True)
-    - relations: (1) scale, (2) to tensor, (3) to DataContainer
-    - texts: (1) to tensor, (2) to DataContainer
-    """
+#     - img: (1) transpose, (2) to tensor, (3) to DataContainer (stack=True)
+#     - proposals: (1) to tensor, (2) to DataContainer
+#     - gt_bboxes: (1) to tensor, (2) to DataContainer
+#     - gt_bboxes_ignore: (1) to tensor, (2) to DataContainer
+#     - gt_labels: (1) to tensor, (2) to DataContainer
+#     - gt_masks: (1) to tensor, (2) to DataContainer (cpu_only=True)
+#     - gt_semantic_seg: (1) unsqueeze dim-0 (2) to tensor,
+#                        (3) to DataContainer (stack=True)
+#     - relations: (1) scale, (2) to tensor, (3) to DataContainer
+#     - texts: (1) to tensor, (2) to DataContainer
+#     """
 
-    def __call__(self, results):
-        """Call function to transform and format common fields in results.
+#     def __call__(self, results):
+#         """Call function to transform and format common fields in results.
 
-        Args:
-            results (dict): Result dict contains the data to convert.
+#         Args:
+#             results (dict): Result dict contains the data to convert.
 
-        Returns:
-            dict: The result dict contains the data that is formatted with
-                default bundle.
-        """
-        super().__call__(results)
-        if 'ann_info' in results:
-            for key in ['relations', 'texts']:
-                value = results['ann_info'][key]
-                if key == 'relations' and 'scale_factor' in results:
-                    scale_factor = results['scale_factor']
-                    if isinstance(scale_factor, float):
-                        sx = sy = scale_factor
-                    else:
-                        sx, sy = results['scale_factor'][:2]
-                    r = sx / sy
-                    factor = np.array([sx, sy, r, 1, r]).astype(np.float32)
-                    value = value * factor[None, None]
-                results[key] = DC(to_tensor(value))
-        return results
+#         Returns:
+#             dict: The result dict contains the data that is formatted with
+#                 default bundle.
+#         """
+#         super().__call__(results)
+#         if 'ann_info' in results:
+#             for key in ['relations', 'texts']:
+#                 value = results['ann_info'][key]
+#                 if key == 'relations' and 'scale_factor' in results:
+#                     scale_factor = results['scale_factor']
+#                     if isinstance(scale_factor, float):
+#                         sx = sy = scale_factor
+#                     else:
+#                         sx, sy = results['scale_factor'][:2]
+#                     r = sx / sy
+#                     factor = np.array([sx, sy, r, 1, r]).astype(np.float32)
+#                     value = value * factor[None, None]
+#                 results[key] = DC(to_tensor(value))
+#         return results
 
-    def __repr__(self):
-        return self.__class__.__name__
+#     def __repr__(self):
+#         return self.__class__.__name__
