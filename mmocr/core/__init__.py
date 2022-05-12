795368c4aab081721487f09e583355d337b26e46
liukuikun <641417025@qq.com>
2022-05-12 11:55:43 +0000

    [Feature] TextDetSample

--- mmocr/core/__init__.py
+++ mmocr/core/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from . import evaluation
+from .data_structures import TextDetDataSample
 from .evaluation import *  # NOQA
 from .mask import extract_boundary, points2boundary, seg2boundary
 from .visualize import (det_recog_show_result, imshow_edge, imshow_node,
@@ -11,6 +12,6 @@ __all__ = [
     'points2boundary', 'seg2boundary', 'extract_boundary', 'overlay_mask_img',
     'show_feature', 'show_img_boundary', 'show_pred_gt',
     'imshow_pred_boundary', 'imshow_text_char_boundary', 'imshow_text_label',
-    'imshow_node', 'det_recog_show_result', 'imshow_edge'
+    'imshow_node', 'det_recog_show_result', 'imshow_edge', 'TextDetDataSample'
 ]
 __all__ += evaluation.__all__
---
+++ mmocr/core/data_structures/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .textdet_data_sample import TextDetDataSample
+
+__all__ = ['TextDetDataSample']
