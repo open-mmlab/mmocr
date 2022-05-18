6fe4ee82f2755eedffbbab20f3723556990f7ce5
liukuikun <641417025@qq.com>
2022-05-18 09:58:43 +0800

    [Refactor] text detection and text recognition dataset

--- mmocr/datasets/ocr_dataset.py
+++ mmocr/datasets/ocr_dataset.py
@@ -1,38 +1,94 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmocr.core.evaluation.ocr_metric import eval_ocr_metric
-from mmocr.datasets.base_dataset import BaseDataset
+from mmengine.dataset import BaseDataset
+
 from mmocr.registry import DATASETS
-from mmocr.utils import is_type_list
 
 
 @DATASETS.register_module()
 class OCRDataset(BaseDataset):
+    r"""OCRDataset for text detection and text recognition.
 
-    def pre_pipeline(self, results):
-        results['img_prefix'] = self.img_prefix
-        results['text'] = results['img_info']['text']
+    The annotation format is shown as follows.
 
-    def evaluate(self, results, metric='acc', logger=None, **kwargs):
-        """Evaluate the dataset.
+    .. code-block:: none
 
-        Args:
-            results (list): Testing results of the dataset.
-            metric (str | list[str]): Metrics to be evaluated.
-            logger (logging.Logger | str | None): Logger used for printing
-                related information during evaluation. Default: None.
-        Returns:
-            dict[str: float]
-        """
-        assert isinstance(metric, str) or is_type_list(metric, str)
+        {
+            "metainfo":
+            {
+              "dataset_type": "test_dataset",
+              "task_name": "test_task"
+            },
+            "data_list":
+            [
+              {
+                "img_path": "test_img.jpg",
+                "height": 604,
+                "width": 640,
+                "instances":
+                [
+                  {
+                    "bbox": [0, 0, 10, 20],
+                    "bbox_label": 1,
+                    "mask": [0,0,0,10,10,20,20,0],
+                    "text": '123'
+                  },
+                  {
+                    "bbox": [10, 10, 110, 120],
+                    "bbox_label": 2,
+                    "mask": [10,10],10,110,110,120,120,10]],
+                    "extra_anns": '456'
+                  }
+                ]
+              },
+            ]
+        }
 
-        gt_texts = []
-        pred_texts = []
-        for i in range(len(self)):
-            item_info = self.data_infos[i]
-            text = item_info['text']
-            gt_texts.append(text)
-            pred_texts.append(results[i]['text'])
+    Args:
+        ann_file (str): Annotation file path. Defaults to ''.
+        metainfo (dict, optional): Meta information for dataset, such as class
+            information. Defaults to None.
+        data_root (str, optional): The root directory for ``data_prefix`` and
+            ``ann_file``. Defaults to None.
+        data_prefix (dict, optional): Prefix for training data. Defaults to
+            dict(img=None, ann=None).
+        filter_cfg (dict, optional): Config for filter data. Defaults to None.
+        indices (int or Sequence[int], optional): Support using first few
+            data in annotation file to facilitate training/testing on a smaller
+            dataset. Defaults to None which means using all ``data_infos``.
+        serialize_data (bool, optional): Whether to hold memory using
+            serialized objects, when enabled, data loader workers can use
+            shared RAM from master process instead of making a copy. Defaults
+            to True.
+        pipeline (list, optional): Processing pipeline. Defaults to [].
+        test_mode (bool, optional): ``test_mode=True`` means in test phase.
+            Defaults to False.
+        lazy_init (bool, optional): Whether to load annotation during
+            instantiation. In some cases, such as visualization, only the meta
+            information of the dataset is needed, which is not necessary to
+            load annotation file. ``OCRdataset`` can skip load annotations to
+            save time by set ``lazy_init=False``. Defaults to False.
+        max_refetch (int, optional): If ``OCRdataset.prepare_data`` get a
+            None img. The maximum extra number of cycles to get a valid
+            image. Defaults to 1000.
 
-        eval_results = eval_ocr_metric(pred_texts, gt_texts, metric=metric)
+    Note:
+        OCRDataset collects meta information from `annotation file` (the
+        lowest priority), ``OCRDataset.METAINFO``(medium) and `metainfo
+        parameter` (highest) passed to constructors. The lower priority meta
+        information will be overwritten by higher one.
 
-        return eval_results
+    Examples:
+        Assume the annotation file is given above.
+        >>> class CustomDataset(OCRDataset):
+        >>>     METAINFO: dict = dict(task_name='custom_task',
+        >>>                           dataset_type='custom_type')
+        >>> metainfo=dict(task_name='custom_task_name')
+        >>> custom_dataset = CustomDataset(
+        >>>                      'path/to/ann_file',
+        >>>                      metainfo=metainfo)
+        >>> # meta information of annotation file will be overwritten by
+        >>> # `CustomDataset.METAINFO`. The merged meta information will
+        >>> # further be overwritten by argument `metainfo`.
+        >>> custom_dataset.metainfo
+        {'task_name': custom_task_name, dataset_type: custom_type}
+    """
