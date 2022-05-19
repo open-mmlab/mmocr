6d1e1ff3d483a36d341af7257de85a126696be94
wangxinyu <wangxinyu2017@gmail.com>
2022-05-19 08:09:16 +0000

    [Refactor] RandomCropFlip

--- mmocr/datasets/pipelines/__init__.py
+++ mmocr/datasets/pipelines/__init__.py
@@ -9,12 +9,13 @@ from .ocr_seg_targets import OCRSegTargets
 from .ocr_transforms import (FancyPCA, NormalizeOCR, OnlineCropOCR,
                              OpencvToPil, PilToOpencv, RandomPaddingOCR,
                              RandomRotateImageBox, ResizeOCR, ToTensorOCR)
-from .processing import PyramidRescale, RandomRotate, Resize
+from .processing import (PyramidRescale, RandomRotate, Resize,
+                         TextDetRandomCropFlip)
 from .test_time_aug import MultiRotateAugOCR
 from .textdet_targets import (DBNetTargets, FCENetTargets, PANetTargets,
                               TextSnakeTargets)
 from .transform_wrappers import OneOfWrapper, RandomWrapper, TorchVisionWrapper
-from .transforms import (ColorJitter, RandomCropFlip, RandomCropInstances,
+from .transforms import (ColorJitter, RandomCropInstances,
                          RandomCropPolyInstances, RandomScaling,
                          ScaleAspectJitter, SquareResizePad)
 from .wrappers import ImgAug
@@ -27,7 +28,7 @@ __all__ = [
     'RandomCropPolyInstances', 'RandomPaddingOCR', 'ImgAug', 'EastRandomCrop',
     'RandomRotateImageBox', 'OpencvToPil', 'PilToOpencv', 'SquareResizePad',
     'TextSnakeTargets', 'sort_vertex', 'LoadImageFromNdarray', 'sort_vertex8',
-    'FCENetTargets', 'RandomScaling', 'RandomCropFlip', 'NerTransform',
+    'FCENetTargets', 'RandomScaling', 'TextDetRandomCropFlip', 'NerTransform',
     'ToTensorNER', 'ResizeNoImg', 'PyramidRescale', 'OneOfWrapper',
     'RandomWrapper', 'TorchVisionWrapper', 'LoadImageFromLMDB', 'Resize'
 ]
