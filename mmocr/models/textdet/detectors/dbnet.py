d34fad1451b9a8452efd8ea7141ef034db15b40b
gaotongxiao <gaotongxiao@gmail.com>
2022-05-25 17:53:00 +0800

    add db

--- mmocr/models/textdet/detectors/dbnet.py
+++ mmocr/models/textdet/detectors/dbnet.py
@@ -1,27 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmocr.registry import MODELS
 from .single_stage_text_detector import SingleStageTextDetector
-from .text_detector_mixin import TextDetectorMixin
 
 
 @MODELS.register_module()
-class DBNet(TextDetectorMixin, SingleStageTextDetector):
+class DBNet(SingleStageTextDetector):
     """The class for implementing DBNet text detector: Real-time Scene Text
     Detection with Differentiable Binarization.
 
     [https://arxiv.org/abs/1911.08947].
     """
-
-    def __init__(self,
-                 backbone,
-                 neck,
-                 bbox_head,
-                 train_cfg=None,
-                 test_cfg=None,
-                 pretrained=None,
-                 show_score=False,
-                 init_cfg=None):
-        SingleStageTextDetector.__init__(self, backbone, neck, bbox_head,
-                                         train_cfg, test_cfg, pretrained,
-                                         init_cfg)
-        TextDetectorMixin.__init__(self, show_score)
