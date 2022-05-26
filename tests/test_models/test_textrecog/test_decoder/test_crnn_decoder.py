8e831b699c59c00a89a1ff4a3fb8cefc8e365821
liukuikun <641417025@qq.com>
2022-05-26 15:45:09 +0000

    [Refactor] crnn decoder

---
+++ tests/test_models/test_textrecog/test_decoder/test_crnn_decoder.py
@@ -0,0 +1,63 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+import tempfile
+from unittest import TestCase
+
+import torch
+import torch.nn as nn
+
+from mmocr.models.textrecog.decoders import CRNNDecoder
+
+
+class TestCRNNDecoder(TestCase):
+
+    def _create_dummy_dict_file(
+        self, dict_file,
+        chars=list('0123456789abcdefghijklmnopqrstuvwxyz')):  # NOQA
+        with open(dict_file, 'w') as f:
+            for char in chars:
+                f.write(char + '\n')
+
+    def test_init(self):
+        tmp_dir = tempfile.TemporaryDirectory()
+        dict_file = osp.join(tmp_dir.name, 'fake_chars.txt')
+        self._create_dummy_dict_file(dict_file)
+        # test diction cfg
+        dict_cfg = dict(
+            type='Dictionary',
+            dict_file=dict_file,
+            with_start=True,
+            with_end=True,
+            same_start_end=False,
+            with_padding=True,
+            with_unknown=True)
+        # test rnn flag false
+        decoder = CRNNDecoder(in_channels=3, dictionary=dict_cfg)
+        self.assertIsInstance(decoder.decoder, nn.Conv2d)
+
+        decoder = CRNNDecoder(
+            in_channels=3, dictionary=dict_cfg, rnn_flag=True)
+        self.assertIsInstance(decoder.decoder, nn.Sequential)
+        tmp_dir.cleanup()
+
+    def test_forward(self):
+        inputs = torch.randn(3, 10, 1, 100)
+        tmp_dir = tempfile.TemporaryDirectory()
+        dict_file = osp.join(tmp_dir.name, 'fake_chars.txt')
+        self._create_dummy_dict_file(dict_file)
+        # test diction cfg
+        dict_cfg = dict(
+            type='Dictionary',
+            dict_file=dict_file,
+            with_start=False,
+            with_end=False,
+            same_start_end=False,
+            with_padding=True,
+            with_unknown=False)
+        decoder = CRNNDecoder(in_channels=10, dictionary=dict_cfg)
+        output = decoder(inputs, train_mode=True)
+        self.assertTupleEqual(tuple(output.shape), (3, 100, 37))
+        decoder = CRNNDecoder(
+            in_channels=10, dictionary=dict_cfg, rnn_flag=True)
+        output = decoder(inputs, train_mode=False)
+        self.assertTupleEqual(tuple(output.shape), (3, 100, 37))
