39254de4eb77e7ca37d07bb0e0b8116a1a54de6f
gaotongxiao <gaotongxiao@gmail.com>
2022-05-26 01:56:37 +0000

    New Hmean-iou metric

---
+++ mmocr/metrics/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .hmean_iou_metric import HmeanIOUMetric
+
+__all__ = ['HmeanIOUMetric']
--- mmocr/utils/__init__.py
+++ mmocr/utils/__init__.py
@@ -15,6 +15,8 @@ from .lmdb_util import recog2lmdb
 from .logger import get_root_logger
 from .model import revert_sync_batchnorm
 from .polygon_utils import (crop_polygon, is_poly_outside_rect, poly2bbox,
+                            poly2shapely, poly_intersection, poly_iou,
+                            poly_make_valid, poly_union, polys2shapely,
                             rescale_polygon, rescale_polygons)
 from .setup_env import setup_multi_processes
 from .string_util import StringStrip
@@ -22,12 +24,12 @@ from .string_util import StringStrip
 __all__ = [
     'Registry', 'build_from_cfg', 'get_root_logger', 'collect_env',
     'is_3dlist', 'is_type_list', 'is_none_or_type', 'equal_len', 'is_2dlist',
-    'valid_boundary', 'lmdb_converter', 'drop_orientation',
-    'convert_annotations', 'is_not_png', 'list_to_file', 'list_from_file',
-    'is_on_same_line', 'stitch_boxes_into_lines', 'StringStrip',
-    'revert_sync_batchnorm', 'bezier_to_polygon', 'sort_points',
-    'setup_multi_processes', 'recog2lmdb', 'dump_ocr_data',
-    'recog_anno_to_imginfo', 'rescale_polygons', 'rescale_polygon',
-    'rescale_bboxes', 'bbox2poly', 'crop_polygon', 'is_poly_outside_rect',
-    'poly2bbox'
+    'valid_boundary', 'drop_orientation', 'convert_annotations', 'is_not_png',
+    'list_to_file', 'list_from_file', 'is_on_same_line',
+    'stitch_boxes_into_lines', 'StringStrip', 'revert_sync_batchnorm',
+    'bezier_to_polygon', 'sort_points', 'setup_multi_processes', 'recog2lmdb',
+    'dump_ocr_data', 'recog_anno_to_imginfo', 'rescale_polygons',
+    'rescale_polygon', 'rescale_bboxes', 'bbox2poly', 'crop_polygon',
+    'is_poly_outside_rect', 'poly2bbox', 'poly_intersection', 'poly_iou',
+    'poly_make_valid', 'poly_union', 'poly2shapely', 'polys2shapely'
 ]
