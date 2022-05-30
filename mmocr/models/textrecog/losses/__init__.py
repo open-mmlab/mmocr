ba9df19c4d597a3a94013906e1e498712c3a4d95
liukuikun <641417025@qq.com>
2022-05-30 09:38:31 +0000

    [Refactor]CELoss

--- mmocr/models/textrecog/losses/__init__.py
+++ mmocr/models/textrecog/losses/__init__.py
@@ -1,11 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base_recog_loss import BaseRecogLoss
-from .ce_loss import CELoss, SARLoss, TFLoss
+from .ce_loss import CELoss
 from .ctc_loss import CTCLoss
 from .mix_loss import ABILoss
 from .seg_loss import SegLoss
 
-__all__ = [
-    'BaseRecogLoss', 'CELoss', 'SARLoss', 'CTCLoss', 'TFLoss', 'SegLoss',
-    'ABILoss'
-]
+__all__ = ['BaseRecogLoss', 'CELoss', 'CTCLoss', 'SegLoss', 'ABILoss']
