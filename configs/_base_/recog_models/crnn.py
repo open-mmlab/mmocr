f1eebe9e344f84a3bed63183d97202e112b6f678
liukuikun <641417025@qq.com>
2022-05-30 10:58:53 +0000

    [CRNN] CRNN config

--- configs/_base_/recog_models/crnn.py
+++ configs/_base_/recog_models/crnn.py
@@ -1,12 +1,18 @@
-label_convertor = dict(
-    type='CTCConvertor', dict_type='DICT36', with_unknown=False, lower=True)
+dictionary = dict(
+    type='Dictionary',
+    dict_file='dicts/lower_english_digits.txt',
+    with_padding=True)
 
 model = dict(
     type='CRNNNet',
     preprocessor=None,
     backbone=dict(type='VeryDeepVgg', leaky_relu=False, input_channels=1),
     encoder=None,
-    decoder=dict(type='CRNNDecoder', in_channels=512, rnn_flag=True),
-    loss=dict(type='CTCLoss'),
-    label_convertor=label_convertor,
-    pretrained=None)
+    decoder=dict(
+        type='CRNNDecoder',
+        in_channels=512,
+        rnn_flag=True,
+        loss=dict(type='CTCLoss', letter_case='lower'),
+        postprocessor=dict(type='CTCPostProcessor')),
+    dictionary=dictionary,
+    preprocess_cfg=dict(mean=[127], std=[127]))
