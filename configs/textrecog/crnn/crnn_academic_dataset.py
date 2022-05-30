18bcc68427212be62248b37f5d4d7bdb774296f6
liukuikun <641417025@qq.com>
2022-05-30 10:58:53 +0000

    [CRNN] CRNN config

--- configs/textrecog/crnn/crnn_academic_dataset.py
+++ configs/textrecog/crnn/crnn_academic_dataset.py
@@ -1,35 +1,78 @@
+# training schedule for 1x
 _base_ = [
-    '../../_base_/default_runtime.py', '../../_base_/recog_models/crnn.py',
-    '../../_base_/recog_pipelines/crnn_pipeline.py',
-    '../../_base_/recog_datasets/MJ_train.py',
-    '../../_base_/recog_datasets/academic_test.py',
-    '../../_base_/schedules/schedule_adadelta_5e.py'
+    '../../_base_/default_runtime.py',
+    '../../_base_/schedules/schedule_adadelta_5e.py',
+    '../../_base_/recog_models/crnn.py',
 ]
 
-train_list = {{_base_.train_list}}
-test_list = {{_base_.test_list}}
+default_hooks = dict(logger=dict(type='LoggerHook', interval=50), )
 
-train_pipeline = {{_base_.train_pipeline}}
-test_pipeline = {{_base_.test_pipeline}}
+# dataset settings
+dataset_type = 'OCRDataset'
+data_root = 'data/recog/'
+file_client_args = dict(backend='disk')
 
-data = dict(
-    samples_per_gpu=64,
-    workers_per_gpu=4,
-    val_dataloader=dict(samples_per_gpu=1),
-    test_dataloader=dict(samples_per_gpu=1),
-    train=dict(
-        type='UniformConcatDataset',
-        datasets=train_list,
-        pipeline=train_pipeline),
-    val=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline),
-    test=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
+train_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        color_type='grayscale',
+        file_client_args=file_client_args),
+    dict(type='LoadOCRAnnotations', with_text=True),
+    dict(type='Resize', scale=(100, 32), keep_ratio=False),
+    dict(
+        type='PackTextRecogInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'valid_ratio'))
+]
+
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        color_type='grayscale',
+        file_client_args=file_client_args),
+    dict(
+        type='RescaleToHeight',
+        height=32,
+        min_width=32,
+        max_width=None,
+        width_divisor=16),
+    dict(
+        type='PackTextRecogInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'valid_ratio',
+                   'instances'))
+]
+
+train_dataloader = dict(
+    batch_size=64,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path=None),
+        ann_file='train_label.json',
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path=None),
+        ann_file='test_label.json',
+        test_mode=True,
         pipeline=test_pipeline))
+test_dataloader = val_dataloader
 
-evaluation = dict(interval=1, metric='acc')
-
-cudnn_benchmark = True
+val_evaluator = [
+    dict(
+        type='WordMetric', mode=['exact', 'ignore_case',
+                                 'ignore_case_symbol']),
+    dict(type='CharMetric')
+]
+test_evaluator = val_evaluator
+visualizer = dict(type='TextRecogLocalVisualizer', name='visualizer')
