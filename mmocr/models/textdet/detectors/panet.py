08a55f506f190560df11a4504af1184f26358052
liukuikun <641417025@qq.com>
2022-06-07 23:04:12 +0800

    pan model

--- mmocr/models/textdet/detectors/panet.py
+++ mmocr/models/textdet/detectors/panet.py
@@ -1,27 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmocr.registry import MODELS
 from .single_stage_text_detector import SingleStageTextDetector
-from .text_detector_mixin import TextDetectorMixin
 
 
 @MODELS.register_module()
-class PANet(TextDetectorMixin, SingleStageTextDetector):
+class PANet(SingleStageTextDetector):
     """The class for implementing PANet text detector:
 
     Efficient and Accurate Arbitrary-Shaped Text Detection with Pixel
     Aggregation Network [https://arxiv.org/abs/1908.05900].
     """
-
-    def __init__(self,
-                 backbone,
-                 neck,
-                 bbox_head,
-                 train_cfg=None,
-                 test_cfg=None,
-                 pretrained=None,
-                 show_score=False,
-                 init_cfg=None):
-        SingleStageTextDetector.__init__(self, backbone, neck, bbox_head,
-                                         train_cfg, test_cfg, pretrained,
-                                         init_cfg)
-        TextDetectorMixin.__init__(self, show_score)
