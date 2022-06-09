ebaef501d2e3bd9daff25c308e8d630c1da1e2e4
gaotongxiao <gaotongxiao@gmail.com>
2022-06-09 15:02:32 +0800

    update

--- setup.py
+++ setup.py
@@ -197,5 +197,6 @@ if __name__ == '__main__':
             'tests': parse_requirements('requirements/tests.txt'),
             'build': parse_requirements('requirements/build.txt'),
             'optional': parse_requirements('requirements/optional.txt'),
+            'mim': parse_requirements('requirements/mminstall.txt'),
         },
         zip_safe=False)
