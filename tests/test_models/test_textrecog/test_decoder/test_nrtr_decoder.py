8614070e364cd79e747cd2f68983030a493800a9
jiangqing.vendor <mountchicken@outlook.com>
2022-06-10 10:14:36 +0000

    [NRTR] NRTR Decoder

---
+++ tests/test_models/test_textrecog/test_decoder/test_nrtr_decoder.py
@@ -0,0 +1,100 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+import tempfile
+from unittest import TestCase
+
+import torch
+from mmengine.data import LabelData
+
+from mmocr.core.data_structures import TextRecogDataSample
+from mmocr.models.textrecog.decoders import NRTRDecoder
+
+
+class TestNRTRDecoder(TestCase):
+
+    def setUp(self):
+        gt_text_sample1 = TextRecogDataSample()
+        gt_text = LabelData()
+        gt_text.item = 'Hello'
+        gt_text_sample1.gt_text = gt_text
+        gt_text_sample1.set_metainfo(dict(valid_ratio=0.9))
+
+        gt_text_sample2 = TextRecogDataSample()
+        gt_text = LabelData()
+        gt_text = LabelData()
+        gt_text.item = 'World'
+        gt_text_sample2.gt_text = gt_text
+        gt_text_sample2.set_metainfo(dict(valid_ratio=1.0))
+
+        self.data_info = [gt_text_sample1, gt_text_sample2]
+
+    def _create_dummy_dict_file(
+        self, dict_file,
+        chars=list('0123456789abcdefghijklmnopqrstuvwxyz')):  # NOQA
+        with open(dict_file, 'w') as f:
+            for char in chars:
+                f.write(char + '\n')
+
+    def test_init(self):
+        tmp_dir = tempfile.TemporaryDirectory()
+        dict_file = osp.join(tmp_dir.name, 'fake_chars.txt')
+        self._create_dummy_dict_file(dict_file)
+        dict_cfg = dict(
+            type='Dictionary',
+            dict_file=dict_file,
+            with_start=True,
+            with_end=True,
+            same_start_end=True,
+            with_padding=True,
+            with_unknown=True)
+        loss_cfg = dict(type='CELoss')
+        NRTRDecoder(dictionary=dict_cfg, loss=loss_cfg)
+        tmp_dir.cleanup()
+
+    def test_forward_train(self):
+        encoder_out = torch.randn(2, 25, 512)
+        tmp_dir = tempfile.TemporaryDirectory()
+        dict_file = osp.join(tmp_dir.name, 'fake_chars.txt')
+        self._create_dummy_dict_file(dict_file)
+        # test diction cfg
+        dict_cfg = dict(
+            type='Dictionary',
+            dict_file=dict_file,
+            with_start=True,
+            with_end=True,
+            same_start_end=True,
+            with_padding=True,
+            with_unknown=True)
+        loss_cfg = dict(type='CELoss')
+        decoder = NRTRDecoder(dictionary=dict_cfg, loss=loss_cfg)
+        data_samples = decoder.loss.get_targets(self.data_info)
+        output = decoder(
+            out_enc=encoder_out, data_samples=data_samples, train_mode=True)
+        self.assertTupleEqual(tuple(output.shape), (2, 40, 39))
+
+    def test_forward_test(self):
+        encoder_out = torch.randn(2, 25, 512)
+        tmp_dir = tempfile.TemporaryDirectory()
+        dict_file = osp.join(tmp_dir.name, 'fake_chars.txt')
+        self._create_dummy_dict_file(dict_file)
+        # test diction cfg
+        dict_cfg = dict(
+            type='Dictionary',
+            dict_file=dict_file,
+            with_start=True,
+            with_end=True,
+            same_start_end=True,
+            with_padding=True,
+            with_unknown=True)
+        loss_cfg = dict(type='CELoss')
+        decoder = NRTRDecoder(
+            dictionary=dict_cfg, loss=loss_cfg, max_seq_len=40)
+        output = decoder(
+            out_enc=encoder_out, data_samples=self.data_info, train_mode=False)
+        self.assertTupleEqual(tuple(output.shape), (2, 40, 39))
+
+
+if __name__ == '__main__':
+    t = TestNRTRDecoder()
+    t.setUp()
+    t.test_forward_test()
