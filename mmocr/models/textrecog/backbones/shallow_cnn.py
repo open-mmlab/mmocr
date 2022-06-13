b44869059bf064dcdc8b9b0499508f19d5e56c76
wangxinyu <wangxinyu2017@gmail.com>
2022-06-13 06:48:54 +0000

    [SATRN] SATRN Backbone

--- mmocr/models/textrecog/backbones/shallow_cnn.py
+++ mmocr/models/textrecog/backbones/shallow_cnn.py
@@ -1,7 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Dict, List, Optional, Union
+
 import torch.nn as nn
 from mmcv.cnn import ConvModule
 from mmcv.runner import BaseModule
+from torch import Tensor
 
 from mmocr.registry import MODELS
 
@@ -14,19 +17,22 @@ class ShallowCNN(BaseModule):
     <https://arxiv.org/pdf/1910.04396.pdf>`_.
 
     Args:
-        base_channels (int): Number of channels of input image tensor
-            :math:`D_i`.
+        input_channels (int): Number of channels of input image tensor
+            :math:`D_i`. Defaults to 1.
         hidden_dim (int): Size of hidden layers of the model :math:`D_m`.
+            Defaults to 512.
         init_cfg (dict or list[dict], optional): Initialization configs.
     """
 
-    def __init__(self,
-                 input_channels=1,
-                 hidden_dim=512,
-                 init_cfg=[
-                     dict(type='Kaiming', layer='Conv2d'),
-                     dict(type='Uniform', layer='BatchNorm2d')
-                 ]):
+    def __init__(
+        self,
+        input_channels: int = 1,
+        hidden_dim: int = 512,
+        init_cfg: Optional[Union[Dict, List[Dict]]] = [
+            dict(type='Kaiming', layer='Conv2d'),
+            dict(type='Uniform', layer='BatchNorm2d')
+        ]
+    ) -> None:
         super().__init__(init_cfg=init_cfg)
         assert isinstance(input_channels, int)
         assert isinstance(hidden_dim, int)
@@ -51,7 +57,7 @@ class ShallowCNN(BaseModule):
             act_cfg=dict(type='ReLU'))
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         """
         Args:
             x (Tensor): Input image feature :math:`(N, D_i, H, W)`.
