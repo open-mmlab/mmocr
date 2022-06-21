05990c58d9f6291b13683e4f2a1e8ceb6cce6e39
wangxinyu <wangxinyu2017@gmail.com>
2022-06-21 07:27:11 +0000

    [Refactor] PSE Head

--- mmocr/models/textdet/heads/pse_head.py
+++ mmocr/models/textdet/heads/pse_head.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Dict, List, Optional, Union
+
 from mmocr.registry import MODELS
 from . import PANHead
 
@@ -8,35 +10,29 @@ class PSEHead(PANHead):
     """The class for PSENet head.
 
     Args:
-        in_channels (list[int]): A list of 4 numbers of input channels.
-        out_channels (int): Number of output channels.
-        downsample_ratio (float): Downsample ratio.
+        in_channels (list[int]): A list of numbers of input channels.
+        hidden_dim (int): The hidden dimension of the first convolutional
+            layer.
+        out_channel (int): Number of output channels.
         loss (dict): Configuration dictionary for loss type. Supported loss
-            types are "PANLoss" and "PSELoss".
+            types are "PANLoss" and "PSELoss". Defaults to PSELoss.
         postprocessor (dict): Config of postprocessor for PSENet.
-        train_cfg, test_cfg (dict): Depreciated.
         init_cfg (dict or list[dict], optional): Initialization configs.
     """
 
     def __init__(self,
-                 in_channels,
-                 out_channels,
-                 downsample_ratio=0.25,
-                 loss=dict(type='PSELoss'),
-                 postprocessor=dict(
+                 in_channels: List[int],
+                 hidden_dim: int,
+                 out_channel: int,
+                 loss: Dict = dict(type='PSELoss'),
+                 postprocessor: Dict = dict(
                      type='PSEPostprocessor', text_repr_type='poly'),
-                 train_cfg=None,
-                 test_cfg=None,
-                 init_cfg=None,
-                 **kwargs):
+                 init_cfg: Optional[Union[Dict, List[Dict]]] = None) -> None:
 
         super().__init__(
             in_channels=in_channels,
-            out_channels=out_channels,
-            downsample_ratio=downsample_ratio,
+            hidden_dim=hidden_dim,
+            out_channel=out_channel,
             loss=loss,
             postprocessor=postprocessor,
-            train_cfg=train_cfg,
-            test_cfg=test_cfg,
-            init_cfg=init_cfg,
-            **kwargs)
+            init_cfg=init_cfg)
