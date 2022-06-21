bc58e75f367ac5aa25f61a3536f4986cc5d1128a
liukuikun <641417025@qq.com>
2022-06-21 06:12:10 +0000

    [Config] PAN config

--- configs/_base_/det_models/panet_r18_fpem_ffm.py
+++ configs/_base_/det_models/panet_r18_fpem_ffm.py
@@ -1,43 +1,34 @@
-model_poly = dict(
-    type='PANet',
-    backbone=dict(
-        type='mmdet.ResNet',
-        depth=18,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=-1,
-        norm_cfg=dict(type='SyncBN', requires_grad=True),
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18'),
-        norm_eval=True,
-        style='caffe'),
-    neck=dict(type='FPEM_FFM', in_channels=[64, 128, 256, 512]),
-    bbox_head=dict(
-        type='PANHead',
-        in_channels=[128, 128, 128, 128],
-        out_channels=6,
-        loss=dict(type='PANLoss'),
-        postprocessor=dict(type='PANPostprocessor', text_repr_type='poly')),
-    train_cfg=None,
-    test_cfg=None)
+preprocess_cfg = dict(
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True,
+    pad_size_divisor=32)
 
-model_quad = dict(
+# BasicBlock has a little difference from official PANet
+# BasicBlock in mmdet lacks RELU in the last convolution.
+model = dict(
     type='PANet',
     backbone=dict(
         type='mmdet.ResNet',
         depth=18,
         num_stages=4,
+        stem_channels=128,
+        deep_stem=True,
         out_indices=(0, 1, 2, 3),
         frozen_stages=-1,
-        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        norm_eval=False,
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18'),
-        norm_eval=True,
-        style='caffe'),
+        style='pytorch'),
     neck=dict(type='FPEM_FFM', in_channels=[64, 128, 256, 512]),
-    bbox_head=dict(
+    det_head=dict(
         type='PANHead',
         in_channels=[128, 128, 128, 128],
-        out_channels=6,
-        loss=dict(type='PANLoss'),
+        hidden_dim=128,
+        out_channel=6,
+        loss=dict(
+            type='PANLoss',
+            loss_text=dict(type='MaskedSquareDiceLoss'),
+            loss_kernel=dict(type='MaskedSquareDiceLoss'),
+        ),
         postprocessor=dict(type='PANPostprocessor', text_repr_type='quad')),
-    train_cfg=None,
-    test_cfg=None)
+    preprocess_cfg=preprocess_cfg)
--- configs/textdet/panet/panet_r18_fpem_ffm_600e_icdar2015.py
+++ configs/textdet/panet/panet_r18_fpem_ffm_600e_icdar2015.py
@@ -2,34 +2,81 @@ _base_ = [
     '../../_base_/default_runtime.py',
     '../../_base_/schedules/schedule_adam_600e.py',
     '../../_base_/det_models/panet_r18_fpem_ffm.py',
-    '../../_base_/det_datasets/icdar2015.py',
-    '../../_base_/det_pipelines/panet_pipeline.py'
 ]
 
-model = {{_base_.model_quad}}
+train_pipeline = [
+    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+    dict(
+        type='LoadOCRAnnotations',
+        with_polygon=True,
+        with_bbox=True,
+        with_label=True,
+    ),
+    dict(type='ShortScaleAspectJitter', short_size=736, scale_divisor=32),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='RandomRotate', max_angle=10),
+    dict(type='TextDetRandomCrop', target_size=(736, 736)),
+    dict(type='Pad', size=(736, 736)),
+    dict(
+        type='TorchVisionWrapper',
+        op='ColorJitter',
+        brightness=32.0 / 255,
+        saturation=0.5),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
 
-train_list = {{_base_.train_list}}
-test_list = {{_base_.test_list}}
+test_pipeline = [
+    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+    # TODO Replace with mmcv.RescaleToShort when it's ready
+    dict(
+        type='RescaleToShortAspectJitter',
+        short_size=736,
+        scale_divisor=1,
+        ratio_range=(1.0, 1.0),
+        aspect_ratio_range=(1.0, 1.0)),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor',
+                   'instances'))
+]
 
-train_pipeline_icdar2015 = {{_base_.train_pipeline_icdar2015}}
-test_pipeline_icdar2015 = {{_base_.test_pipeline_icdar2015}}
+dataset_type = 'OCRDataset'
+data_root = 'data/det/icdar2015'
 
-data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=2,
-    val_dataloader=dict(samples_per_gpu=1),
-    test_dataloader=dict(samples_per_gpu=1),
-    train=dict(
-        type='UniformConcatDataset',
-        datasets=train_list,
-        pipeline=train_pipeline_icdar2015),
-    val=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline_icdar2015),
-    test=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline_icdar2015))
+train_dataset = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='instance_training.json',
+    data_prefix=dict(img_path='imgs/'),
+    filter_cfg=dict(filter_empty_gt=True),
+    pipeline=train_pipeline)
 
-evaluation = dict(interval=10, metric='hmean-iou')
+test_dataset = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='instance_test.json',
+    data_prefix=dict(img_path='imgs/'),
+    test_mode=True,
+    pipeline=test_pipeline)
+
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=train_dataset)
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=test_dataset)
+test_dataloader = val_dataloader
+
+val_evaluator = dict(
+    type='HmeanIOUMetric', pred_score_thrs=dict(start=0.3, stop=1, step=0.05))
+test_evaluator = val_evaluator
+
+visualizer = dict(type='TextDetLocalVisualizer', name='visualizer')
