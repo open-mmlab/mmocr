01de158282c44f9af7088713a173bb6cb2d19849
gaotongxiao <gaotongxiao@gmail.com>
2022-06-21 06:04:30 +0000

    Fix MaskedCELoss

--- mmocr/models/textdet/losses/common/__init__.py
+++ mmocr/models/textdet/losses/common/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .bce_loss import MaskedBalancedBCELoss
+from .bce_loss import MaskedBalancedBCELoss, MaskedBCELoss
 from .dice_loss import MaskedDiceLoss, MaskedSquareDiceLoss
 from .l1_loss import MaskedSmoothL1Loss, SmoothL1Loss
 
 __all__ = [
     'MaskedBalancedBCELoss', 'MaskedDiceLoss', 'MaskedSmoothL1Loss',
-    'MaskedSquareDiceLoss', 'SmoothL1Loss'
+    'MaskedSquareDiceLoss', 'MaskedBCELoss', 'SmoothL1Loss'
 ]
