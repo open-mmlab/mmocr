4a04982806d94612320b1711fb9ca705f8838e5c
wangxinyu <wangxinyu2017@gmail.com>
2022-06-21 09:16:19 +0000

    [PSE] PSE Loss

--- mmocr/models/textdet/losses/pse_loss.py
+++ mmocr/models/textdet/losses/pse_loss.py
@@ -1,87 +1,109 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmdet.core import BitmapMasks
+from typing import Dict, Sequence, Tuple, Union
 
+import torch
+import torch.nn.functional as F
+
+from mmocr.core import TextDetDataSample
 from mmocr.registry import MODELS
-from mmocr.utils import check_argument
 from . import PANLoss
 
 
 @MODELS.register_module()
 class PSELoss(PANLoss):
-    r"""The class for implementing PSENet loss. This is partially adapted from
+    """The class for implementing PSENet loss. This is partially adapted from
     https://github.com/whai362/PSENet.
 
     PSENet: `Shape Robust Text Detection with
     Progressive Scale Expansion Network <https://arxiv.org/abs/1806.02559>`_.
 
     Args:
-        alpha (float): Text loss coefficient, and :math:`1-\alpha` is the
-            kernel loss coefficient.
-        ohem_ratio (float): The negative/positive ratio in ohem.
-        reduction (str): The way to reduce the loss. Available options are
-            "mean" and "sum".
+        weight_text (float): The weight of text loss. Defaults to 0.7.
+        weight_kernel (float): The weight of text kernel. Defaults to 0.3.
+        loss_text (dict): Loss type for text. Defaults to
+            dict('MaskedSquareDiceLoss').
+        loss_kernel (dict): Loss type for kernel. Defaults to
+            dict('MaskedSquareDiceLoss').
+        ohem_ratio (int or float): The negative/positive ratio in ohem.
+            Defaults to 3.
+        reduction (str): The way to reduce the loss. Defaults to 'mean'.
+            Options are 'mean' and 'sum'.
+        kernel_sample_type (str): The way to sample kernel. Defaults to
+            adaptive. Options are 'adaptive' and 'hard'.
+        shrink_ratio (tuple): The ratio for shirinking text instances.
+            Defaults to (1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4).
+        max_shrink_dist (int or float): The maximum shrinking distance.
+            Defaults to 20.
     """
 
-    def __init__(self,
-                 alpha=0.7,
-                 ohem_ratio=3,
-                 reduction='mean',
-                 kernel_sample_type='adaptive'):
+    def __init__(
+        self,
+        weight_text: float = 0.7,
+        weight_kernel: float = 0.3,
+        loss_text: Dict = dict(type='MaskedSquareDiceLoss'),
+        loss_kernel: Dict = dict(type='MaskedSquareDiceLoss'),
+        ohem_ratio: Union[int, float] = 3,
+        reduction: str = 'mean',
+        kernel_sample_type: str = 'adaptive',
+        shrink_ratio: Tuple[float] = (1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4),
+        max_shrink_dist: Union[int, float] = 20,
+    ) -> None:
         super().__init__()
         assert reduction in ['mean', 'sum'
                              ], "reduction must be either of ['mean','sum']"
-        self.alpha = alpha
+        assert kernel_sample_type in [
+            'adaptive', 'hard'
+        ], "kernel_sample_type must be either of ['hard', 'adaptive']"
+        self.weight_text = weight_text
+        self.weight_kernel = weight_kernel
         self.ohem_ratio = ohem_ratio
         self.reduction = reduction
+        self.shrink_ratio = shrink_ratio
         self.kernel_sample_type = kernel_sample_type
+        self.max_shrink_dist = max_shrink_dist
+        self.loss_text = MODELS.build(loss_text)
+        self.loss_kernel = MODELS.build(loss_kernel)
 
-    def forward(self, score_maps, downsample_ratio, gt_kernels, gt_mask):
+    def forward(self, preds: torch.Tensor,
+                data_samples: Sequence[TextDetDataSample]) -> Dict:
         """Compute PSENet loss.
 
         Args:
-            score_maps (tensor): The output tensor with size of Nx6xHxW.
-            downsample_ratio (float): The downsample ratio between score_maps
-                and the input img.
-            gt_kernels (list[BitmapMasks]): The kernel list with each element
-                being the text kernel mask for one img.
-            gt_mask (list[BitmapMasks]): The effective mask list
-                with each element being the effective mask for one img.
+            preds (torch.Tensor): Raw predictions from model with
+                shape :math:`(N, C, H, W)`.
+            data_samples (list[TextDetDataSample]): The data samples.
 
         Returns:
-            dict:  A loss dict with ``loss_text`` and ``loss_kernel``.
+            dict: The dict for pse losses with loss_text, loss_kernel,
+            loss_aggregation and loss_discrimination.
         """
-
-        assert check_argument.is_type_list(gt_kernels, BitmapMasks)
-        assert check_argument.is_type_list(gt_mask, BitmapMasks)
-        assert isinstance(downsample_ratio, float)
         losses = []
 
-        pred_texts = score_maps[:, 0, :, :]
-        pred_kernels = score_maps[:, 1:, :, :]
-        feature_sz = score_maps.size()
+        gt_kernels, gt_masks = self.get_targets(data_samples)
+        target_size = gt_kernels.size()[2:]
+        preds = F.interpolate(preds, size=target_size, mode='bilinear')
+        pred_texts = preds[:, 0, :, :]
+        pred_kernels = preds[:, 1:, :, :]
 
-        gt_kernels = [item.rescale(downsample_ratio) for item in gt_kernels]
-        gt_kernels = self.bitmasks2tensor(gt_kernels, feature_sz[2:])
-        gt_kernels = [item.to(score_maps.device) for item in gt_kernels]
-
-        gt_mask = [item.rescale(downsample_ratio) for item in gt_mask]
-        gt_mask = self.bitmasks2tensor(gt_mask, feature_sz[2:])
-        gt_mask = [item.to(score_maps.device) for item in gt_mask]
+        gt_kernels = gt_kernels.to(preds.device)
+        gt_kernels[gt_kernels <= 0.5] = 0
+        gt_kernels[gt_kernels > 0.5] = 1
+        gt_masks = gt_masks.to(preds.device)
 
         # compute text loss
-        sampled_masks_text = self.ohem_batch(pred_texts.detach(),
-                                             gt_kernels[0], gt_mask[0])
-        loss_texts = self.dice_loss_with_logits(pred_texts, gt_kernels[0],
-                                                sampled_masks_text)
-        losses.append(self.alpha * loss_texts)
+        sampled_mask = self._ohem_batch(pred_texts.detach(), gt_kernels[0],
+                                        gt_masks)
+        loss_texts = self.loss_text(pred_texts.sigmoid(), gt_kernels[0],
+                                    sampled_mask)
+        losses.append(self.weight_text * loss_texts)
 
         # compute kernel loss
         if self.kernel_sample_type == 'hard':
-            sampled_masks_kernel = (gt_kernels[0] > 0.5).float() * (
-                gt_mask[0].float())
+            sampled_masks_kernel = (gt_kernels[0] >
+                                    0.5).float() * gt_masks.float()
         elif self.kernel_sample_type == 'adaptive':
             sampled_masks_kernel = (pred_texts > 0).float() * (
-                gt_mask[0].float())
+                gt_masks.float())
         else:
             raise NotImplementedError
 
@@ -89,12 +111,12 @@ class PSELoss(PANLoss):
         assert num_kernel == len(gt_kernels) - 1
         loss_list = []
         for idx in range(num_kernel):
-            loss_kernels = self.dice_loss_with_logits(
-                pred_kernels[:, idx, :, :], gt_kernels[1 + idx],
+            loss_kernels = self.loss_kernel(
+                pred_kernels[:, idx, :, :].sigmoid(), gt_kernels[1 + idx],
                 sampled_masks_kernel)
             loss_list.append(loss_kernels)
 
-        losses.append((1 - self.alpha) * sum(loss_list) / len(loss_list))
+        losses.append(self.weight_kernel * sum(loss_list) / len(loss_list))
 
         if self.reduction == 'mean':
             losses = [item.mean() for item in losses]
@@ -102,5 +124,6 @@ class PSELoss(PANLoss):
             losses = [item.sum() for item in losses]
         else:
             raise NotImplementedError
+
         results = dict(loss_text=losses[0], loss_kernel=losses[1])
         return results
