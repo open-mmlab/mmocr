0bf1ce88c23cc93d017190c1daf4e3f49861da48
jiangqing.vendor <mountchicken@outlook.com>
2022-06-23 02:27:09 +0000

    [FCENet] Add FCE Postprocessor

--- mmocr/models/textdet/postprocessors/fce_postprocessor.py
+++ mmocr/models/textdet/postprocessors/fce_postprocessor.py
@@ -1,66 +1,164 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Dict, List, Sequence
+
 import cv2
 import numpy as np
+import torch
+from mmengine import InstanceData
+from numpy.fft import ifft
 
+from mmocr.core import TextDetDataSample
 from mmocr.registry import MODELS
-from .base_postprocessor import BasePostprocessor
-from .utils import fill_hole, fourier2poly, poly_nms
+from .base_postprocessor import BaseTextDetPostProcessor
+from .utils import fill_hole
 
 
 @MODELS.register_module()
-class FCEPostprocessor(BasePostprocessor):
+class FCEPostprocessor(BaseTextDetPostProcessor):
     """Decoding predictions of FCENet to instances.
 
     Args:
         fourier_degree (int): The maximum Fourier transform degree k.
         num_reconstr_points (int): The points number of the polygon
             reconstructed from predicted Fourier coefficients.
-        text_repr_type (str): Boundary encoding type 'poly' or 'quad'.
-        scale (int): The down-sample scale of the prediction.
-        alpha (float): The parameter to calculate final scores. Score_{final}
-                = (Score_{text region} ^ alpha)
-                * (Score_{text center region}^ beta)
-        beta (float): The parameter to calculate final score.
+        rescale_fields (list[str]): The bbox/polygon field names to
+            be rescaled. If None, no rescaling will be performed. Defaults to
+            ['polygons'].
+        scales (list[int]) : The down-sample scale of each layer. Defaults
+            to [8, 16, 32].
+        text_repr_type (str): Boundary encoding type 'poly' or 'quad'. Defaults
+            to 'poly'.
+         alpha (float): The parameter to calculate final scores
+            :math:`Score_{final} = (Score_{text region} ^ alpha)
+            * (Score_{text center_region}^ beta)`. Defaults to 1.0.
+        beta (float): The parameter to calculate final score. Defaults to 2.0.
         score_thr (float): The threshold used to filter out the final
-            candidates.
-        nms_thr (float): The threshold of nms.
+            candidates.Defaults to 0.3.
+        nms_thr (float): The threshold of nms. Defaults to 0.1.
     """
 
     def __init__(self,
-                 fourier_degree,
-                 num_reconstr_points,
-                 text_repr_type='poly',
-                 alpha=1.0,
-                 beta=2.0,
-                 score_thr=0.3,
-                 nms_thr=0.1,
-                 **kwargs):
-        super().__init__(text_repr_type)
+                 fourier_degree: int,
+                 num_reconstr_points: int,
+                 rescale_fields: Sequence[str] = ['polygons'],
+                 scales: Sequence[int] = [8, 16, 32],
+                 text_repr_type: str = 'poly',
+                 alpha: float = 1.0,
+                 beta: float = 2.0,
+                 score_thr: float = 0.3,
+                 nms_thr: float = 0.1,
+                 **kwargs) -> None:
+        super().__init__(
+            text_repr_type=text_repr_type,
+            rescale_fields=rescale_fields,
+            **kwargs)
         self.fourier_degree = fourier_degree
         self.num_reconstr_points = num_reconstr_points
+        self.scales = scales
         self.alpha = alpha
         self.beta = beta
         self.score_thr = score_thr
         self.nms_thr = nms_thr
 
-    def __call__(self, preds, scale):
-        """
+    def split_results(self, pred_results: List[Dict]) -> List[List[Dict]]:
+        """Split batched elements in pred_results along the first dimension
+        into ``batch_num`` sub-elements and regather them into a list of dicts.
+
         Args:
-            preds (list[Tensor]): Classification prediction and regression
-                prediction.
-            scale (float): Scale of current layer.
+            pred_results (list[dict]): A list of dict with keys of ``cls_res``,
+                ``reg_res`` corresponding to the classification result and
+                regression result computed from the input tensor with the
+                same index. They have the shapes of :math:`(N, C_{cls,i},
+                H_i, W_i)` and :math:`(N, C_{out,i}, H_i, W_i)`.
 
         Returns:
-            list[list[float]]: The instance boundary and confidence.
+            list[list[dict]]: N lists. Each list contains three dicts from
+            different feature level.
         """
-        assert isinstance(preds, list)
-        assert len(preds) == 2
+        assert isinstance(pred_results, list) and len(pred_results) == len(
+            self.scales)
 
-        cls_pred = preds[0][0]
+        fields = list(pred_results[0].keys())
+        batch_num = len(pred_results[0][fields[0]])
+        level_num = len(pred_results)
+        results = []
+        for i in range(batch_num):
+            batch_list = []
+            for level in range(level_num):
+                feat_dict = {}
+                for field in fields:
+                    feat_dict[field] = pred_results[level][field][i]
+                batch_list.append(feat_dict)
+            results.append(batch_list)
+        return results
+
+    def get_text_instances(self, pred_results: Sequence[Dict],
+                           data_sample: TextDetDataSample
+                           ) -> TextDetDataSample:
+        """Get text instance predictions of one image.
+
+        Args:
+            pred_results (List[dict]): A list of dict with keys of ``cls_res``,
+                ``reg_res`` corresponding to the classification result and
+                regression result computed from the input tensor with the
+                same index. They have the shapes of :math:`(N, C_{cls,i}, H_i,
+                W_i)` and :math:`(N, C_{out,i}, H_i, W_i)`.
+            data_sample (TextDetDataSample): Datasample of an image.
+
+        Returns:
+            TextDetDataSample: A new DataSample with predictions filled in.
+            Polygons and results are saved in
+            ``TextDetDataSample.pred_instances.polygons``. The confidence
+            scores are saved in ``TextDetDataSample.pred_instances.scores``.
+        """
+        assert len(pred_results) == len(self.scales)
+        data_sample.pred_instances = InstanceData()
+        data_sample.pred_instances.polygons = []
+        data_sample.pred_instances.scores = []
+
+        result_polys = []
+        result_scores = []
+        for idx, pred_result in enumerate(pred_results):
+            # TODO: Scale can be calculated given image shape and feature
+            # shape. This param can be removed in the future.
+            polygons, scores = self._get_text_instances_single(
+                pred_result, self.scales[idx])
+            result_polys += polygons
+            result_scores += scores
+        result_polys, result_scores = self.poly_nms(result_polys,
+                                                    result_scores,
+                                                    self.nms_thr)
+        for result_poly, result_score in zip(result_polys, result_scores):
+            result_poly = np.array(result_poly, dtype=np.float32)
+            data_sample.pred_instances.polygons.append(result_poly)
+            data_sample.pred_instances.scores.append(result_score)
+        data_sample.pred_instances.scores = torch.FloatTensor(
+            data_sample.pred_instances.scores)
+
+        return data_sample
+
+    def _get_text_instances_single(self, pred_result: Dict, scale: int):
+        """Get text instance predictions from one feature level.
+
+        Args:
+            pred_result (dict): A dict with keys of ``cls_res``, ``reg_res``
+                corresponding to the classification result and regression
+                result computed from the input tensor with the same index.
+                They have the shapes of :math:`(1, C_{cls,i}, H_i, W_i)` and
+                :math:`(1, C_{out,i}, H_i, W_i)`.
+            scale (int): Scale of current feature map which equals to
+                img_size / feat_size.
+
+        Returns:
+            result_polys (list[ndarray]): A list of polygons after postprocess.
+            result_scores (list[ndarray]): A list of scores after postprocess.
+        """
+
+        cls_pred = pred_result['cls_res']
         tr_pred = cls_pred[0:2].softmax(dim=0).data.cpu().numpy()
         tcl_pred = cls_pred[2:].softmax(dim=0).data.cpu().numpy()
 
-        reg_pred = preds[1][0].permute(1, 2, 0).data.cpu().numpy()
+        reg_pred = pred_result['reg_res'].permute(1, 2, 0).data.cpu().numpy()
         x_pred = reg_pred[:, :, :2 * self.fourier_degree + 1]
         y_pred = reg_pred[:, :, 2 * self.fourier_degree + 1:]
 
@@ -73,7 +171,9 @@ class FCEPostprocessor(BasePostprocessor):
             cv2.CHAIN_APPROX_SIMPLE)  # opencv4
 
         mask = np.zeros_like(tr_mask)
-        boundaries = []
+
+        result_polys = []
+        result_scores = []
         for cont in tr_contours:
             deal_map = mask.copy().astype(np.int8)
             cv2.drawContours(deal_map, [cont], -1, 1, -1)
@@ -88,23 +188,52 @@ class FCEPostprocessor(BasePostprocessor):
             c[:, self.fourier_degree] = c[:, self.fourier_degree] + dxy
             c *= scale
 
-            polygons = fourier2poly(c, self.num_reconstr_points)
-            score = score_map[score_mask].reshape(-1, 1)
-            polygons = poly_nms(
-                np.hstack((polygons, score)).tolist(), self.nms_thr)
+            polygons = self._fourier2poly(c, self.num_reconstr_points)
+            scores = score_map[score_mask].reshape(-1, 1).tolist()
+            polygons, scores = self.poly_nms(polygons, scores, self.nms_thr)
+            result_polys += polygons
+            result_scores += scores
 
-            boundaries = boundaries + polygons
-
-        boundaries = poly_nms(boundaries, self.nms_thr)
+        result_polys, result_scores = self.poly_nms(result_polys,
+                                                    result_scores,
+                                                    self.nms_thr)
 
         if self.text_repr_type == 'quad':
-            new_boundaries = []
-            for boundary in boundaries:
-                poly = np.array(boundary[:-1]).reshape(-1,
-                                                       2).astype(np.float32)
-                score = boundary[-1]
+            new_polys = []
+            for poly in result_polys:
+                poly = np.array(poly).reshape(-1, 2).astype(np.float32)
                 points = cv2.boxPoints(cv2.minAreaRect(poly))
                 points = np.int0(points)
-                new_boundaries.append(points.reshape(-1).tolist() + [score])
+                new_polys.append(points.reshape(-1))
 
-        return boundaries
+            return new_polys, result_scores
+        return result_polys, result_scores
+
+    def _fourier2poly(self,
+                      fourier_coeff: np.ndarray,
+                      num_reconstr_points: int = 50):
+        """ Inverse Fourier transform
+            Args:
+                fourier_coeff (ndarray): Fourier coefficients shaped (n, 2k+1),
+                    with n and k being candidates number and Fourier degree
+                    respectively.
+                num_reconstr_points (int): Number of reconstructed polygon
+                    points. Defaults to 50.
+
+            Returns:
+                List[ndarray]: The reconstructed polygons.
+            """
+
+        a = np.zeros((len(fourier_coeff), num_reconstr_points),
+                     dtype='complex')
+        k = (len(fourier_coeff[0]) - 1) // 2
+
+        a[:, 0:k + 1] = fourier_coeff[:, k:]
+        a[:, -k:] = fourier_coeff[:, :k]
+
+        poly_complex = ifft(a) * num_reconstr_points
+        polygon = np.zeros((len(fourier_coeff), num_reconstr_points, 2))
+        polygon[:, :, 0] = poly_complex.real
+        polygon[:, :, 1] = poly_complex.imag
+        return polygon.astype('int32').reshape(
+            (len(fourier_coeff), -1)).tolist()
---
+++ tests/test_models/test_textdet/test_postprocessors/test_fce_postprocessor.py
@@ -0,0 +1,70 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import unittest
+
+import numpy as np
+import torch
+from mmengine import InstanceData
+from nose_parameterized import parameterized
+
+from mmocr.core import TextDetDataSample
+from mmocr.models.textdet.postprocessors import FCEPostprocessor
+
+
+class TestFCEPostProcessor(unittest.TestCase):
+
+    def test_split_results(self):
+        pred_results = [
+            dict(
+                cls_res=torch.rand(2, 4, 10, 10),
+                reg_res=torch.rand(2, 21, 10, 10)),
+            dict(
+                cls_res=torch.rand(2, 4, 20, 20),
+                reg_res=torch.rand(2, 21, 20, 20)),
+            dict(
+                cls_res=torch.rand(2, 4, 40, 40),
+                reg_res=torch.rand(2, 21, 40, 40)),
+        ]
+        postprocessor = FCEPostprocessor(
+            fourier_degree=5, num_reconstr_points=20, score_thr=0.3)
+        split_results = postprocessor.split_results(pred_results)
+        self.assertEqual(len(split_results), 2)
+        self.assertEqual(len(split_results[0]), 3)
+        self.assertEqual(len(split_results[1]), 3)
+        self.assertEqual(split_results[0][0]['cls_res'].shape, (4, 10, 10))
+        self.assertEqual(split_results[0][0]['reg_res'].shape, (21, 10, 10))
+
+    @parameterized.expand([('poly'), ('quad')])
+    def test_get_text_instances(self, text_repr_type):
+        postprocessor = FCEPostprocessor(
+            fourier_degree=5,
+            num_reconstr_points=20,
+            score_thr=0.3,
+            text_repr_type=text_repr_type)
+        pred_result = [
+            dict(
+                cls_res=torch.rand(4, 10, 10), reg_res=torch.rand(22, 10, 10)),
+            dict(
+                cls_res=torch.rand(4, 20, 20), reg_res=torch.rand(22, 20, 20)),
+            dict(
+                cls_res=torch.rand(4, 30, 30), reg_res=torch.rand(22, 30, 30)),
+        ]
+        data_sample = TextDetDataSample(
+            gt_instances=InstanceData(polygons=[
+                np.array([0, 0, 0, 1, 2, 1, 2, 0]),
+                np.array([1, 1, 1, 2, 3, 2, 3, 1])
+            ]))
+        results = postprocessor.get_text_instances(pred_result, data_sample)
+        self.assertIn('polygons', results.pred_instances)
+        self.assertIn('scores', results.pred_instances)
+        self.assertTrue(
+            isinstance(results.pred_instances.scores, torch.FloatTensor))
+        self.assertEqual(
+            len(results.pred_instances.scores),
+            len(results.pred_instances.polygons))
+        if len(results.pred_instances.polygons) > 0:
+            if text_repr_type == 'poly':
+                self.assertEqual(results.pred_instances.polygons[0].shape,
+                                 (40, ))
+            else:
+                self.assertEqual(results.pred_instances.polygons[0].shape,
+                                 (8, ))
