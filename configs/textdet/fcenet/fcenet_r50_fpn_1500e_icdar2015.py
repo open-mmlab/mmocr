d201c6ccf28f23748752fc0ca5d2fe5ec6357774
jiangqing.vendor <mountchicken@outlook.com>
2022-06-23 02:51:48 +0000

    [FCENet] Add FCENet config

--- configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py
+++ configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py
@@ -2,32 +2,102 @@ _base_ = [
     '../../_base_/default_runtime.py',
     '../../_base_/schedules/schedule_sgd_1500e.py',
     '../../_base_/det_models/fcenet_r50_fpn.py',
-    '../../_base_/det_datasets/icdar2015.py',
-    '../../_base_/det_pipelines/fcenet_pipeline.py'
 ]
 
-train_list = {{_base_.train_list}}
-test_list = {{_base_.test_list}}
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=20),
+    logger=dict(type='LoggerHook', interval=20))
 
-train_pipeline_icdar2015 = {{_base_.train_pipeline_icdar2015}}
-test_pipeline_icdar2015 = {{_base_.test_pipeline_icdar2015}}
+train_pipeline = [
+    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+    dict(
+        type='LoadOCRAnnotations',
+        with_polygon=True,
+        with_bbox=True,
+        with_label=True,
+    ),
+    dict(
+        type='RandomResize',
+        scale=(800, 800),
+        ratio_range=(0.75, 2.5),
+        keep_ratio=True),
+    dict(
+        type='TextDetRandomCropFlip',
+        crop_ratio=0.5,
+        iter_num=1,
+        min_area_ratio=0.2),
+    dict(
+        type='RandomApply',
+        transforms=[dict(type='RandomCrop', min_side_ratio=0.3)],
+        prob=0.8),
+    dict(
+        type='RandomRotate',
+        max_angle=30,
+        pad_with_fixed_color=False,
+        use_canvas=True),
+    dict(
+        type='RandomChoice',
+        transforms=[[
+            dict(type='Resize', scale=800, keep_ratio=True),
+            dict(type='SourceImagePad', target_scale=800)
+        ],
+                    dict(type='Resize', scale=800, keep_ratio=False)],
+        prob=[0.6, 0.4]),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(
+        type='TorchVisionWrapper',
+        op='ColorJitter',
+        brightness=32.0 / 255,
+        saturation=0.5,
+        contrast=0.5),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+    dict(type='Resize', scale=(2260, 2260), keep_ratio=True),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor',
+                   'instances'))
+]
 
-data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=2,
-    val_dataloader=dict(samples_per_gpu=1),
-    test_dataloader=dict(samples_per_gpu=1),
-    train=dict(
-        type='UniformConcatDataset',
-        datasets=train_list,
-        pipeline=train_pipeline_icdar2015),
-    val=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline_icdar2015),
-    test=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline_icdar2015))
+dataset_type = 'OCRDataset'
+data_root = 'data/icdar2015'
 
-evaluation = dict(interval=10, metric='hmean-iou')
+train_dataset = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='instances_training.json',
+    data_prefix=dict(img_path='imgs/'),
+    filter_cfg=dict(filter_empty_gt=True, min_size=32),
+    pipeline=train_pipeline)
+
+test_dataset = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='instances_test.json',
+    data_prefix=dict(img_path='imgs/'),
+    test_mode=True,
+    pipeline=test_pipeline)
+
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=train_dataset)
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=test_dataset)
+test_dataloader = val_dataloader
+
+val_evaluator = dict(type='HmeanIOUMetric')
+test_evaluator = val_evaluator
+
+visualizer = dict(
+    type='TextDetLocalVisualizer', name='visualizer', save_dir='imgs')
