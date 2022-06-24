ca35c78e692c477d82bcb49da2d720a32be9c2ea
liukuikun <641417025@qq.com>
2022-06-24 04:47:20 +0000

    [Config] SAR config

--- configs/_base_/recog_models/sar.py
+++ configs/_base_/recog_models/sar.py
@@ -1,8 +1,15 @@
-label_convertor = dict(
-    type='AttnConvertor', dict_type='DICT90', with_unknown=True)
+dictionary = dict(
+    type='Dictionary',
+    dict_file='dicts/english_digits_symbols.txt',
+    with_start=True,
+    with_end=True,
+    same_start_end=True,
+    with_padding=True,
+    with_unknown=True)
 
 model = dict(
     type='SARNet',
+    preprocess_cfg=dict(mean=[127, 127, 127], std=[127, 127, 127]),
     backbone=dict(type='ResNet31OCR'),
     encoder=dict(
         type='SAREncoder',
@@ -18,7 +25,8 @@ model = dict(
         dec_gru=False,
         pred_dropout=0.1,
         d_k=512,
-        pred_concat=True),
-    loss=dict(type='SARLoss'),
-    label_convertor=label_convertor,
+        pred_concat=True,
+        postprocessor=dict(type='AttentionPostprocessor'),
+        loss=dict(type='CELoss', ignore_first_char=True, reduction='mean')),
+    dictionary=dictionary,
     max_seq_len=30)
