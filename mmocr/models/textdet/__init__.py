9b7f75e15703548234bfa68b87731a1d40eaa0bf
xinyu <wangxinyu2017@gmail.com>
2022-07-07 17:52:46 +0800

    delete __all

--- mmocr/models/textdet/__init__.py
+++ mmocr/models/textdet/__init__.py
@@ -1,13 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from . import (data_preprocessors, detectors, heads, losses, necks,
-               postprocessors)
 from .data_preprocessors import *  # NOQA
 from .detectors import *  # NOQA
 from .heads import *  # NOQA
 from .losses import *  # NOQA
 from .necks import *  # NOQA
 from .postprocessors import *  # NOQA
-
-__all__ = (
-    heads.__all__ + detectors.__all__ + losses.__all__ + necks.__all__ +
-    postprocessors.__all__ + data_preprocessors.__all__)
