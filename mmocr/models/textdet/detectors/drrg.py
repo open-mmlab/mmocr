62716452e003646027f71039862fe6b840504757
gaotongxiao <gaotongxiao@gmail.com>
2022-07-08 15:59:56 +0000

    [DRRG] DRRG framework

--- mmocr/models/textdet/detectors/drrg.py
+++ mmocr/models/textdet/detectors/drrg.py
@@ -1,54 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmocr.registry import MODELS
 from .single_stage_text_detector import SingleStageTextDetector
-from .text_detector_mixin import TextDetectorMixin
 
 
 @MODELS.register_module()
-class DRRG(TextDetectorMixin, SingleStageTextDetector):
+class DRRG(SingleStageTextDetector):
     """The class for implementing DRRG text detector. Deep Relational Reasoning
     Graph Network for Arbitrary Shape Text Detection.
 
     [https://arxiv.org/abs/2003.07493]
     """
-
-    def __init__(self,
-                 backbone,
-                 neck,
-                 bbox_head,
-                 train_cfg=None,
-                 test_cfg=None,
-                 pretrained=None,
-                 show_score=False,
-                 init_cfg=None):
-        SingleStageTextDetector.__init__(self, backbone, neck, bbox_head,
-                                         train_cfg, test_cfg, pretrained,
-                                         init_cfg)
-        TextDetectorMixin.__init__(self, show_score)
-
-    def forward_train(self, img, img_metas, **kwargs):
-        """
-        Args:
-            img (Tensor): Input images of shape (N, C, H, W).
-                Typically these should be mean centered and std scaled.
-            img_metas (list[dict]): A List of image info dict where each dict
-                has: 'img_shape', 'scale_factor', 'flip', and may also contain
-                'filename', 'ori_shape', 'pad_shape', and 'img_norm_cfg'.
-                For details of the values of these keys see
-                :class:`mmdet.datasets.pipelines.Collect`.
-        Returns:
-            dict[str, Tensor]: A dictionary of loss components.
-        """
-        x = self.extract_feat(img)
-        gt_comp_attribs = kwargs.pop('gt_comp_attribs')
-        preds = self.bbox_head(x, gt_comp_attribs)
-        losses = self.bbox_head.loss(preds, **kwargs)
-        return losses
-
-    def simple_test(self, img, img_metas, rescale=False):
-
-        x = self.extract_feat(img)
-        outs = self.bbox_head.single_test(x)
-        boundaries = self.bbox_head.get_boundary(*outs, img_metas, rescale)
-
-        return [boundaries]
---
+++ tests/test_models/test_textdet/test_detectors/base.py
@@ -0,0 +1,120 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import copy
+from os.path import dirname, exists, join
+
+import numpy as np
+import torch
+from mmengine import Config, ConfigDict, InstanceData
+
+from mmocr.core import TextDetDataSample
+
+
+class BaseTestUtils:
+
+    def _create_dummy_inputs(self,
+                             input_shape=(1, 3, 300, 300),
+                             num_items=None,
+                             num_classes=1):
+        """Create a superset of inputs needed to run test or train batches."""
+        (N, C, H, W) = input_shape
+
+        rng = np.random.RandomState(0)
+
+        imgs = rng.rand(*input_shape)
+
+        metainfo = dict(
+            img_shape=(H, W, C),
+            ori_shape=(H, W, C),
+            pad_shape=(H, W, C),
+            filename='test.jpg',
+            scale_factor=(1, 1),
+            flip=False)
+
+        gt_masks = []
+        gt_kernels = []
+        gt_effective_mask = []
+
+        data_samples = []
+
+        for batch_idx in range(N):
+            if num_items is None:
+                num_boxes = rng.randint(1, 10)
+            else:
+                num_boxes = num_items[batch_idx]
+
+            data_sample = TextDetDataSample(
+                metainfo=metainfo, gt_instances=InstanceData())
+
+            cx, cy, bw, bh = rng.rand(num_boxes, 4).T
+
+            tl_x = ((cx * W) - (W * bw / 2)).clip(0, W)
+            tl_y = ((cy * H) - (H * bh / 2)).clip(0, H)
+            br_x = ((cx * W) + (W * bw / 2)).clip(0, W)
+            br_y = ((cy * H) + (H * bh / 2)).clip(0, H)
+
+            boxes = np.vstack([tl_x, tl_y, br_x, br_y]).T
+            class_idxs = [0] * num_boxes
+
+            data_sample.gt_instances.bboxes = torch.FloatTensor(boxes)
+            data_sample.gt_instances.labels = torch.LongTensor(class_idxs)
+            data_sample.gt_instances.ignored = torch.BoolTensor([False] *
+                                                                num_boxes)
+            data_samples.append(data_sample)
+
+            # kernels = []
+            # TODO: add support for multiple kernels (if necessary)
+            # for _ in range(num_kernels):
+            #     kernel = np.random.rand(H, W)
+            #     kernels.append(kernel)
+            gt_kernels.append(np.random.rand(H, W))
+            gt_effective_mask.append(np.ones((H, W)))
+
+        mask = np.random.randint(0, 2, (len(boxes), H, W), dtype=np.uint8)
+        gt_masks.append(mask)
+
+        mm_inputs = {
+            'imgs': torch.FloatTensor(imgs).requires_grad_(True),
+            'data_samples': data_samples,
+            'gt_masks': gt_masks,
+            'gt_kernels': gt_kernels,
+            'gt_mask': gt_effective_mask,
+            'gt_thr_mask': gt_effective_mask,
+            'gt_text_mask': gt_effective_mask,
+            'gt_center_region_mask': gt_effective_mask,
+            'gt_radius_map': gt_kernels,
+            'gt_sin_map': gt_kernels,
+            'gt_cos_map': gt_kernels,
+        }
+        return mm_inputs
+
+    def _get_config_directory(self):
+        """Find the predefined detector config directory."""
+        try:
+            # Assume we are running in the source mmocr repo
+            repo_dpath = dirname(dirname(dirname(dirname(dirname(__file__)))))
+        except NameError:
+            # For IPython development when this __file__ is not defined
+            import mmocr
+            repo_dpath = dirname(
+                dirname(dirname(dirname(dirname(mmocr.__file__)))))
+        config_dpath = join(repo_dpath, 'configs')
+        if not exists(config_dpath):
+            raise Exception('Cannot find config path')
+        return config_dpath
+
+    def _get_config_module(self, fname: str) -> 'ConfigDict':
+        """Load a configuration as a python module."""
+        config_dpath = self._get_config_directory()
+        config_fpath = join(config_dpath, fname)
+        config_mod = Config.fromfile(config_fpath)
+        return config_mod
+
+    def _get_detector_cfg(self, fname: str) -> 'ConfigDict':
+        """Grab configs necessary to create a detector.
+
+        These are deep copied to allow for safe modification of parameters
+        without influencing other tests.
+        """
+        config = self._get_config_module(fname)
+        model = copy.deepcopy(config.model)
+        return model
