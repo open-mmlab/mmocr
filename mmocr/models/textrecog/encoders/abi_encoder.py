e7a6fba1b06ee203754cb2954da1fd8995190893
gaotongxiao <gaotongxiao@gmail.com>
2022-07-08 14:42:06 +0800

    update encoders

--- mmocr/models/textrecog/encoders/abi_encoder.py
+++ mmocr/models/textrecog/encoders/abi_encoder.py
@@ -1,36 +1,43 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import copy
+from typing import Dict, List, Optional, Union
 
+import torch
 from mmcv.cnn.bricks.transformer import BaseTransformerLayer
 from mmcv.runner import BaseModule, ModuleList
 
+from mmocr.core import TextRecogDataSample
 from mmocr.models.common.modules import PositionalEncoding
 from mmocr.registry import MODELS
 
 
 @MODELS.register_module()
-class TransformerEncoder(BaseModule):
+class ABIEncoder(BaseModule):
     """Implement transformer encoder for text recognition, modified from
     `<https://github.com/FangShancheng/ABINet>`.
 
     Args:
-        n_layers (int): Number of attention layers.
-        n_head (int): Number of parallel attention heads.
+        n_layers (int): Number of attention layers. Defaults to 2.
+        n_head (int): Number of parallel attention heads. Defaults to 8.
         d_model (int): Dimension :math:`D_m` of the input from previous model.
-        d_inner (int): Hidden dimension of feedforward layers.
-        dropout (float): Dropout rate.
-        max_len (int): Maximum output sequence length :math:`T`.
+            Defaults to 512.
+        d_inner (int): Hidden dimension of feedforward layers. Defaults to
+            2048.
+        dropout (float): Dropout rate. Defaults to 0.1.
+        max_len (int): Maximum output sequence length :math:`T`. Defaults to
+            8 * 32.
         init_cfg (dict or list[dict], optional): Initialization configs.
+            Defaults to None.
     """
 
     def __init__(self,
-                 n_layers=2,
-                 n_head=8,
-                 d_model=512,
-                 d_inner=2048,
-                 dropout=0.1,
-                 max_len=8 * 32,
-                 init_cfg=None):
+                 n_layers: int = 2,
+                 n_head: int = 8,
+                 d_model: int = 512,
+                 d_inner: int = 2048,
+                 dropout: float = 0.1,
+                 max_len: int = 8 * 32,
+                 init_cfg: Optional[Union[Dict, List[Dict]]] = None):
         super().__init__(init_cfg=init_cfg)
 
         assert d_model % n_head == 0, 'd_model must be divisible by n_head'
@@ -56,10 +63,12 @@ class TransformerEncoder(BaseModule):
         self.transformer = ModuleList(
             [copy.deepcopy(encoder_layer) for _ in range(n_layers)])
 
-    def forward(self, feature):
+    def forward(self, feature: torch.Tensor,
+                data_samples: List[TextRecogDataSample]) -> torch.Tensor:
         """
         Args:
             feature (Tensor): Feature tensor of shape :math:`(N, D_m, H, W)`.
+            data_samples (List[TextRecogDataSample]): List of data samples.
 
         Returns:
             Tensor: Features of shape :math:`(N, D_m, H, W)`.
