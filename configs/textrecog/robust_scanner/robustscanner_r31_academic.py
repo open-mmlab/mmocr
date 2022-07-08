77ffe8fb0098ef2fa66e28fde784f16350b43371
liukuikun <641417025@qq.com>
2022-07-08 16:17:05 +0000

    [Config] scanner config

--- configs/textrecog/robust_scanner/robustscanner_r31_academic.py
+++ configs/textrecog/robust_scanner/robustscanner_r31_academic.py
@@ -1,34 +1,70 @@
 _base_ = [
     '../../_base_/default_runtime.py',
     '../../_base_/recog_models/robust_scanner.py',
-    '../../_base_/schedules/schedule_adam_step_5e.py',
-    '../../_base_/recog_pipelines/sar_pipeline.py',
-    '../../_base_/recog_datasets/ST_SA_MJ_real_train.py',
-    '../../_base_/recog_datasets/academic_test.py'
+    '../../_base_/schedules/schedule_adam_step_5e.py'
 ]
 
-train_list = {{_base_.train_list}}
-test_list = {{_base_.test_list}}
+# dataset settings
+dataset_type = 'OCRDataset'
+data_root = 'data/recog'
+file_client_args = dict(backend='disk')
 
-train_pipeline = {{_base_.train_pipeline}}
-test_pipeline = {{_base_.test_pipeline}}
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadOCRAnnotations', with_text=True),
+    dict(type='Resize', scale=(160, 48), keep_ratio=False),
+    dict(
+        type='PackTextRecogInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'valid_ratio'))
+]
 
-data = dict(
-    samples_per_gpu=64,
-    workers_per_gpu=2,
-    val_dataloader=dict(samples_per_gpu=1),
-    test_dataloader=dict(samples_per_gpu=1),
-    train=dict(
-        type='UniformConcatDataset',
-        datasets=train_list,
-        pipeline=train_pipeline),
-    val=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline),
-    test=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(
+        type='RescaleToHeight',
+        height=48,
+        min_width=48,
+        max_width=160,
+        width_divisor=4),
+    dict(type='PadToWidth', width=160),
+    dict(
+        type='PackTextRecogInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'valid_ratio',
+                   'instances'))
+]
+
+train_dataloader = dict(
+    batch_size=64,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path=''),
+        ann_file='train_label.json',
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path=''),
+        ann_file='test_label.json',
+        test_mode=True,
         pipeline=test_pipeline))
+test_dataloader = val_dataloader
 
-evaluation = dict(interval=1, metric='acc')
+val_evaluator = [
+    dict(
+        type='WordMetric', mode=['exact', 'ignore_case',
+                                 'ignore_case_symbol']),
+    dict(type='CharMetric')
+]
+test_evaluator = val_evaluator
+visualizer = dict(type='TextRecogLocalVisualizer', name='visualizer')
