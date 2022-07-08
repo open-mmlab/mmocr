6dbde20ab0d1d5f883e676901c6cd0c08f6e62ae
liukuikun <641417025@qq.com>
2022-07-08 16:09:06 +0000

    [Encoder] scanner encoder

--- mmocr/models/textrecog/encoders/channel_reduction_encoder.py
+++ mmocr/models/textrecog/encoders/channel_reduction_encoder.py
@@ -1,6 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import Dict, Optional, Sequence
+
+import torch
 import torch.nn as nn
 
+from mmocr.core.data_structures import TextRecogDataSample
 from mmocr.registry import MODELS
 from .base_encoder import BaseEncoder
 
@@ -13,23 +17,32 @@ class ChannelReductionEncoder(BaseEncoder):
         in_channels (int): Number of input channels.
         out_channels (int): Number of output channels.
         init_cfg (dict or list[dict], optional): Initialization configs.
+            Defaults to dict(type='Xavier', layer='Conv2d').
     """
 
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 init_cfg=dict(type='Xavier', layer='Conv2d')):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        init_cfg: Dict = dict(type='Xavier', layer='Conv2d')
+    ) -> None:
         super().__init__(init_cfg=init_cfg)
 
         self.layer = nn.Conv2d(
             in_channels, out_channels, kernel_size=1, stride=1, padding=0)
 
-    def forward(self, feat, img_metas=None):
+    def forward(
+        self,
+        feat: torch.Tensor,
+        data_samples: Optional[Sequence[TextRecogDataSample]] = None
+    ) -> torch.Tensor:
         """
         Args:
             feat (Tensor): Image features with the shape of
                 :math:`(N, C_{in}, H, W)`.
-            img_metas (None): Unused.
+            data_samples (list[TextRecogDataSample], optional): Batch of
+                TextRecogDataSample, containing valid_ratio information.
+                Defaults to None.
 
         Returns:
             Tensor: A tensor of shape :math:`(N, C_{out}, H, W)`.
