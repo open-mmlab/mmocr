de78a8839f78a37fc8c364a6b69e3c36720b52e1
wangxinyu <wangxinyu2017@gmail.com>
2022-07-11 10:03:01 +0000

    Remove useless

--- mmocr/models/textdet/detectors/__init__.py
+++ mmocr/models/textdet/detectors/__init__.py
@@ -2,14 +2,12 @@
 from .dbnet import DBNet
 from .drrg import DRRG
 from .fcenet import FCENet
-from .ocr_mask_rcnn import OCRMaskRCNN
 from .panet import PANet
 from .psenet import PSENet
 from .single_stage_text_detector import SingleStageTextDetector
-from .text_detector_mixin import TextDetectorMixin
 from .textsnake import TextSnake
 
 __all__ = [
-    'TextDetectorMixin', 'SingleStageTextDetector', 'OCRMaskRCNN', 'DBNet',
-    'PANet', 'PSENet', 'TextSnake', 'FCENet', 'DRRG'
+    'SingleStageTextDetector', 'DBNet', 'PANet', 'PSENet', 'TextSnake',
+    'FCENet', 'DRRG'
 ]
--- mmocr/models/textdet/heads/__init__.py
+++ mmocr/models/textdet/heads/__init__.py
@@ -3,12 +3,11 @@ from .base_textdet_head import BaseTextDetHead
 from .db_head import DBHead
 from .drrg_head import DRRGHead
 from .fce_head import FCEHead
-from .head_mixin import HeadMixin
 from .pan_head import PANHead
 from .pse_head import PSEHead
 from .textsnake_head import TextSnakeHead
 
 __all__ = [
     'PSEHead', 'PANHead', 'DBHead', 'FCEHead', 'TextSnakeHead', 'DRRGHead',
-    'HeadMixin', 'BaseTextDetHead'
+    'BaseTextDetHead'
 ]
