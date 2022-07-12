2d478ea244bc66ef00665f21d18da31dcabf58a0
gaotongxiao <gaotongxiao@gmail.com>
2022-07-12 08:27:55 +0000

    Add ABINet cfg

--- configs/textrecog/abinet/abinet_academic.py
+++ configs/textrecog/abinet/abinet_academic.py
@@ -1,35 +1,3 @@
-_base_ = [
-    '../../_base_/default_runtime.py',
-    '../../_base_/schedules/schedule_adam_step_20e.py',
-    '../../_base_/recog_pipelines/abinet_pipeline.py',
-    '../../_base_/recog_models/abinet.py',
-    # '../../_base_/recog_datasets/ST_MJ_alphanumeric_train.py',
-    '../../_base_/recog_datasets/toy_data.py'
-    # '../../_base_/recog_datasets/academic_test.py'
-]
+_base_ = ['../../_base_/recog_models/abinet.py', 'base.py']
 
-train_list = {{_base_.train_list}}
-test_list = {{_base_.test_list}}
-
-train_pipeline = {{_base_.train_pipeline}}
-test_pipeline = {{_base_.test_pipeline}}
-
-data = dict(
-    samples_per_gpu=192,
-    workers_per_gpu=8,
-    val_dataloader=dict(samples_per_gpu=1),
-    test_dataloader=dict(samples_per_gpu=1),
-    train=dict(
-        type='UniformConcatDataset',
-        datasets=train_list,
-        pipeline=train_pipeline),
-    val=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline),
-    test=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline))
-
-evaluation = dict(interval=1, metric='acc')
+load_from = 'abinet_pretrain-1bed979b.pth'
