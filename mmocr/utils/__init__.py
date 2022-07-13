17b56ac646d341c59906fa9f7914ecffbc3b58b8
wangxinyu <wangxinyu2017@gmail.com>
2022-07-13 14:33:08 +0000

    [Utils] Remove useless & Update covignore

--- mmocr/utils/__init__.py
+++ mmocr/utils/__init__.py
@@ -12,9 +12,7 @@ from .data_converter_utils import dump_ocr_data, recog_anno_to_imginfo
 from .evaluation_utils import compute_hmean
 from .fileio import list_from_file, list_to_file
 from .img_utils import crop_img, warp_img
-from .logger import get_root_logger
 from .mask_utils import fill_hole
-from .model import revert_sync_batchnorm
 from .parsers import LineJsonParser, LineStrParser
 from .point_utils import point_distance, points_center
 from .polygon_utils import (boundary_iou, crop_polygon, is_poly_inside_rect,
@@ -31,23 +29,20 @@ from .typing import (ColorType, ConfigType, DetSampleList, InitConfigType,
                      RecForwardResults, RecSampleList)
 
 __all__ = [
-    'Registry', 'build_from_cfg', 'get_root_logger', 'collect_env',
-    'is_3dlist', 'is_type_list', 'is_none_or_type', 'equal_len', 'is_2dlist',
-    'valid_boundary', 'list_to_file', 'list_from_file', 'is_on_same_line',
-    'stitch_boxes_into_lines', 'StringStripper', 'revert_sync_batchnorm',
-    'bezier_to_polygon', 'sort_points', 'dump_ocr_data',
-    'recog_anno_to_imginfo', 'rescale_polygons', 'rescale_polygon',
-    'rescale_bboxes', 'bbox2poly', 'crop_polygon', 'is_poly_inside_rect',
-    'poly2bbox', 'poly_intersection', 'poly_iou', 'poly_make_valid',
-    'poly_union', 'poly2shapely', 'polys2shapely', 'register_all_modules',
-    'offset_polygon', 'sort_vertex8', 'sort_vertex', 'bbox_center_distance',
-    'bbox_diag', 'compute_hmean', 'filter_2dlist_result',
-    'many2one_match_ic13', 'one2one_match_ic13', 'select_top_boundary',
-    'boundary_iou', 'point_distance', 'points_center', 'fill_hole',
-    'LineJsonParser', 'LineStrParser', 'shapely2poly', 'crop_img', 'warp_img',
-    'ConfigType', 'DetSampleList', 'RecForwardResults', 'InitConfigType',
-    'OptConfigType', 'OptDetSampleList', 'OptInitConfigType', 'OptMultiConfig',
-    'OptRecSampleList', 'RecSampleList', 'MultiConfig', 'OptTensor',
-    'ColorType', 'OptKIESampleList', 'KIESampleList', 'bbox_diag_distance',
-    'bezier2polygon'
+    'Registry', 'build_from_cfg', 'collect_env', 'is_3dlist', 'is_type_list',
+    'is_none_or_type', 'equal_len', 'is_2dlist', 'valid_boundary',
+    'list_to_file', 'list_from_file', 'is_on_same_line',
+    'stitch_boxes_into_lines', 'StringStripper', 'bezier2polygon',
+    'sort_points', 'dump_ocr_data', 'recog_anno_to_imginfo',
+    'rescale_polygons', 'rescale_polygon', 'rescale_bboxes', 'bbox2poly',
+    'crop_polygon', 'is_poly_inside_rect', 'poly2bbox', 'poly_intersection',
+    'poly_iou', 'poly_make_valid', 'poly_union', 'poly2shapely',
+    'polys2shapely', 'register_all_modules', 'offset_polygon', 'sort_vertex8',
+    'sort_vertex', 'bbox_center_distance', 'bbox_diag_distance',
+    'compute_hmean', 'boundary_iou', 'point_distance', 'points_center',
+    'fill_hole', 'LineJsonParser', 'LineStrParser', 'shapely2poly', 'crop_img',
+    'warp_img', 'ConfigType', 'DetSampleList', 'RecForwardResults',
+    'InitConfigType', 'OptConfigType', 'OptDetSampleList', 'OptInitConfigType',
+    'OptMultiConfig', 'OptRecSampleList', 'RecSampleList', 'MultiConfig',
+    'OptTensor', 'ColorType', 'OptKIESampleList', 'KIESampleList'
 ]
