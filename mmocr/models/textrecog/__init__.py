d8c3aeff3af26a4cbc1832eb45fe93933a3eddf5
wangxinyu <wangxinyu2017@gmail.com>
2022-07-14 11:23:40 +0000

    Remove useless & Rename

--- mmocr/models/textrecog/__init__.py
+++ mmocr/models/textrecog/__init__.py
@@ -1,12 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .backbones import *  # NOQA
-from .convertors import *  # NOQA
 from .data_preprocessors import *  # NOQA
 from .decoders import *  # NOQA
 from .dictionary import *  # NOQA
 from .encoders import *  # NOQA
-from .heads import *  # NOQA
-from .module_losses import *  # NOQA
 from .plugins import *  # NOQA
 from .postprocessors import *  # NOQA
 from .preprocessors import *  # NOQA
