a538c643525c3a2fd1281c4f5def0df6c19337be
jiangqing.vendor <mountchicken@outlook.com>
2022-07-15 19:53:16 +0800

    update

--- mmocr/models/textrecog/__init__.py
+++ mmocr/models/textrecog/__init__.py
@@ -4,6 +4,7 @@ from .data_preprocessors import *  # NOQA
 from .decoders import *  # NOQA
 from .dictionary import *  # NOQA
 from .encoders import *  # NOQA
+from .module_losses import *  # NOQA
 from .plugins import *  # NOQA
 from .postprocessors import *  # NOQA
 from .recognizers import *  # NOQA
