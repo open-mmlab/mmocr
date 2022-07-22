5dfa68641c65ca834370aeaf249b1af3a6788236
liukuikun <24622904+Harold-lkk@users.noreply.github.com>
2022-07-22 16:46:06 +0800

    [Config] Add multiloop cfg (#1186)

--- configs/textrecog/satrn/satrn_academic.py
+++ configs/textrecog/satrn/satrn_academic.py
@@ -8,7 +8,6 @@ _base_ = [
 
 # dataset settings
 train_list = {{_base_.train_list}}
-test_list = {{_base_.test_list}}
 file_client_args = dict(backend='disk')
 default_hooks = dict(logger=dict(type='LoggerHook', interval=50))
 
@@ -69,21 +68,12 @@ train_dataloader = dict(
     dataset=dict(
         type='ConcatDataset', datasets=train_list, pipeline=train_pipeline))
 
-val_dataloader = dict(
-    batch_size=64,
-    num_workers=4,
-    persistent_workers=True,
-    drop_last=False,
-    sampler=dict(type='DefaultSampler', shuffle=False),
-    dataset=dict(
-        type='ConcatDataset', datasets=test_list, pipeline=test_pipeline))
-test_dataloader = val_dataloader
-
-val_evaluator = [
-    dict(
-        type='WordMetric', mode=['exact', 'ignore_case',
-                                 'ignore_case_symbol']),
-    dict(type='CharMetric')
-]
-test_evaluator = val_evaluator
+test_cfg = dict(type='MultiTestLoop')
+val_cfg = dict(type='MultiValLoop')
+val_dataloader = _base_.val_dataloader
+test_dataloader = _base_.test_dataloader
+for dataloader in test_dataloader:
+    dataloader['dataset']['pipeline'] = test_pipeline
+for dataloader in val_dataloader:
+    dataloader['dataset']['pipeline'] = test_pipeline
 visualizer = dict(type='TextRecogLocalVisualizer', name='visualizer')
