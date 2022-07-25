2b476bd8c037e2a8ce50c5895a76186b01731902
Tong Gao <gaotongxiao@gmail.com>
2022-07-25 19:11:57 +0800

    [Config] Refactor & fix DB configs (#1188)

---
+++ configs/textdet/dbnet/_base_dbnet_r18_fpnc.py
@@ -0,0 +1,67 @@
+file_client_args = dict(backend='disk')
+
+model = dict(
+    type='DBNet',
+    backbone=dict(
+        type='mmdet.ResNet',
+        depth=18,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=-1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18'),
+        norm_eval=False,
+        style='caffe'),
+    neck=dict(
+        type='FPNC', in_channels=[64, 128, 256, 512], lateral_channels=256),
+    det_head=dict(
+        type='DBHead',
+        in_channels=256,
+        module_loss=dict(type='DBModuleLoss'),
+        postprocessor=dict(type='DBPostprocessor', text_repr_type='quad')),
+    data_preprocessor=dict(
+        type='TextDetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=32))
+
+train_pipeline_r18 = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=file_client_args,
+        color_type='color_ignore_orientation'),
+    dict(
+        type='LoadOCRAnnotations',
+        with_polygon=True,
+        with_bbox=True,
+        with_label=True,
+    ),
+    dict(
+        type='TorchVisionWrapper',
+        op='ColorJitter',
+        brightness=32.0 / 255,
+        saturation=0.5),
+    dict(
+        type='ImgAugWrapper',
+        args=[['Fliplr', 0.5],
+              dict(cls='Affine', rotate=[-10, 10]), ['Resize', [0.5, 3.0]]]),
+    dict(type='RandomCrop', min_side_ratio=0.1),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='Pad', size=(640, 640)),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape'))
+]
+
+test_pipeline_1333_736 = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=file_client_args,
+        color_type='color_ignore_orientation'),
+    dict(type='Resize', scale=(1333, 736), keep_ratio=True),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor',
+                   'instances'))
+]
--- configs/textdet/dbnet/dbnet_r18_fpnc_100k_iters_synthtext.py
+++ configs/textdet/dbnet/dbnet_r18_fpnc_100k_iters_synthtext.py
@@ -1,58 +1,32 @@
 _base_ = [
-    'dbnet_r18_fpnc.py', '../../_base_/default_runtime.py',
-    '../../_base_/schedules/schedule_sgd_100k_iters.py',
+    '_base_dbnet_r18_fpnc.py',
     '../../_base_/det_datasets/synthtext.py',
-    '../../_base_/det_pipelines/dbnet_pipeline.py'
+    '../../_base_/textdet_default_runtime.py',
+    '../../_base_/schedules/schedule_sgd_100k_iters.py',
 ]
 
+# dataset settings
 train_list = {{_base_.train_list}}
 test_list = {{_base_.test_list}}
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline_r18 = [
-    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
-    dict(
-        type='LoadTextAnnotations',
-        with_bbox=True,
-        with_mask=True,
-        poly2mask=False),
-    dict(type='ColorJitter', brightness=32.0 / 255, saturation=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(
-        type='ImgAug',
-        args=[['Fliplr', 0.5],
-              dict(cls='Affine', rotate=[-10, 10]), ['Resize', [0.5, 3.0]]],
-        clip_invalid_ploys=False),
-    dict(type='EastRandomCrop', target_size=(640, 640)),
-    dict(type='DBNetTargets', shrink_ratio=0.4),
-    dict(type='Pad', size_divisor=32),
-    dict(
-        type='CustomFormatBundle',
-        keys=['gt_shrink', 'gt_shrink_mask', 'gt_thr', 'gt_thr_mask'],
-        visualize=dict(flag=False, boundary_key='gt_shrink')),
-    dict(
-        type='Collect',
-        keys=['img', 'gt_shrink', 'gt_shrink_mask', 'gt_thr', 'gt_thr_mask'])
-]
-test_pipeline_1333_736 = {{_base_.test_pipeline_1333_736}}
-
-data = dict(
-    samples_per_gpu=16,
-    workers_per_gpu=8,
-    val_dataloader=dict(samples_per_gpu=1),
-    test_dataloader=dict(samples_per_gpu=1),
-    train=dict(
-        type='UniformConcatDataset',
+train_dataloader = dict(
+    batch_size=16,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type='ConcatDataset',
         datasets=train_list,
-        pipeline=train_pipeline_r18),
-    val=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline_1333_736),
-    test=dict(
-        type='UniformConcatDataset',
-        datasets=test_list,
-        pipeline=test_pipeline_1333_736))
+        pipeline=_base_.train_pipeline_r18))
 
-evaluation = dict(interval=999999, metric='hmean-iou')  # do not evaluate
+val_dataloader = dict(
+    batch_size=16,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='ConcatDataset',
+        datasets=test_list,
+        pipeline=_base_.test_pipeline_1333_736))
+
+test_dataloader = val_dataloader
