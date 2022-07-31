7d4fe6d5a8d846f7ac9cb6ae19bd5236661938d9
willpa11213 <willpat_lau@163.com>
2022-07-31 22:38:22 +0800

    [Feature] Add OverlapPatchEmbed module of SVTR

--- mmocr/models/textrecog/backbones/svtr.py
+++ mmocr/models/textrecog/backbones/svtr.py
@@ -1,60 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # Modified from <https://arxiv.org/abs/2205.00159>
 # Adapted from <https://github.com/PaddlePaddle/PaddleOCR>
-from typing import Callable
 
 import torch.nn as nn
+from mmcv.cnn import ConvModule
 from mmengine.model import BaseModule
-
-
-class Conv_BN_Layer(BaseModule):
-    """The unit layer of conv3x3 and BatchNorm.
-
-    Args:
-        in_channels (int): Number of input channels.
-        out_channels (int): Number of output channels.
-        kernel_size (int, optional): Number of kernel size.
-        stride (int, optional): The stride for the cross-correlation.
-        padding (int, optional): The amount of padding applied to the input
-        bias (bool, optional): Use or not bias parameters. Defaults to True.
-        groups (int, optional): The amount of group convolution.
-        act (Callable, optional): The activation function.
-    """
-
-    def __init__(self,
-                 in_channels: int,
-                 out_channels: int,
-                 kernel_size: int = 3,
-                 stride: int = 2,
-                 padding: int = 1,
-                 bias: bool = False,
-                 groups: int = 1,
-                 act: Callable = nn.GELU):
-        super().__init__()
-        self.conv = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            groups=groups,
-            bias=bias)
-        self.norm = nn.BatchNorm2d(out_channels)
-        self.act = act()
-
-    def forward(self, x):
-        """Forward function.
-
-        Args:
-            x (Tensor): A Tensor of shape :math:`(N, C, H, W)`.
-
-        Returns:
-            Tensor: The raw logit tensor. Shape :math:`(N, C, H, W)`.
-        """
-        x = self.conv(x)
-        x = self.norm(x)
-        outputs = self.act(x)
-        return outputs
 
 
 class OverlapPatchEmbed(BaseModule):
@@ -87,48 +37,53 @@ class OverlapPatchEmbed(BaseModule):
         self.norm = None
         if num_layers == 2:
             self.proj = nn.Sequential(
-                Conv_BN_Layer(
+                ConvModule(
                     in_channels=in_channels,
                     out_channels=embed_dims // 2,
                     kernel_size=3,
                     stride=2,
                     padding=1,
-                    act=nn.GELU,
-                    bias=None),
-                Conv_BN_Layer(
+                    bias=False,
+                    norm_cfg=dict(type='BN'),
+                    act_cfg=dict(type='GELU')),
+                ConvModule(
                     in_channels=embed_dims // 2,
                     out_channels=embed_dims,
                     kernel_size=3,
                     stride=2,
                     padding=1,
-                    act=nn.GELU,
-                    bias=None))
+                    bias=False,
+                    norm_cfg=dict(type='BN'),
+                    act_cfg=dict(type='GELU')))
         if num_layers == 3:
             self.proj = nn.Sequential(
-                Conv_BN_Layer(
+                ConvModule(
                     in_channels=in_channels,
                     out_channels=embed_dims // 4,
                     kernel_size=3,
                     stride=2,
                     padding=1,
-                    act=nn.GELU,
-                    bias=None),
-                Conv_BN_Layer(
+                    bias=False,
+                    norm_cfg=dict(type='BN'),
+                    act_cfg=dict(type='GELU')),
+                ConvModule(
                     in_channels=embed_dims // 4,
                     out_channels=embed_dims // 2,
                     kernel_size=3,
                     stride=2,
                     padding=1,
-                    act=nn.GELU,
-                    bias=None),
-                Conv_BN_Layer(
+                    bias=False,
+                    norm_cfg=dict(type='BN'),
+                    act_cfg=dict(type='GELU')),
+                ConvModule(
                     in_channels=embed_dims // 2,
                     out_channels=embed_dims,
                     kernel_size=3,
                     stride=2,
                     padding=1,
-                    act=nn.GELU,
-                    bias=None))
+                    bias=False,
+                    norm_cfg=dict(type='BN'),
+                    act_cfg=dict(type='GELU')))
 
     def forward(self, x):
         """Forward function.
