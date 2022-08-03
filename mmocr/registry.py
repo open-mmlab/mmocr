c7221bea7fd2df678c5f00f4c45ecc0a0a3e3573
liukuikun <liukuikun@sensetime.com>
2022-08-03 14:17:53 +0800

    fix base name and dist_xx

--- mmocr/registry.py
+++ mmocr/registry.py
@@ -9,6 +9,7 @@ https://mmengine.readthedocs.io/en/latest/tutorials/registry.html.
 from mmengine.registry import DATA_SAMPLERS as MMENGINE_DATA_SAMPLERS
 from mmengine.registry import DATASETS as MMENGINE_DATASETS
 from mmengine.registry import HOOKS as MMENGINE_HOOKS
+from mmengine.registry import LOG_PROCESSORS as MMENGINE_LOG_PROCESSORS
 from mmengine.registry import LOOPS as MMENGINE_LOOPS
 from mmengine.registry import METRICS as MMENGINE_METRICS
 from mmengine.registry import MODEL_WRAPPERS as MMENGINE_MODEL_WRAPPERS
@@ -69,3 +70,6 @@ TASK_UTILS = Registry('task util', parent=MMENGINE_TASK_UTILS)
 VISUALIZERS = Registry('visualizer', parent=MMENGINE_VISUALIZERS)
 # manage visualizer backend
 VISBACKENDS = Registry('vis_backend', parent=MMENGINE_VISBACKENDS)
+
+# manage logprocessor
+LOG_PROCESSORS = Registry('log_processor', parent=MMENGINE_LOG_PROCESSORS)
