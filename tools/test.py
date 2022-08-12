8f13b10991db745a38cb7b84f76194e4a833d9f1
gaotongxiao <gaotongxiao@gmail.com>
2022-08-12 20:17:42 +0800

    Fix vis

--- tools/test.py
+++ tools/test.py
@@ -57,13 +57,15 @@ def trigger_visualization_hook(cfg, args):
     if 'visualization' in default_hooks:
         visualization_hook = default_hooks['visualization']
         # Turn on visualization
+        visualization_hook['enable'] = True
         visualization_hook['draw_gt'] = True
         visualization_hook['draw_pred'] = True
         if args.show:
             visualization_hook['show'] = True
             visualization_hook['wait_time'] = args.wait_time
         if args.show_dir:
-            visualization_hook['test_out_dir'] = args.show_dir
+            cfg.visualizer['save_dir'] = args.show_dir
+            cfg.visualizer['vis_backends'] = [dict(type='LocalVisBackend')]
     else:
         raise RuntimeError(
             'VisualizationHook must be included in default_hooks.'
