e4dce25a0536a3de5a18c4f16ec45cd83796b78b
xinyu <wangxinyu2017@gmail.com>
2022-08-19 18:58:52 +0800

    fix

--- configs/textdet/fcenet/_base_fcenet_resnet50-dcnv2_fpn.py
+++ configs/textdet/fcenet/_base_fcenet_resnet50-dcnv2_fpn.py
@@ -14,7 +14,3 @@ model = dict(
             num_sample=50,
             level_proportion_range=((0, 0.25), (0.2, 0.65), (0.55, 1.0))),
         postprocessor=dict(text_repr_type='poly', alpha=1.0, beta=2.0)))
-
-test_pipeline = [
-    dict(type='Resize', scale=(1080, 736), keep_ratio=True),
-]
--- configs/textdet/fcenet/fcenet_resnet50-dcnv2_fpn_1500e_ctw1500.py
+++ configs/textdet/fcenet/fcenet_resnet50-dcnv2_fpn_1500e_ctw1500.py
@@ -9,8 +9,28 @@ file_client_args = dict(backend='disk')
 # dataset settings
 ctw_det_train = _base_.ctw_det_train
 ctw_det_test = _base_.ctw_det_test
+
+# test pipeline for CTW1500
+ctw_test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=file_client_args,
+        color_type='color_ignore_orientation'),
+    dict(type='Resize', scale=(1080, 736), keep_ratio=True),
+    # add loading annotation after ``Resize`` because ground truth
+    # does not need to do resize data transform
+    dict(
+        type='LoadOCRAnnotations',
+        with_polygon=True,
+        with_bbox=True,
+        with_label=True),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
+
 ctw_det_train.pipeline = _base_.train_pipeline
-ctw_det_test.pipeline = _base_.test_pipeline
+ctw_det_test.pipeline = ctw_test_pipeline
 
 train_dataloader = dict(
     batch_size=8,
