064a03d92ede74db044e002ff13f296cd28c2e76
gaotongxiao <gaotongxiao@gmail.com>
2022-08-20 18:48:06 +0800

    [Config] Add textrec_default_runtime

---
+++ configs/_base_/textrec_default_runtime.py
@@ -0,0 +1,32 @@
+default_scope = 'mmocr'
+
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=100),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', interval=1),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+)
+
+env_cfg = dict(
+    cudnn_benchmark=True,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'),
+)
+
+log_level = 'INFO'
+load_from = None
+resume = False
+
+val_evaluator = dict(
+    type='MultiDatasetsEvaluator',
+    metrics=[
+        dict(
+            type='WordMetric',
+            mode=['exact', 'ignore_case', 'ignore_case_symbol']),
+        dict(type='CharMetric')
+    ],
+    dataset_prefixes=['CUTE80', 'IIIT5K', 'SVT', 'SVTP', 'IC13', 'IC15'])
+test_evaluator = val_evaluator
+
+visualizer = dict(type='TextRecogLocalVisualizer', name='visualizer')
