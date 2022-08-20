29157b23fd70aa7f567e9726dae8a9ab870b03cc
gaotongxiao <gaotongxiao@gmail.com>
2022-08-20 18:30:25 +0800

    [Config] Rename textsnake

---
+++ configs/textdet/textsnake/textsnake_resnet50_fpn-unet_1200e_ctw1500.py
@@ -0,0 +1,30 @@
+_base_ = [
+    '_base_textsnake_resnet50_fpn-unet.py',
+    '../../_base_/det_datasets/ctw1500.py',
+    '../../_base_/textdet_default_runtime.py',
+    '../../_base_/schedules/schedule_sgd_1200e.py',
+]
+
+default_hooks = dict(logger=dict(type='LoggerHook', interval=20))
+
+# dataset settings
+ctw_det_train = _base_.ctw_det_train
+ctw_det_train.pipeline = _base_.train_pipeline
+ctw_det_test = _base_.ctw_det_test
+ctw_det_test.pipeline = _base_.test_pipeline
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=ctw_det_train)
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=ctw_det_test)
+
+test_dataloader = val_dataloader
