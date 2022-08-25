c27f74bed758981b7a8a29555ce7e3d6f1991abf
gaotongxiao <gaotongxiao@gmail.com>
2022-08-25 16:33:00 +0800

    update

--- configs/textrecog/tps/crnn_tps.py
+++ configs/textrecog/tps/crnn_tps.py
@@ -10,7 +10,7 @@ model = dict(
         img_size=(32, 100),
         rectified_img_size=(32, 100),
         num_img_channel=1),
-    backbone=dict(type='VeryDeepVgg', leaky_relu=False, input_channels=1),
+    backbone=dict(type='MiniVGG', leaky_relu=False, input_channels=1),
     encoder=None,
     decoder=dict(type='CRNNDecoder', in_channels=512, rnn_flag=True),
     module_loss=dict(type='CTCModuleLoss'),
