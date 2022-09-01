182902b12f06eb5f82b05f94d36d01a680f81925
xinyu <wangxinyu2017@gmail.com>
2022-09-01 14:20:03 +0800

    update init

--- mmocr/__init__.py
+++ mmocr/__init__.py
@@ -1,60 +1,30 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import warnings
 
 import mmcv
 import mmdet
-from packaging.version import parse
+import mmengine
+from mmengine.utils import digit_version
 
 from .version import __version__, short_version
-
-
-def digit_version(version_str: str, length: int = 4):
-    """Convert a version string into a tuple of integers.
-
-    This method is usually used for comparing two versions. For pre-release
-    versions: alpha < beta < rc.
-    Args:
-        version_str (str): The version string.
-        length (int): The maximum number of version levels. Default: 4.
-    Returns:
-        tuple[int]: The version info in digits (integers).
-    """
-    version = parse(version_str)
-    assert version.release, f'failed to parse version {version_str}'
-    release = list(version.release)
-    release = release[:length]
-    if len(release) < length:
-        release = release + [0] * (length - len(release))
-    if version.is_prerelease:
-        mapping = {'a': -3, 'b': -2, 'rc': -1}
-        val = -4
-        # version.pre can be None
-        if version.pre:
-            if version.pre[0] not in mapping:
-                warnings.warn(f'unknown prerelease version {version.pre[0]}, '
-                              'version checking may go wrong')
-            else:
-                val = mapping[version.pre[0]]
-            release.extend([val, version.pre[-1]])
-        else:
-            release.extend([val, 0])
-
-    elif version.is_postrelease:
-        release.extend([1, version.post])
-    else:
-        release.extend([0, 0])
-    return tuple(release)
-
 
 mmcv_minimum_version = '2.0.0rc1'
 mmcv_maximum_version = '2.1.0'
 mmcv_version = digit_version(mmcv.__version__)
+mmengine_minimum_version = '0.0.0'
+mmengine_maximum_version = '0.2.0'
+mmengine_version = digit_version(mmengine.__version__)
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
         and mmcv_version < digit_version(mmcv_maximum_version)), \
     f'MMCV {mmcv.__version__} is incompatible with MMOCR {__version__}. ' \
     f'Please use MMCV >= {mmcv_minimum_version}, ' \
     f'< {mmcv_maximum_version} instead.'
+
+assert (mmengine_version >= digit_version(mmengine_minimum_version)
+        and mmengine_version < digit_version(mmengine_maximum_version)), \
+    f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
+    f'Please install mmengine>={mmengine_minimum_version}, ' \
+    f'<{mmengine_maximum_version}.'
 
 mmdet_minimum_version = '3.0.0rc0'
 mmdet_maximum_version = '3.1.0'
