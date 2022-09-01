189f5a6cdfc3b4a99a701bd101981059d6a087c2
gaotongxiao <gaotongxiao@gmail.com>
2022-09-01 13:15:25 +0800

    Bump version to 1.0.0rc0

--- mmocr/__init__.py
+++ mmocr/__init__.py
@@ -46,7 +46,7 @@ def digit_version(version_str: str, length: int = 4):
     return tuple(release)
 
 
-mmcv_minimum_version = '2.0.0rc0'
+mmcv_minimum_version = '2.0.0rc1'
 mmcv_maximum_version = '2.1.0'
 mmcv_version = digit_version(mmcv.__version__)
 
