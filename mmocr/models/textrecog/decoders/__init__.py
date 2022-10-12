78b84be0c4e71c84cc88b4930b8f4480c005c8e7
willpa11213 <willpat_lau@163.com>
2022-10-12 18:43:27 +0800

    add svtr decoder

--- mmocr/models/textrecog/decoders/__init__.py
+++ mmocr/models/textrecog/decoders/__init__.py
@@ -11,11 +11,12 @@ from .robust_scanner_fuser import RobustScannerFuser
 from .sar_decoder import ParallelSARDecoder, SequentialSARDecoder
 from .sar_decoder_with_bs import ParallelSARDecoderWithBS
 from .sequence_attention_decoder import SequenceAttentionDecoder
+from .svtr_decoder import SVTRDecoder
 
 __all__ = [
     'CRNNDecoder', 'ParallelSARDecoder', 'SequentialSARDecoder',
     'ParallelSARDecoderWithBS', 'NRTRDecoder', 'BaseDecoder',
     'SequenceAttentionDecoder', 'PositionAttentionDecoder',
     'ABILanguageDecoder', 'ABIVisionDecoder', 'MasterDecoder',
-    'RobustScannerFuser', 'ABIFuser'
+    'RobustScannerFuser', 'ABIFuser', 'SVTRDecoder'
 ]
