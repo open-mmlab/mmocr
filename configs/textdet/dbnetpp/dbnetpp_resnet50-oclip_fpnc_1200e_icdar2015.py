d92444097dcb4f1bb94139bf829fe0fd7a4a4861
Tong Gao <gaotongxiao@gmail.com>
2022-11-03 17:57:13 +0800

    [Config] Add oCLIP configs (#1509)
    * [Config] Add oClip configs
    
    * fix linitng
    
    * fix

---
+++ configs/textdet/dbnetpp/dbnetpp_resnet50-oclip_fpnc_1200e_icdar2015.py
@@ -0,0 +1,20 @@
+_base_ = [
+    'dbnetpp_resnet50-dcnv2_fpnc_1200e_icdar2015.py',
+]
+
+load_from = None
+
+_base_.model.backbone = dict(
+    type='CLIPResNet',
+    init_cfg=dict(
+        type='Pretrained',
+        checkpoint='https://download.openmmlab.com/'
+        'mmocr/backbone/resnet50-oclip-7ba0c533.pth'))
+
+_base_.train_dataloader.num_workers = 24
+_base_.optim_wrapper.optimizer.lr = 0.002
+
+param_scheduler = [
+    dict(type='LinearLR', end=200, start_factor=0.001),
+    dict(type='PolyLR', power=0.9, eta_min=1e-7, begin=200, end=1200),
+]
---
+++ configs/textdet/fcenet/fcenet_resnet50-oclip_fpn_1500e_icdar2015.py
@@ -0,0 +1,16 @@
+_base_ = [
+    'fcenet_resnet50_fpn_1500e_icdar2015.py',
+]
+load_from = None
+
+_base_.model.backbone = dict(
+    type='CLIPResNet',
+    out_indices=(1, 2, 3),
+    init_cfg=dict(
+        type='Pretrained',
+        checkpoint='https://download.openmmlab.com/'
+        'mmocr/backbone/resnet50-oclip-7ba0c533.pth'))
+
+_base_.train_dataloader.batch_size = 16
+_base_.train_dataloader.num_workers = 24
+_base_.optim_wrapper.optimizer.lr = 0.0005
