6ad1163d06e87ce07e82b07c997d796573c4478c
Xinyu <wangxinyu2017@gmail.com>
2022-11-04 20:54:35 +0800

    add cocotextv2 to data preparer

---
+++ dataset_zoo/cocotextv2/sample_anno.md
@@ -0,0 +1,34 @@
+**Text Detection/Spotting**
+
+```json
+{
+  "cats": {},
+  "anns": {
+      "45346": {
+          "mask":[468.9,286.7,468.9,295.2,493.0,295.8,493.0,287.2],
+          "class":"machine printed",
+          "bbox":[468.9,286.7,24.1,9.1],
+          "image_id":522579,
+          "id":167312,
+          "language":"english",
+          "area":55.5,
+          "utf8_string":"the",
+          "legibility":"legible"
+      },
+      // ...
+  },
+  "imgs": {
+      "522579": {
+          "file_name":"COCO_train2014_000000492314.jpg",
+          "height":480,
+          "width":640,
+          "id":522579
+      },
+      // ...
+  },
+  "imgToAnns": {
+
+  },
+  "info": {}
+}
+```
--- mmocr/datasets/preparers/parsers/coco_parser.py
+++ mmocr/datasets/preparers/parsers/coco_parser.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
 from typing import Dict, Tuple
 
 from mmdet.datasets.api_wrappers import COCO
@@ -15,32 +16,60 @@ class COCOTextDetAnnParser(BaseParser):
         data_root (str): The root path of the dataset. Defaults to None.
         nproc (int): The number of processes to parse the annotation. Defaults
             to 1.
+        cocotext (bool): Whether to use the cocotext format. Defaults to False.
     """
 
-    def __init__(self, data_root: str = None, nproc: int = 1) -> None:
+    def __init__(self,
+                 data_root: str = None,
+                 nproc: int = 1,
+                 cocotext: bool = False) -> None:
 
         super().__init__(nproc=nproc, data_root=data_root)
+        self.cocotext = cocotext
 
     def parse_files(self, files: Tuple, split: str = None) -> Dict:
         """Parse single annotation."""
         samples = list()
         coco = COCO(files)
+        if self.cocotext:
+            for img in coco.dataset['imgs']:
+                if split == coco.dataset['imgs'][img]['set']:
+                    coco.imgs[img] = coco.dataset['imgs'][img]
+            for img in coco.dataset['imgToAnns']:
+                ann_ids = coco.dataset['imgToAnns'][img]
+                anns = [
+                    coco.dataset['anns'][str(ann_id)] for ann_id in ann_ids
+                ]
+                coco.dataset['imgToAnns'][img] = anns
+                coco.imgToAnns = coco.dataset['imgToAnns']
+                coco.anns = coco.dataset['anns']
         img_ids = coco.get_img_ids()
-
         total_ann_ids = []
         for img_id in img_ids:
             img_info = coco.load_imgs([img_id])[0]
             img_info['img_id'] = img_id
             img_path = img_info['file_name']
+            if self.data_root is not None:
+                img_path = osp.join(self.data_root, img_path)
             ann_ids = coco.get_ann_ids(img_ids=[img_id])
+            if len(ann_ids) == 0:
+                continue
+            ann_ids = [str(ann_id) for ann_id in ann_ids]
             ann_info = coco.load_anns(ann_ids)
             total_ann_ids.extend(ann_ids)
             instances = list()
             for ann in ann_info:
-                instances.append(
-                    dict(
-                        poly=ann['segmentation'][0],
-                        text=ann.get('text', None),
-                        ignore=ann.get('iscrowd', False)))
+                if self.cocotext:
+                    instances.append(
+                        dict(
+                            poly=ann['mask'],
+                            text=ann.get('utf8_string', None),
+                            ignore=ann['legibility'] == 'illegible'))
+                else:
+                    instances.append(
+                        dict(
+                            poly=ann['segmentation'][0],
+                            text=ann.get('text', None),
+                            ignore=ann.get('iscrowd', False)))
             samples.append((img_path, instances))
         return samples
