50c60d268142ef18cf9b5e1a9c0b489d423167d9
gaotongxiao <gaotongxiao@gmail.com>
2022-11-17 20:18:32 +0800

    update icdar2013

--- dataset_zoo/icdar2013/textrecog.py
+++ dataset_zoo/icdar2013/textrecog.py
@@ -1,9 +1,9 @@
-# By default, this configuration prepares the ICDAR13 1015 version.
-# If you want to use the original 1095 version, please
-# Comment out the following lines:
-# Line #38-45; #62-69
-# and uncomment the following lines:
-# Line #24-27; #51-58; #73-80
+# This configuration prepares the ICDAR13 857 and 1015
+# version, and uses ICDAR13 1015 version by default.
+# You may uncomment the lines if you want to you the original version,
+# which contains 1095 samples.
+# You can check out the generated base config and use the 857
+# version by using its corresponding config variables in your model.
 
 data_root = 'data/icdar2013'
 cache_path = 'data/cache'
@@ -20,13 +20,9 @@ data_obtainer = dict(
             md5='6f0dbc823645968030878df7543f40a4',
             split=['train'],
             content=['image'],
-            mapping=[
-                # [
-                #     'ic13_textrecog_train_img_gt/gt.txt',
-                #     'annotations/train.txt'
-                # ],
-                ['ic13_textrecog_train_img_gt', 'textrecog_imgs/train']
-            ]),
+            mapping=[[
+                'ic13_textrecog_train_img_gt/gt.txt', 'annotations/train.txt'
+            ], ['ic13_textrecog_train_img_gt', 'textrecog_imgs/train']]),
         dict(
             url='https://rrc.cvc.uab.es/downloads/'
             'Challenge2_Test_Task3_Images.zip',
@@ -55,18 +51,26 @@ data_obtainer = dict(
         #     md5='2634060ed8fe6e7a4a9b8d68785835a1',
         #     split=['test'],
         #     content=['annotation'],
-        #     mapping=[['ic13_textrecog_test_gt.txt', 'annotations/test.txt']])
+        #     mapping=[['ic13_textrecog_test_gt.txt', 'annotations/test.txt']]),  # noqa
         # 2. The widely-used version for academic purpose, which filters out
         # words with non-alphanumeric characters. This version contains 1015
         # test samples.
         dict(
             url='https://download.openmmlab.com/mmocr/data/1.x/recog/'
-            'icdar_2013/test_labels.json',
-            save_name='ic13_test_labels.json',
-            md5='2b78d3b29851402318bd4f69f41273b2',
+            'icdar_2013/textrecog_test_1015.json',
+            save_name='textrecog_test.json',
+            md5='5a384bf10ecaaa4021ca3d2e6263f5ca',
             split=['test'],
             content=['annotation'],
-            mapping=[['ic13_test_labels.json', 'textrecog_test.json']])
+        ),
+        dict(
+            url='https://download.openmmlab.com/mmocr/data/1.x/recog/'
+            'icdar_2013/textrecog_test_857.json',
+            save_name='textrecog_test_857.json',
+            md5='dc9f851db2eab02c77743674cf10df6a',
+            split=['test'],
+            content=['annotation'],
+        ),
     ])
 
 # Uncomment the data converter if you want to use the original 1095 version.
@@ -76,5 +80,16 @@ data_obtainer = dict(
 #     data_root=data_root,
 #     gatherer=dict(type='mono_gather', mapping="f'{split}.txt'"),
 #     parser=dict(
-#       type='ICDARTxtTextRecogAnnParser', separator=', ', format='img, text'),
+#         type='ICDARTxtTextRecogAnnParser', separator=', ', format='img, text'), # noqa
 #     dumper=dict(type='JsonDumper'))
+
+config_generator = dict(
+    type='TextRecogConfigGenerator',
+    data_root=data_root,
+    extra_anns=[
+        dict(split='test', file='textrecog_test.json'),
+        dict(
+            prefix='icdar2013_857',
+            split='test',
+            file='textrecog_test_857.json'),
+    ])
