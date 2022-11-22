39cbbafa491cf26d175a96eb059ee5ce26112ca3
liukuikun <641417025@qq.com>
2022-11-22 14:07:44 +0800

    [WIP] e2e metric

--- mmocr/evaluation/metrics/__init__.py
+++ mmocr/evaluation/metrics/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .e2e_hmean_iou_metric import E2EHmeanIOUMetric
 from .f_metric import F1Metric
 from .hmean_iou_metric import HmeanIOUMetric
 from .recog_metric import CharMetric, OneMinusNEDMetric, WordMetric
 
 __all__ = [
     'WordMetric', 'CharMetric', 'OneMinusNEDMetric', 'HmeanIOUMetric',
-    'F1Metric'
+    'F1Metric', 'E2EHmeanIOUMetric'
 ]
