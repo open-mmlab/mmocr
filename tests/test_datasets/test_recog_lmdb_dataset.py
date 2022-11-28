5dc9a6f0cc76348d61586e156043f11d443a2f39
gaotongxiao <gaotongxiao@gmail.com>
2022-11-28 17:21:24 +0800

    [Enhance] Discard deprecated lmdb dataset format and only support img+label now

--- tests/test_datasets/test_recog_lmdb_dataset.py
+++ tests/test_datasets/test_recog_lmdb_dataset.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
-import tempfile
 from unittest import TestCase
 
 import lmdb
@@ -24,23 +23,6 @@ class TestRecogLMDBDataset(TestCase):
             cursor = txn.cursor()
             cursor.putmulti(cache, dupdata=False, overwrite=True)
 
-    def test_label_only_dataset(self):
-
-        # test initialization
-        dataset = RecogLMDBDataset(
-            ann_file='tests/data/rec_toy_dataset/label.lmdb',
-            data_prefix=dict(img_path='imgs'),
-            pipeline=[])
-        dataset.full_init()
-        self.assertEqual(len(dataset), 10)
-        self.assertEqual(len(dataset.load_data_list()), 10)
-
-        # test load_data_list
-        anno = dataset.load_data_list()[0]
-        self.assertIn(anno['img_path'],
-                      ['imgs/1223731.jpg', 'imgs\\1223731.jpg'])
-        self.assertEqual(anno['instances'][0]['text'], 'GRAND')
-
     def test_label_and_image_dataset(self):
 
         # test initialization
@@ -54,27 +36,8 @@ class TestRecogLMDBDataset(TestCase):
 
         # test load_data_list
         anno = dataset.load_data_list()[0]
-        self.assertIn(anno['img_path'],
-                      [f'imgs/image-{1:09d}', f'imgs\\image-{1:09d}'])
+        self.assertIn(anno['img_path'], [
+            f'tests/data/rec_toy_dataset/imgs.lmdb/image-{1:09d}',
+            f'tests\\data\\rec_toy_dataset\\imgs.lmdb\\image-{1:09d}'
+        ])
         self.assertEqual(anno['instances'][0]['text'], 'GRAND')
-
-    def test_deprecated_format(self):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            self.create_deprecated_format_lmdb(
-                os.path.join(tmpdirname, 'data'))
-            dataset = RecogLMDBDataset(
-                ann_file=os.path.join(tmpdirname, 'data'),
-                data_prefix=dict(img_path='imgs'),
-                pipeline=[])
-
-            warm_msg = 'DeprecationWarning: The lmdb dataset generated with '
-            warm_msg += 'txt2lmdb will be deprecate, please use the latest '
-            warm_msg += 'tools/data/utils/recog2lmdb to generate lmdb dataset.'
-            warm_msg += ' See https://mmocr.readthedocs.io/en/'
-            warm_msg += 'latest/tools.html#'
-            warm_msg += 'convert-text-recognition-dataset-to-lmdb-format for '
-            warm_msg += 'details.'
-
-            dataset.full_init()
-            self.assertWarnsRegex(UserWarning, warm_msg)
-            dataset.close()
