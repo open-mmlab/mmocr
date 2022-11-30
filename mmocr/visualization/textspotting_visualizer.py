1a44354ac2420efd762be9409cca21df017a904c
ProtossDragoon <dlwkdgn1@naver.com>
2022-11-30 17:33:40 +0900

    [Style] Not need

--- mmocr/visualization/textspotting_visualizer.py
+++ mmocr/visualization/textspotting_visualizer.py
@@ -14,9 +14,6 @@ from .base_visualizer import BaseLocalVisualizer
 @VISUALIZERS.register_module()
 class TextSpottingLocalVisualizer(BaseLocalVisualizer):
 
-    def __init__(self, name: str = 'visualizer', **kwargs) -> None:
-        super().__init__(name=name, **kwargs)
-
     def _draw_instances(
         self,
         image: np.ndarray,
