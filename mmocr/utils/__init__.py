c06fa9979471e81000fcdb6503fe1d5a02cd0e76
liukuikun <641417025@qq.com>
2022-12-05 18:14:01 +0800

    add common typing

--- mmocr/utils/__init__.py
+++ mmocr/utils/__init__.py
@@ -21,10 +21,12 @@ from .polygon_utils import (boundary_iou, crop_polygon, is_poly_inside_rect,
 from .setup_env import register_all_modules
 from .string_utils import StringStripper
 from .typing_utils import (ColorType, ConfigType, DetSampleList,
-                           InitConfigType, KIESampleList, MultiConfig,
-                           OptConfigType, OptDetSampleList, OptInitConfigType,
-                           OptKIESampleList, OptMultiConfig, OptRecSampleList,
-                           OptTensor, RecForwardResults, RecSampleList)
+                           InitConfigType, InstanceList, KIESampleList,
+                           LabelList, MultiConfig, OptConfigType,
+                           OptDetSampleList, OptInitConfigType,
+                           OptInstanceList, OptKIESampleList, OptLabelList,
+                           OptMultiConfig, OptRecSampleList, OptTensor,
+                           RangeType, RecForwardResults, RecSampleList)
 
 __all__ = [
     'collect_env', 'is_3dlist', 'is_type_list', 'is_none_or_type', 'equal_len',
@@ -42,5 +44,6 @@ __all__ = [
     'OptConfigType', 'OptDetSampleList', 'OptInitConfigType', 'OptMultiConfig',
     'OptRecSampleList', 'RecSampleList', 'MultiConfig', 'OptTensor',
     'ColorType', 'OptKIESampleList', 'KIESampleList', 'is_archive',
-    'check_integrity', 'list_files', 'get_md5'
+    'check_integrity', 'list_files', 'get_md5', 'InstanceList', 'LabelList',
+    'OptInstanceList', 'OptLabelList', 'RangeType'
 ]
