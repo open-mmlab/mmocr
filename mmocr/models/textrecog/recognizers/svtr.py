583eb88cb3ff0472c4e14b9fb925d1dedab7761d
gaotongxiao <gaotongxiao@gmail.com>
2022-12-13 14:01:08 +0800

    update

--- mmocr/models/textrecog/recognizers/svtr.py
+++ mmocr/models/textrecog/recognizers/svtr.py
@@ -5,5 +5,5 @@ from .encoder_decoder_recognizer import EncoderDecoderRecognizer
 
 @MODELS.register_module()
 class SVTR(EncoderDecoderRecognizer):
-    """A PyTorch implement of : `SVTR: Scene Text Recognition with a Single
-        Visual Model <https://arxiv.org/abs/2205.00159>`_"""
+    """A PyTorch implementation of : `SVTR: Scene Text Recognition with a
+    Single Visual Model <https://arxiv.org/abs/2205.00159>`_"""
