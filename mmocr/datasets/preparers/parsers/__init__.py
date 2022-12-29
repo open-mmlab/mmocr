b79382cd6bc63dde72cb4ef0ff4f2b6e6f9c488c
Qing Jiang <mountchicken@outlook.com>
2022-12-29 15:19:49 +0800

    [Feature] CodeCamp #115 Add NAF to dataset preparer (#1609)
    * add naf converter
    
    * fix test
    
    * update
    
    * use fuzzy search instead
    
    * update
    
    * update

--- mmocr/datasets/preparers/parsers/__init__.py
+++ mmocr/datasets/preparers/parsers/__init__.py
@@ -3,6 +3,7 @@ from .coco_parser import COCOTextDetAnnParser
 from .funsd_parser import FUNSDTextDetAnnParser
 from .icdar_txt_parser import (ICDARTxtTextDetAnnParser,
                                ICDARTxtTextRecogAnnParser)
+from .naf_parser import NAFAnnParser
 from .svt_parser import SVTTextDetAnnParser
 from .totaltext_parser import TotaltextTextDetAnnParser
 from .wildreceipt_parser import WildreceiptKIEAnnParser
@@ -10,5 +11,6 @@ from .wildreceipt_parser import WildreceiptKIEAnnParser
 __all__ = [
     'ICDARTxtTextDetAnnParser', 'ICDARTxtTextRecogAnnParser',
     'TotaltextTextDetAnnParser', 'WildreceiptKIEAnnParser',
-    'COCOTextDetAnnParser', 'SVTTextDetAnnParser', 'FUNSDTextDetAnnParser'
+    'COCOTextDetAnnParser', 'SVTTextDetAnnParser', 'FUNSDTextDetAnnParser',
+    'NAFAnnParser'
 ]
