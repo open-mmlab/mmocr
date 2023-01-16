bd21b76d744df7d37772eeebd5643e11f280b3ba
liukuikun <641417025@qq.com>
2023-01-16 17:08:51 +0800

    add ut

--- tests/test_datasets/test_recog_lmdb_dataset.py
+++ tests/test_datasets/test_recog_lmdb_dataset.py
@@ -1,43 +1,44 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
+import tempfile
 from unittest import TestCase
 
+import cv2
 import lmdb
+import numpy as np
 
 from mmocr.datasets import RecogLMDBDataset
 
 
 class TestRecogLMDBDataset(TestCase):
 
-    def create_deprecated_format_lmdb(self, temp_dir):
-        os.makedirs(temp_dir, exist_ok=True)
-        env = lmdb.open(temp_dir, map_size=102400)
-        cache = [(str(0).encode('utf-8'), b'test test')]
-        with env.begin(write=True) as txn:
-            cursor = txn.cursor()
-            cursor.putmulti(cache, dupdata=False, overwrite=True)
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        tmp_image = np.zeros((100, 100, 3), dtype=np.uint8)
+        img_path = os.path.join(self.temp_dir.name, 'image.jpg')
+        cv2.imwrite(img_path, tmp_image)
+        with open(img_path, 'rb') as f:
+            img_bin = f.read()
+        self.lmdb_path = os.path.join(self.temp_dir.name, 'lmdb')
+        env = lmdb.open(self.lmdb_path, map_size=102400)
+        self.total_number = 5
 
-        cache = []
-        cache.append((b'total_number', str(1).encode('utf-8')))
         with env.begin(write=True) as txn:
-            cursor = txn.cursor()
-            cursor.putmulti(cache, dupdata=False, overwrite=True)
+            for idx in range(self.total_number):
+                txn.put(f'label-{idx + 1:09d}'.encode('utf-8'), b'mmocr')
+                txn.put(f'image-{idx + 1:09d}'.encode('utf-8'), img_bin)
+            txn.put('num-samples'.encode('utf-8'),
+                    str(self.total_number).encode('utf-8'))
+        env.close()
 
     def test_label_and_image_dataset(self):
 
         # test initialization
-        dataset = RecogLMDBDataset(
-            ann_file='tests/data/rec_toy_dataset/imgs.lmdb',
-            data_prefix=dict(img_path='imgs'),
-            pipeline=[])
+        dataset = RecogLMDBDataset(ann_file=self.lmdb_path, pipeline=[])
         dataset.full_init()
-        self.assertEqual(len(dataset), 10)
-        self.assertEqual(len(dataset.load_data_list()), 10)
+        self.assertEqual(len(dataset), self.total_number)
+        self.assertEqual(len(dataset.load_data_list()), self.total_number)
 
-        # test load_data_list
-        anno = dataset.load_data_list()[0]
-        self.assertIn(anno['img_path'], [
-            f'tests/data/rec_toy_dataset/imgs.lmdb/image-{1:09d}',
-            f'tests/data/rec_toy_dataset/imgs.lmdb\\image-{1:09d}'
-        ])
-        self.assertEqual(anno['instances'][0]['text'], 'GRAND')
+        for data in dataset:
+            self.assertEqual(data['img'].shape, (100, 100, 3))
+            self.assertEqual(data['instances'][0]['text'], 'mmocr')
