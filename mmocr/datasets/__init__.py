208d78571296c098722f47009015226e9b4011cb
gaotongxiao <gaotongxiao@gmail.com>
2023-01-31 14:28:38 +0800

    Revert "[Feature] Add RepeatAugSampler"
    This reverts commit 2089b02b4844157670033766f257b5d1bca452ce.

--- mmocr/datasets/__init__.py
+++ mmocr/datasets/__init__.py
@@ -4,7 +4,6 @@ from .icdar_dataset import IcdarDataset
 from .ocr_dataset import OCRDataset
 from .recog_lmdb_dataset import RecogLMDBDataset
 from .recog_text_dataset import RecogTextDataset
-from .samplers import *  # NOQA
 from .transforms import *  # NOQA
 from .wildreceipt_dataset import WildReceiptDataset
 
--- mmocr/datasets/samplers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .repeat_aug import RepeatAugSampler
-
-__all__ = ['RepeatAugSampler']
