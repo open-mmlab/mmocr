2a2cab3c8c17cac31e6d00f2b68f9eb467a15727
Tong Gao <gaotongxiao@gmail.com>
2023-02-06 15:16:08 +0800

    [Checkpoints] Add ST-pretrained DB-series models and logs (#1635)
    * [Fix] Auto scale lr
    
    * update

--- configs/textdet/_base_/schedules/schedule_sgd_100k.py
+++ configs/textdet/_base_/schedules/schedule_sgd_100k.py
@@ -4,7 +4,7 @@ optim_wrapper = dict(
     optimizer=dict(type='SGD', lr=0.007, momentum=0.9, weight_decay=0.0001))
 
 train_cfg = dict(type='IterBasedTrainLoop', max_iters=100000)
-test_cfg = dict(type='TestLoop')
+test_cfg = None
 val_cfg = None
 # learning policy
 param_scheduler = [
