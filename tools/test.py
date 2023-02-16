f82047041578d1629d53083780c2c7928ca7b96c
liukuikun <24622904+Harold-lkk@users.noreply.github.com>
2023-02-16 10:27:07 +0800

    [Feature] Rec TTA (#1401)
    * Support TTA for recognition
    
    * updata readme
    
    * updata abinet readme
    
    * updata train_test doc for tta

--- tools/test.py
+++ tools/test.py
@@ -45,6 +45,8 @@ def parse_args():
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='Job launcher')
+    parser.add_argument(
+        '--tta', action='store_true', help='Test time augmentation')
     parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
@@ -106,6 +108,11 @@ def main():
 
     if args.show or args.show_dir:
         cfg = trigger_visualization_hook(cfg, args)
+
+    if args.tta:
+        cfg.test_dataloader.dataset.pipeline = cfg.tta_pipeline
+        cfg.tta_model.module = cfg.model
+        cfg.model = cfg.tta_model
 
     # save predictions
     if args.save_preds:
