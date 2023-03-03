fdb89d12aa26e3b6f47a2e082793a898a0ca25a1
gaotongxiao <gaotongxiao@gmail.com>
2023-03-03 19:42:36 +0800

    [Fix] Fix wrong ic13 textspotting split data; add lexicons to ic13, ic15 and totaltext

--- dataset_zoo/totaltext/textspotting.py
+++ dataset_zoo/totaltext/textspotting.py
@@ -1,8 +1,7 @@
 _base_ = ['textdet.py']
 
-# _base_.train_preparer.gatherer.img_dir = 'textdet_imgs/train'
-# _base_.train_preparer.packer.type = 'TextSpottingPacker'
-_base_.train_preparer = None
+_base_.train_preparer.gatherer.img_dir = 'textdet_imgs/train'
+_base_.train_preparer.packer.type = 'TextSpottingPacker'
 
 _base_.test_preparer.gatherer.img_dir = 'textdet_imgs/test'
 _base_.test_preparer.packer.type = 'TextSpottingPacker'
