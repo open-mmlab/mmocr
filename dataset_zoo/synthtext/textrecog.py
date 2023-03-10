5bb4f2663115588e82d54ef78c4fb81c315671f5
gaotongxiao <gaotongxiao@gmail.com>
2023-03-10 18:58:14 +0800

    textrec

--- dataset_zoo/synthtext/textrecog.py
+++ dataset_zoo/synthtext/textrecog.py
@@ -1,5 +1,7 @@
 _base_ = ['textdet.py']
 
+_base_.train_preparer.obtainer = None
+_base_.train_preparer.gatherer.img_dir = 'textdet_imgs/train'
 _base_.train_preparer.packer.type = 'TextRecogCropPacker'
 
 config_generator = dict(type='TextRecogConfigGenerator')
