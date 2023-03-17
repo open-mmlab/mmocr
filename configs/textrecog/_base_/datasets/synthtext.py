c6580a48c17b814794a18c7549f9db829d404eec
Tong Gao <gaotongxiao@gmail.com>
2023-03-17 14:34:12 +0800

    [Dataset Preparer] SynthText (#1779)
    * [Dataset] Support Synthtext
    
    * update
    
    * update
    
    * finalize setting
    
    * fix
    
    * textrec
    
    * update
    
    * add fake magnet obtainer
    
    * update rec
    
    * update
    
    * sample_ann

--- configs/textrecog/_base_/datasets/synthtext.py
+++ configs/textrecog/_base_/datasets/synthtext.py
@@ -1,25 +1,19 @@
-synthtext_textrecog_data_root = 'data/rec/SynthText/'
+synthtext_textrecog_data_root = 'data/synthtext'
 
 synthtext_textrecog_train = dict(
     type='OCRDataset',
     data_root=synthtext_textrecog_data_root,
-    data_prefix=dict(img_path='synthtext/SynthText_patch_horizontal'),
-    ann_file='train_labels.json',
-    test_mode=False,
-    pipeline=None)
-
-synthtext_an_textrecog_train = dict(
-    type='OCRDataset',
-    data_root=synthtext_textrecog_data_root,
-    data_prefix=dict(img_path='synthtext/SynthText_patch_horizontal'),
-    ann_file='alphanumeric_train_labels.json',
-    test_mode=False,
+    ann_file='textrecog_train.json',
     pipeline=None)
 
 synthtext_sub_textrecog_train = dict(
     type='OCRDataset',
     data_root=synthtext_textrecog_data_root,
-    data_prefix=dict(img_path='synthtext/SynthText_patch_horizontal'),
-    ann_file='subset_train_labels.json',
-    test_mode=False,
+    ann_file='subset_textrecog_train.json',
+    pipeline=None)
+
+synthtext_an_textrecog_train = dict(
+    type='OCRDataset',
+    data_root=synthtext_textrecog_data_root,
+    ann_file='alphanumeric_textrecog_train.json',
     pipeline=None)
