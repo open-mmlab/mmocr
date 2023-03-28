bb591d2b1b0430c0a3dc6d1a750c0ee98d9cde34
Tong Gao <gaotongxiao@gmail.com>
2023-03-28 10:40:53 +0800

    [Config] CTW1500 (#1814)

--- configs/textdet/_base_/datasets/ctw1500.py
+++ configs/textdet/_base_/datasets/ctw1500.py
@@ -1,17 +1,15 @@
-ctw1500_textdet_data_root = 'data/det/ctw1500'
+ctw1500_textdet_data_root = 'data/ctw1500'
 
 ctw1500_textdet_train = dict(
     type='OCRDataset',
     data_root=ctw1500_textdet_data_root,
-    ann_file='instances_training.json',
-    data_prefix=dict(img_path='imgs/'),
+    ann_file='textdet_train.json',
     filter_cfg=dict(filter_empty_gt=True, min_size=32),
     pipeline=None)
 
 ctw1500_textdet_test = dict(
     type='OCRDataset',
     data_root=ctw1500_textdet_data_root,
-    ann_file='instances_test.json',
-    data_prefix=dict(img_path='imgs/'),
+    ann_file='textdet_test.json',
     test_mode=True,
     pipeline=None)
