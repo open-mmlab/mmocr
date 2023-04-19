44c68b1a03a456cebe6f8572bb8c3f50dfa27d21
Kevin Wang <wangnu_043@126.com>
2023-04-19 17:06:14 +0800

    修改SERDataSample形式

--- mmocr/structures/ser_data_sample.py
+++ mmocr/structures/ser_data_sample.py
@@ -1,61 +1,82 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .kie_data_sample import KIEDataSample
+from mmengine.structures import BaseDataElement, LabelData
 
 
-class SERDataSample(KIEDataSample):
-    """A data structure interface of MMOCR. They are used as interfaces between
-    different components.
+class SERDataSample(BaseDataElement):
+    """A data structure interface of MMOCR for Semantic Entity Recognition.
+    They are used as interfaces between different components.
 
     The attributes in ``SERDataSample`` are divided into two parts:
 
-        - ``gt_instances``(InstanceData): Ground truth of instance annotations.
-        - ``pred_instances``(InstanceData): Instances of model predictions.
+        - ``gt_label``(LabelData): Ground truth label.
+        - ``pred_label``(LabelData): predictions label.
 
     Examples:
          >>> import torch
          >>> import numpy as np
-         >>> from mmengine.structures import InstanceData
+         >>> from mmengine.structures import LabelData
          >>> from mmocr.data import SERDataSample
-         >>> # gt_instances
+         >>> # gt_label
          >>> data_sample = SERDataSample()
          >>> img_meta = dict(img_shape=(800, 1196, 3),
          ...                 pad_shape=(800, 1216, 3))
-         >>> gt_instances = InstanceData(metainfo=img_meta)
-         >>> gt_instances.bboxes = torch.rand((5, 4))
-         >>> gt_instances.labels = torch.rand((5,))
-         >>> data_sample.gt_instances = gt_instances
-         >>> assert 'img_shape' in data_sample.gt_instances.metainfo_keys()
-         >>> len(data_sample.gt_instances)
-         5
+         >>> gt_label = LabelData(metainfo=img_meta)
+         >>> gt_label.item = 'mmocr'
+         >>> data_sample.gt_label = gt_label
+         >>> assert 'img_shape' in data_sample.gt_label.metainfo_keys()
          >>> print(data_sample)
          <SERDataSample(
              META INFORMATION
              DATA FIELDS
-             gt_instances: <InstanceData(
+             gt_label: <LabelData(
                      META INFORMATION
                      pad_shape: (800, 1216, 3)
                      img_shape: (800, 1196, 3)
                      DATA FIELDS
-                     labels: tensor([0.8533, 0.1550, 0.5433, 0.7294, 0.5098])
-                     bboxes:
-                     tensor([[9.7725e-01, 5.8417e-01, 1.7269e-01, 6.5694e-01],
-                             [1.7894e-01, 5.1780e-01, 7.0590e-01, 4.8589e-01],
-                             [7.0392e-01, 6.6770e-01, 1.7520e-01, 1.4267e-01],
-                             [2.2411e-01, 5.1962e-01, 9.6953e-01, 6.6994e-01],
-                             [4.1338e-01, 2.1165e-01, 2.7239e-04, 6.8477e-01]])
+                     item: 'mmocr'
                  ) at 0x7f21fb1b9190>
          ) at 0x7f21fb1b9880>
-         >>> # pred_instances
-         >>> pred_instances = InstanceData(metainfo=img_meta)
-         >>> pred_instances.bboxes = torch.rand((5, 4))
-         >>> pred_instances.scores = torch.rand((5,))
-         >>> data_sample = SERDataSample(pred_instances=pred_instances)
-         >>> assert 'pred_instances' in data_sample
+         >>> # pred_label
+         >>> pred_label = LabelData(metainfo=img_meta)
+         >>> pred_label.item = 'mmocr'
+         >>> data_sample = SERDataSample(pred_label=pred_label)
+         >>> assert 'pred_label' in data_sample
          >>> data_sample = SERDataSample()
-         >>> gt_instances_data = dict(
-         ...                        bboxes=torch.rand(2, 4),
-         ...                        labels=torch.rand(2))
-         >>> gt_instances = InstanceData(**gt_instances_data)
-         >>> data_sample.gt_instances = gt_instances
-         >>> assert 'gt_instances' in data_sample
+         >>> gt_label_data = dict(item='mmocr')
+         >>> gt_label = LabelData(**gt_label_data)
+         >>> data_sample.gt_label = gt_label
+         >>> assert 'gt_label' in data_sample
+         >>> assert 'item' in data_sample.gt_label
     """
+
+    @property
+    def gt_label(self) -> LabelData:
+        """LabelData: ground truth label.
+        """
+        return self._gt_label
+
+    @gt_label.setter
+    def gt_label(self, value: LabelData) -> None:
+        """gt_label setter."""
+        self.set_field(value, '_gt_label', dtype=LabelData)
+
+    @gt_label.deleter
+    def gt_label(self) -> None:
+        """gt_label deleter."""
+        del self._gt_label
+
+    @property
+    def pred_label(self) -> LabelData:
+        """LabelData: prediction label.
+        """
+        return self._pred_label
+
+    @pred_label.setter
+    def pred_label(self, value: LabelData) -> None:
+        """pred_label setter."""
+        self.set_field(value, '_pred_label', dtype=LabelData)
+
+    @pred_label.deleter
+    def pred_label(self) -> None:
+        """pred_label deleter."""
+        del self._pred_label
